8718642108d84e01b2ed899b3e1cecf45c22099e
dpinney <david.pinney@nreca.coop>
2012-08-14 14:14:20 -0400

    CLEANING!

--- doedemo/run.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-touch log.txt
-python doeDemo.py 1>>log.txt 2>>log.txt &
--- doedemo/scratch.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import treeParser as tp
-
-model_id = 'Simple Market System'
-parsed = tp.parse('./feeders/' + model_id + "/main.glm")
-print parsed
