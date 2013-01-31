8b15e1df7cb77c33d1c57e9fe8d1579f08e3e87c
dpinney <david.pinney@nreca.coop>
2013-01-31 12:12:14 -0500

    Capacitor Activation report ported to Highcharts.

--- reports/capacitorActivation.py
+++ reports/capacitorActivation.py
@@ -2,6 +2,7 @@
 
 import os
 import __util__ as util
+import json
 
 # The config template, when inserted, will have the string REMOVALID replaced with a unique GUID.
 configHtmlTemplate = "<a href='javascript:removeStudyReport(REMOVALID)' class='removeStudyReport'>x</a><table class='reportOptions'><tr><td>Report Name</td><td class='reportName'>capacitorActivation</td></tr></table>"
@@ -24,9 +25,21 @@ def outputHtml(analysisName):
 			if 'days' == resolution:
 				# Aggregate to the day level, averaging to percentage charging:
 				fullArray = [fullArray[0]] + util.aggCsv(fullArray[1:], lambda x:sum(x)/len(x), 'day')
-			outputBuffer += '<div id="chartDiv' + study + cap + '" class="capChart" style="height:90px"></div>'
-			chartOptions = '{chartArea:{left:60,top:20,height:60,width:"93%",height:"70%"}, hAxis:{title:"Time",textPosition:"none"}, vAxis:{title:"On/Off", gridlines:{count:2}}, colors:["gainsboro","silver","gray"], legend:{position:"top"}}'
-			outputBuffer += '<script>drawAreaChart(' + str(fullArray) + ',' + '"chartDiv' + study + cap + '",' + chartOptions + ')</script>'
+			# Draw the graphs:
+			graphParams = {
+				'chart':{'renderTo':'chartDiv' + study + cap, 'marginRight':20, 'marginBottom':10, 'marginTop':0, 'zoomType':'x', 'height':90},
+				'title':{'text':None},
+				'yAxis':{'title':{'text':None}, 'labels':{'enabled':False}},
+				'legend':{'enabled':False},
+				'credits':{'enabled':False},
+				'xAxis':{'categories':[x[0] for x in fullArray[1:]],'tickColor':'gray','lineColor':'gray','labels':{'enabled':False},'maxZoom':20},
+				'plotOptions':{'area':{'shadow':False,'stacking':'normal','marker':{'enabled':False},'fillOpacity':1,}},
+				'series':[	{'type':'area','name':'PhaseA','data':[x[1] for x in fullArray[1:]],'color':'gainsboro'},
+							{'type':'area','name':'PhaseB','data':[x[2] for x in fullArray[1:]],'color':'darkgray'},
+							{'type':'area','name':'PhaseC','data':[x[3] for x in fullArray[1:]],'color':'gray'}
+						]
+			}
+			outputBuffer += '<div id="chartDiv' + study + cap + '" class="capChart" style="height:90px"><script>new Highcharts.Chart(' + json.dumps(graphParams) + ')</script></div>\n'
 		outputBuffer += '</div>'
 	return outputBuffer + '</div>'
 
