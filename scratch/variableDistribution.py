7948c47f2f34ff8688b7b3e0575a1ec3882bc866
David Pinney <dpinney@me.com>
2013-02-15 16:40:42 -0500

    Waterheater model attachment code.

--- scratch/variableDistribution.py
+++ scratch/variableDistribution.py
@@ -3954,14 +3954,14 @@ Okay, here are the derived rules.
 
 REMEMBER: only 2/3 of houses have electric water heaters.
 
-schedule_skew: gaussian*2000
-temperature: 135
-tank_volume: 50
-heating_element_capacity: uniform between 3.7 and 5.3 kW
-thermostat_deadband: uniform between 2.0 and 6.0.
-location: INSIDE
-demand: waterX*1 for X uniform on ints [1,...,20]
-tank_setpoint: gaussian really tight around 130.
-tank_UA: uniform between 2.0 and 4.0.
+* schedule_skew: gaussian*2000
+* temperature: 135
+* tank_volume: 50
+* heating_element_capacity: uniform between 3.7 and 5.3 kW
+* thermostat_deadband: uniform between 2.0 and 6.0.
+* location: INSIDE
+* demand: waterX*1 for X uniform on ints [1,...,20]
+* tank_setpoint: gaussian really tight around 130.
+* tank_UA: uniform between 2.0 and 4.0.
 
 '''
