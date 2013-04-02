a5584c184eb51134262dc2ae89de8c6939f2c4da
David Pinney <dpinney@me.com>
2013-04-02 15:35:34 -0400

    Climate modified to render a template.

--- reports/climate.py
+++ reports/climate.py
@@ -3,14 +3,13 @@
 import os
 import __util__ as util
 import json
+from jinja2 import Template
 
 with open('./reports/defaultConfig.html','r') as configFile:
 	configHtmlTemplate = configFile.read().replace('{{reportName}}','climate')
 
 def outputHtml(analysisName):
 	# Put the title in:
-	outputBuffer = '<p class="reportTitle">Climate</p>\n'
-	outputBuffer += '<div id="climateReport" class="tightContent" style="position:relative">\n'
 	# Collect study variables:
 	pathPrefix = './analyses/' + analysisName
 	resolution = json.loads(util.fileSlurp(pathPrefix + '/metadata.json'))['simLengthUnits']
@@ -24,7 +23,9 @@ def outputHtml(analysisName):
 	else:
 		title = True
 	# Turn each study into graphics:
+	studyList = []
 	for study in studies:
+		studyDict = {'studyName':study}
 		climateFiles = [x for x in os.listdir(pathPrefix + '/studies/' + study) if x.startswith('Climate_')]
 		fullArray = util.csvToArray(pathPrefix + '/studies/' + study + '/' + climateFiles[0])
 		fullArray[0] = ['Timestamp','Temperature (dF)','D.Insolation (W/m^2)', 'Wind Speed', 'Rainfall (in/h)', 'Snow Depth (in)']
@@ -41,14 +42,14 @@ def outputHtml(analysisName):
 		colorMap = {1:'dimgray',2:'darkgray',3:'darkgray',4:'gainsboro',5:'gainsboro'}
 		for x in [1,2,3,4,5]:
 			graphParameters['series'].append({'name':fullArray[0][x],'data':util.roundSeries([float(y[x]) for y in fullArray[1:]]),'marker':{'enabled':False},'color':colorMap[x]})
-		# Write one study's worth of HTML:
-		outputBuffer += '<div id="climateStudy' + study + '" class="studyContainer">\n'
-		outputBuffer += '<div id="climateChartDiv' + study + '" style="height:250px"></div>\n'
-		if True == title:
-			outputBuffer += '<div class="studyTitleBox"><p class="studyTitle">' + study + '</p></div>\n'
-		outputBuffer += '<script>new Highcharts.Chart(' + json.dumps(graphParameters) + ')</script>\n'
-		outputBuffer += '</div>\n'
-	return outputBuffer + '</div>\n\n'
+		studyDict['graphParameters'] = json.dumps(graphParameters)
+		studyList.append(studyDict)
+	# Get the template in.
+	with open('./reports/climateOutput.html','r') as tempFile:
+		template = Template(tempFile.read())
+	# Write the results.
+	return template.render(studyList=studyList, title=title)
+
 
 def modifyStudy(analysisName):
 	pass
