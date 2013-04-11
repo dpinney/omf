522251c8328d76637880dd493b62fc361b6d660c
dpinney <david.pinney@nreca.coop>
2013-04-11 16:23:54 -0400

    Fixing embedded feeder edit link. Study name not feeder name...

---
+++ reports/studyDetailsOLD.py
@@ -0,0 +1,65 @@
+﻿#!/usr/bin/env python
+
+import os
+import __util__
+import feeder
+import json
+
+with open('./reports/defaultConfig.html','r') as configFile:
+	configHtmlTemplate = configFile.read().replace('{{reportName}}','studyDetails')
+
+def outputHtml(analysisName):
+	# Put the title in:
+	outputBuffer =  '<p class="reportTitle">Study Details</p>'
+	outputBuffer += '<div id="studyDetails" class="detailsContainer tightContent" style="height:400px;padding:10px;position:relative;">'
+	outputBuffer += ''' <style type="text/css" scoped>
+							table#detailsTable th {border-bottom:2px solid gray; text-align:left;}
+							table#detailsTable th, td {padding:5px;}
+						</style>'''
+	# Build up the data:
+	studies = []
+	climates = [['location','marker']]
+	pathPrefix = './analyses/' + analysisName
+	# put the map and table in:
+	for study in os.listdir(pathPrefix + '/studies/'):
+		with open(pathPrefix + '/studies/' + study + '/metadata.json', 'r') as mdFile:
+			metadata = json.load(mdFile)
+			metadata['name'] = study
+		climates.append([str(metadata['climate']),1])
+		studies.append([metadata['name'], metadata['sourceFeeder']])
+	outputBuffer += '<div id="mapDiv" style="position:absolute;width:500px;height:400px;top:0px;left:500px"><script>drawMap(' + str(climates) + ',"mapDiv")</script></div>'
+	# handle the creation date and other time variables:
+	with open(pathPrefix + '/metadata.json','r') as anaMdFile:
+		anaMd = json.loads(anaMdFile.read())
+		created = anaMd['created']
+		simLength = anaMd['simLength']
+		simLengthUnits = anaMd['simLengthUnits']
+		simStartDate = anaMd['simStartDate']
+	outputBuffer += '<div id="detailsDiv" style="position:absolute;width:500px;height:250px;left:0px;top:0px;padding:10px";overflow:auto><p>Analysis created ' + created + '</p><p>Simulated ' + str(simLength) + ' ' + simLengthUnits + ' starting on ' + simStartDate + '</p>'
+	# add the feeder table:
+	outputBuffer += '<table id="detailsTable" style="padding-top:10px"><tr><th>Study</th><th>Source Feeder</th></tr>'
+	for row in studies:
+		sourceFeedName = row[1]
+		outputBuffer += '<tr><td>' + row[0] + '</td><td><a href="/analysisFeeder/' + analysisName + '/' + row[0] + '">' + sourceFeedName + '</a></td></tr>'
+	outputBuffer += '</table></div>'
+	# Build up the pie chart:
+	dataTree = {}
+	pathPrefix = './analyses/' + analysisName + '/studies/'
+	for study in os.listdir('./analyses/' + analysisName + '/studies/'):
+		glmTree = feeder.parse(pathPrefix + study + '/main.glm')
+		names = [glmTree[x]['name'] for x in glmTree if 'name' in glmTree[x]]
+		dataTree[str(study)] = set(names)
+	# Partition the names into common names and those unique to each feeder:
+	commonNames = reduce(lambda x,y:x.intersection(y), dataTree.values())
+	uniqueNames = {study:dataTree[study].difference(commonNames) for study in dataTree}
+	uniqueNameCounts = {study:len(uniqueNames[study]) for study in uniqueNames}
+	uniqueNameCounts['Common to All'] = len(commonNames)
+	# Put the data into a chart-friendly format, and chart it:
+	pieChartData = [['Study','Components']] + [[study, uniqueNameCounts[study]] for study in uniqueNameCounts]
+	graphOptions = '{title:"Grid Components", colors:["gainsboro","silver","gray"], titleTextStyle:{fontSize:14},chartArea:{left:10}}'
+	outputBuffer += '<div id="comparisonPieChart" style="position:absolute;width:500px;height:150px;left:0px;top:250px"><script>drawPieChart(' + str(pieChartData) + ',"comparisonPieChart",' + graphOptions + ')</script></div>'
+	return outputBuffer + '</div>\n\n'
+
+def modifyStudy(analysisName):
+	pass
+	#TODO: implement if needed.
