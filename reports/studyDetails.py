5d3bfbc137687d9e71841d85eec95f149fbaff64
dpinney <david.pinney@nreca.coop>
2013-04-17 20:20:57 -0400

    study details ported to new JSON data

--- reports/studyDetails.py
+++ reports/studyDetails.py
@@ -24,6 +24,7 @@ def outputHtml(analysisName):
 	# Gather study data:
 	climates = {}
 	studyList = []
+	allComponents = set()
 	for study in os.listdir(pathPrefix + '/studies/'):
 		newStudy = {'studyName':study}
 		with open(pathPrefix + '/studies/' + study + '/metadata.json', 'r') as mdFile:
@@ -37,24 +38,19 @@ def outputHtml(analysisName):
 				climates[md['climate']] = 1
 		with open(pathPrefix + '/studies/' + study + '/cleanOutput.json','r') as outFile:
 			out = json.load(outFile)
-			# newStudy['componentNames'] = out['componentNames']
+			newStudy['componentNames'] = set(out['componentNames'])
+			allComponents.update(out['componentNames'])
 		studyList.append(newStudy)
 	climates = [['Climate','Studies']] + [[key.replace('.tmy2',''), climates[key]] for key in climates]
 	# Partition the names into common names and those unique to each feeder:
-	pie = {}
-	# commonNames = reduce(lambda x,y:x.intersection(y), dataTree.values())
-	# uniqueNames = {study:dataTree[study].difference(commonNames) for study in dataTree}
-	# uniqueNameCounts = {study:len(uniqueNames[study]) for study in uniqueNames}
-	# uniqueNameCounts['Common to All'] = len(commonNames)
-	# Put the data into a chart-friendly format, and chart it:
-	# pieChartData = [['Study','Components']] + [[study, uniqueNameCounts[study]] for study in uniqueNameCounts]
-	# graphOptions = '{title:"Grid Components", colors:["gainsboro","silver","gray"], titleTextStyle:{fontSize:14},chartArea:{left:10}}'
-	# outputBuffer += '<div id="comparisonPieChart" style="position:absolute;width:500px;height:150px;left:0px;top:250px"><script>drawPieChart(' + str(pieChartData) + ',"comparisonPieChart",' + graphOptions + ')</script></div>'
+	uniqueNameCounts = {x['studyName']:len(allComponents.difference(x['componentNames'])) for x in studyList}
+	uniqueNameCounts['Common to All'] = len(allComponents)
+	pieChartData = [['Study','Components']] + [[x, uniqueNameCounts[x]] for x in uniqueNameCounts]
 	# Get the template in.
 	with open('./reports/studyDetailsOutput.html','r') as tempFile:
 		template = Template(tempFile.read())
 	# Write the results.
-	return template.render(studyList=studyList, ana=ana, climates=json.dumps(climates), pie=json.dumps(pie))
+	return template.render(studyList=studyList, ana=ana, climates=json.dumps(climates), pieChartData=json.dumps(pieChartData))
 
 
 
