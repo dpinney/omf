9604dd3c1a53c5a0ac244f4c84b7a804df3a4e88
dpinney <david.pinney@nreca.coop>
2013-04-26 14:22:48 -0400

    Gridlabd solver updated to run on embedded binary on all platforms.

--- solvers/gridlabd/__init__.py
+++ solvers/gridlabd/__init__.py
@@ -4,18 +4,30 @@ import sys, struct, subprocess, os
 
 def run(analysisName, studyName):
 	# Choose our platform:
+	thisFile = os.path.realpath(__file__)
+	solverRoot = os.path.split(thisFile)[0]
+	# Get current environment variables:
+	enviro = os.environ
+	# print solverRoot
 	if sys.platform == 'win32' or sys.platform == 'cygwin':
 		if 8*struct.calcsize("P") == 64:
-			binary = "solvers/gridlabd/win64/gridlabd.exe"
+			binary = solverRoot + "\\win64\\gridlabd.exe"
+			enviro['GRIDLABD'] = solverRoot + "\\win64"
+			enviro['GLPATH'] = solverRoot + "\\win64\\"
 		else:
-			binary = "solvers/gridlabd/win32/gridlabd.exe"
+			binary = solverRoot + "\\win32\\gridlabd.exe"
+			enviro['GRIDLABD'] = solverRoot + "\\win32"
+			enviro['GLPATH'] = solverRoot + "\\win32\\"
 	elif sys.platform == 'darwin':
 		# Implement me, maybe.
 		pass
 	elif sys.platform == 'linux2':
-		binary = "solvers/gridlabd/linx64/gridlabd"
+		binary = solverRoot + "/linx64/gridlabd"
+		enviro['GRIDLABD'] = solverRoot + "/linx64"
+		enviro['GLPATH'] = solverRoot + "/linx64"
 	else:
 		print "Platform not supported ", sys.platform
+		return False
 	# Path to glm etc.:
 	studyPath = 'analyses/' + analysisName + '/studies/' + studyName
 	
@@ -23,7 +35,7 @@ def run(analysisName, studyName):
 	with open(studyPath + '/stdout.txt','w') as stdout, open(studyPath + '/stderr.txt','w') as stderr:
 		# TODO: turn standerr WARNINGS back on once we figure out how to supress the 500MB of lines gridlabd wants to write...
 		# TODO: actually hook up platform-specific binary instead of the site package.
-		proc = subprocess.Popen(['gridlabd','-w','main.glm'], cwd=studyPath, stdout=stdout, stderr=stderr)
+		proc = subprocess.Popen([binary,'-w','main.glm'], cwd=studyPath, stdout=stdout, stderr=stderr, env=enviro)
 		# Put PID.
 		with open(studyPath + '/PID.txt','w') as pidFile:
 			pidFile.write(str(proc.pid))
@@ -36,3 +48,13 @@ def run(analysisName, studyName):
 			return False
 	# Return true to indicate success.
 	return True
+
+def main():
+	# os.chdir('../..')
+	# run('zSolar Trio','NoSolar')
+	# with open('analyses/zSolar Trio/studies/NoSolar/stdout.txt') as stdout:
+	# 	print stdout.read()
+	pass
+
+if __name__ == '__main__':
+	main()
