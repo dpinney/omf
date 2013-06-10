91db45ea5a6ccd66bd34b4102aabe508917078ef
dpinney <david.pinney@nreca.coop>
2013-06-10 14:13:03 -0400

    Fixing LOC counter.

--- scratch/locCount.py
+++ scratch/locCount.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 
-
-''' 
+'''
 Count the number of lines of code in this project. Ignores the data stores and libraries, etc.
-I was at 2.5 kLOC as of 2012-10-24.
-At 12kLOC as of 2013-01-24. Wow!
-At 48kLOC as of 2013-06-04.
+2.5 kLOC as of 2012-10-24.
+9.6 kLOC as of 2013-06-10.
 '''
 
 import os
 
 def cleanList(inList):
 	goodSuffixes = ['py','js','htm','html']
-	libraries = ['d3.v3.js','highcharts.src.js','jquery-1.10.0.js']
+	libraries = ['..\static\d3.v3.js','..\static\highcharts.src.js','..\static\jquery-1.10.0.js']
 	return [x for x in inList if (x.split('.')[-1] in goodSuffixes and x not in libraries)]
 
 def lineCount(fileName):
@@ -20,6 +18,9 @@ def lineCount(fileName):
 		for x, y in enumerate(openFile):
 			pass
 	return x + 1
+
+def fileNameAndLineCount(fileName):
+	return [fileName, lineCount(fileName)]
 
 def recursiveFileList(direct):
 	fileList = []
@@ -30,4 +31,8 @@ def recursiveFileList(direct):
 allSource = cleanList(recursiveFileList('..'))
 lineCountList = map(lineCount, allSource)
 
-print sum(lineCountList)
+print 'Total:', sum(lineCountList), 'lines.'
+
+print 'Per-file breakdown:'
+for pair in map(fileNameAndLineCount, allSource):
+	print pair[1], 'lines in', pair[0]
