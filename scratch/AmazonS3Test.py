131ac61b98b94f12902081b3e7333d6ee8d94240
dpinney <david.pinney@nreca.coop>
2013-06-13 19:44:52 -0400

    S3 test updates.

--- scratch/AmazonS3Test.py
+++ scratch/AmazonS3Test.py
@@ -3,4 +3,10 @@
 import boto
 
 from boto.s3.connection import S3Connection
-# conn = S3Connection('<aws access key>', '<aws secret key>')
+from boto.s3.key import Key
+conn = S3Connection('AKIAIBEYABXHAKRQCB5Q', 'yaright')
+bucket = conn.create_bucket('ohdwptestthisbucket')
+k = boto.s3.key.Key(bucket)
+k.key = 'foobar'
+k.set_contents_from_string('Hello David here we are.')
+print k.get_contents_as_string()
