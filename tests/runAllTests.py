2b301cef755157453effba37ca527a74cfae2efb
Matt Havard <mh6445a@american.edu>
2014-02-18 10:56:01 -0500

    runAllTets.py is pretty solid now

--- tests/runAllTests.py
+++ tests/runAllTests.py
@@ -1,6 +1,6 @@
 #TODO: walk the /omf/ directory, run _tests() in all modules.
 
-import os, sys
+import os, sys, subprocess
 
 # Probably will eventually remove some of these,
 # especially scratch and calibration
@@ -16,34 +16,40 @@ IGNORE_DIRS = [
     "venv",
     ]
 
-from subprocess import call
+# Useful for debugging this script (runAllTests.py) specifically.
+# milToGridlab.py, for example, usually does not fail, but running its tests
+# takes a long time.
+IGNORE_FILES = [
+    "milToGridlab.py", 
+]
 
 def runAllTests(startingdir):
     os.chdir(startingdir)
-    sys.path.append(".")
     nextdirs = []
-    ignorefiles = ["milToGridlab.py", "storage.py"]
     the_errors = 0
-    misfires = []
+    misfires = {}
     for item in os.listdir("."):
-        if item not in ignorefiles and item.endswith(".py") and "def _tests():" in open(item).read():
-            # the_errors += call(["python", item])
-            x = call(["python", item])
-            the_errors += x
-            misfires += [os.path.join(os.getcwd(), item)] if x else []
+        if item not in IGNORE_FILES and item.endswith(".py") and "def _tests():" in open(item).read():
+            p = subprocess.Popen(["python", item], stderr=subprocess.PIPE)
+            p.wait()
+            if p.returncode:
+                the_errors += 1
+                misfires[os.path.join(os.getcwd(), item)] = p.stderr.read()
         elif os.path.isdir(item) and item not in IGNORE_DIRS:
             nextdirs.append(os.path.join(os.getcwd(), item))
     for d in nextdirs:
         e, m = runAllTests(d)
         the_errors += e
-        misfires += m
+        misfires.update(m)
     return the_errors, misfires
 
 if __name__ == "__main__":
     os.chdir(sys.argv[1] if len(sys.argv) > 1 else ".")
-    # print "There were", runAllTests(os.getcwd()), "errors"
     i, mis = runAllTests(os.getcwd())
-    print "there were", i, "errors"
-    for m in mis:
-        print m
-    
+    print "\n\n+------------------------+"
+    print "\n\nTEST RESULTS"
+    print "\n\n+------------------------+"
+    print i, "tests failed:\n\n"
+    for fname, err in mis.items():
+        print fname
+        print err, "\n\n"
