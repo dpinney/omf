8659721c0cf82c77ced1464daf2cc05ec4fb2b49
dpinney <david.pinney@nreca.coop>
2014-02-25 16:13:56 -0500

    Removed buggy __import__.

--- solvers/__init__.py
+++ solvers/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import os, sys
+import os, sys, imp
 
 # Path magic.
 myDir = os.path.dirname(__file__)
@@ -9,5 +9,6 @@ sys.path.append(myDir)
 # Grab all our submodules and include them whenever we include this package:
 __all__ = [x for x in os.listdir(myDir) if not x.startswith('__')]
 
-# Neat little import of all the sub-modules in the __all__ variable:
-map(lambda x:__import__(x), __all__)
+# Import of all the sub-modules:
+import gridlabd
+import nrelsam
