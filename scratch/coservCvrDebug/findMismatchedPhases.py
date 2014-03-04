678709cffbd345b496d1d6dae27c222ceb3567af
Matt Havard <mh6445a@american.edu>
2014-03-04 15:07:34 -0500

    pruning the problem links into different subsets

--- scratch/coservCvrDebug/findMismatchedPhases.py
+++ scratch/coservCvrDebug/findMismatchedPhases.py
@@ -29,11 +29,14 @@ def printProbLinks(probLinks):
 def phasEq(n1, n2):
 	return n1["phases"] == n2["phases"]
 
+def breakDown(lnk):
+	return lnk["fromnode"], lnk["tonode"], lnk["ldata"]
+
 def twoOutta3(lnk):
 	# We know that at least one object is mismatched.
 	# If one object is different from the other two, it returns that
 	# But if all three are different, it implicitly returns None
-	a, b, c = lnk["fromnode"], lnk["tonode"], lnk["ldata"]
+	a, b, c = breakDown(lnk)
 	if phasEq(a, b):
 		return c
 	elif phasEq(a, c):
@@ -41,13 +44,21 @@ def twoOutta3(lnk):
 	elif phasEq(b, c):
 		return a
 
-# Using these switches instead of functions because I want access to pretty much
-# all the vars below at the interpreter, but I want the option not to execute 
-# certain if blocks.  feeder.parse takes a long time and I don't need to do it
-# each time I make a tiny change to this script.
+def invalidPhaseNum(lnk):
+	# returns True if any of the objects have invalid phase numbers, like 26N, 17N, XXX, etc.
+	# implicitly returns None otherwise
+	# However, this would not catch something like ABABN, but I don't think there are any like that
+	# anyway
+	for k, gridobj in lnk.items():
+		for char in gridobj["phases"].strip('"'):
+			if char not in "ABCN":
+				return True
+
+def fltr(pred):
+	return len(filter(pred, problemlinks))
+
+# parsing the feeder takes a few seconds and it only needs to be done the first time you run the script
 parseNewGuy = False
-getLinks = True
-getpblinks = True
 
 if parseNewGuy:
 	print "parsing RectorAlphaPhases.glm"
@@ -60,22 +71,72 @@ if parseNewGuy:
 			newtree[treeObj["name"]] = treeObj
 	print "Done"
 
-if getLinks:
-	links = {k:v for k, v in newtree.items() if v.get("from") and v.get("to")}
+links = {k:v for k, v in newtree.items() if v.get("from") and v.get("to")}
 
-if getpblinks:
-	problemlinks = []
-	print "Finding problem links"
-	for lname, ldata in links.items():
-		fromnode = newtree[ldata["from"]]
-		tonode = newtree[ldata["to"]]
-		if gp(ldata) != gp(fromnode) or gp(ldata) != gp(tonode):
-			problemlinks.append({
-				"fromnode":fromnode,
-				"ldata":ldata,
-				"tonode":tonode
-				})
+problemlinks = []
+print "Finding problem links"
+for lname, ldata in links.items():
+	fromnode = newtree[ldata["from"]]
+	tonode = newtree[ldata["to"]]
+	if gp(ldata) != gp(fromnode) or gp(ldata) != gp(tonode):
+		problemlinks.append({
+			"fromnode":fromnode,
+			"ldata":ldata,
+			"tonode":tonode
+			})
 
-	print len(problemlinks), "problemlinks"
-	print "found all the problem links!  use printProbLinks if there's not too many!"
+print len(problemlinks), "node-link-node configs with AT LEAST one phase mismatch"
+print
+print len(filter(twoOutta3, problemlinks)), "node-link-node configs with EXACTLY one phase mismatch"
+print
+print len(filter(invalidPhaseNum, problemlinks)), "node-link-node configs with AT LEAST one invalid phase number"
+print
+print len(filter(lambda x: twoOutta3(x) and invalidPhaseNum(x), problemlinks)), "node-link-node configs with EXACTLY one phase mismatch and AT LEAST one invalid phase number"
 
+print
+xxx = filter(lambda x: not twoOutta3(x) and not invalidPhaseNum(x), problemlinks)
+print len(xxx), "more than one mismatch, no invalid phase numbers"
+print
+# print len(filter(lambda x: not twoOutta3(x) and not invalidPhaseNum(x), problemlinks))
+notInvalid = filter(lambda x: not invalidPhaseNum(x), problemlinks)
+print len(notInvalid), "at least one mismatch, no invalid phase numbers"
+print
+whatevs = filter(lambda x: twoOutta3(x) and not invalidPhaseNum(x), problemlinks)
+print len(whatevs), "exactly one mismatch, no invalid phase numbers"
+print
+lineneut = filter(lambda x: x["ldata"]["phases"] == "N", notInvalid)
+print len(lineneut), "at least one mismatch, no invalid phase numbers, line object just has N phase"
+print
+nodeneut= filter(lambda x: x["fromnode"]["phases"] == "N" or x["tonode"]["phases"] == "N", problemlinks)
+print len(nodeneut), "configs where a node just has N"
+
+def is_invalid(phasenum):
+	for e in phasenum.strip('"'):
+		if e not in "ABCN":
+			return 1
+	return 0
+
+def exactlyOneInvalid(lnk):
+	i = 0
+	for e in breakDown(lnk):
+		i += is_invalid(e["phases"])
+	return i == 1
+
+def oneIsSuperset(lnk):
+	a,b,c = breakDown(lnk)
+	m = max(a,b,c, key=lambda x: len(x["phases"].strip('"')))["phases"].strip('"')
+	for obj in [a,b,c]:
+		for c in obj["phases"].strip('"'):
+			if c not in m:
+				return False
+	return True
+
+
+# One Mismatch One Invalid
+omov = filter(lambda x: exactlyOneInvalid(x) and twoOutta3(x), problemlinks)
+print len(omov), "exactly one mismatch and exactly one invalid.  Logical deduction: the invalid phase is the mismatch"
+
+# Mismatch but one object's phases are a superset of the other two's
+mmbss = filter(oneIsSuperset, problemlinks)
+print
+print len(mmbss), "mismatched, but one object is a superset of the others"
