abe2b7ed31c447a2dd700f58b5e0376846e3e617
Matt Havard <mh6445a@american.edu>
2014-03-07 11:46:22 -0500

    best debug script(s) ever!

--- scratch/coservCvrDebug/findMismatchedPhases.py
+++ scratch/coservCvrDebug/findMismatchedPhases.py
@@ -1,95 +1,11 @@
 
-from omf import feeder
 
-def gp(obj):
-	# gp for get phases
-	objStr = obj["phases"].strip('"')
-	return objStr[:-1] if objStr[-1] == "N" else objStr
-
-def prLnkRng(lower, upper, probLinks):
-	for i in range(lower, upper):
-		if i < len(probLinks):
-			printOneLink(probLinks[i])
-		else:
-			print "All links have been printed"
-			return
-
-def printOneLink(l):
-	fromnode, ldata, tonode = l["fromnode"], l["ldata"], l["tonode"]
-	print "phase mismatch at", ldata["object"], ldata["name"],":"
-	print "linkobj:\n", ldata
-	print "tonode:\n", tonode
-	print "fromnode:\n",fromnode, "\n\n"
-
-def printProbLinks(probLinks):
-	for l in probLinks:
-		printOneLink(l)
-	print "that's all of em!"
-
-def phasEq(n1, n2):
-	return n1["phases"] == n2["phases"]
-
-def breakDown(lnk):
-	return lnk["fromnode"], lnk["tonode"], lnk["ldata"]
-
-def twoOutta3(lnk):
-	# We know that at least one object is mismatched.
-	# If one object is different from the other two, it returns that
-	# But if all three are different, it implicitly returns None
-	a, b, c = breakDown(lnk)
-	if phasEq(a, b):
-		return c
-	elif phasEq(a, c):
-		return b
-	elif phasEq(b, c):
-		return a
-
-def invalidPhaseNum(lnk):
-	# returns True if any of the objects have invalid phase numbers, like 26N, 17N, XXX, etc.
-	# implicitly returns None otherwise
-	# However, this would not catch something like ABABN, but I don't think there are any like that anyway
-	for k, gridobj in lnk.items():
-		for char in gridobj["phases"].strip('"'):
-			if char not in "ABCN":
-				return True
-
-def fltr(pred):
-	return len(filter(pred, problemlinks))
-
-def dictcopy(d):
-	newd = {}
-	for k, v in d.items():
-		newd[k] = v
-	return newd
-
-if "parsedFeeder" not in globals():
-	# Parsing the feeder takes a long time, so I don't want to do it every time I modify and re-run this script
-	print "parsing RectorAlphaPhases.glm"
-	parsedFeeder = feeder.parse("RectorAlphaPhases.glm")
-
-
-newtree = {}
-print "Parsing done, converting dict"
-for guid, t in parsedFeeder.items():
-	treeObj = dictcopy(t)
-	if treeObj.get("name") and treeObj.get("phases"):
-		treeObj["id"] = guid
-		newtree[treeObj["name"]] = treeObj
-print "Done"
-
-links = {k:v for k, v in newtree.items() if v.get("from") and v.get("to")}
-
-problemlinks = []
-print "Finding problem links"
-for lname, ldata in links.items():
-	fromnode = newtree[ldata["from"]]
-	tonode = newtree[ldata["to"]]
-	if gp(ldata) != gp(fromnode) or gp(ldata) != gp(tonode):
-		problemlinks.append({
-			"fromnode":fromnode,
-			"ldata":ldata,
-			"tonode":tonode
-			})
+import helperfuncs
+import feederSetup
+reload(helperfuncs)
+reload(feederSetup)
+from helperfuncs import *
+from feederSetup import *
 
 print len(problemlinks), "node-link-node configs with AT LEAST one phase mismatch"
 print
@@ -116,26 +32,6 @@ print
 nodeneut= filter(lambda x: x["fromnode"]["phases"] == "N" or x["tonode"]["phases"] == "N", problemlinks)
 print len(nodeneut), "configs where a node just has N"
 
-def is_invalid(phasenum):
-	for e in phasenum.strip('"'):
-		if e not in "ABCN":
-			return 1
-	return 0
-
-def exactlyOneInvalid(lnk):
-	i = 0
-	for e in breakDown(lnk):
-		i += is_invalid(e["phases"])
-	return i == 1
-
-def oneIsSuperset(lnk):
-	a,b,c = breakDown(lnk)
-	m = max(a,b,c, key=lambda x: len(x["phases"].strip('"')))["phases"].strip('"')
-	for obj in [a,b,c]:
-		for c in obj["phases"].strip('"'):
-			if c not in m:
-				return False
-	return True
 
 
 # One Mismatch One Invalid
@@ -146,3 +42,4 @@ print len(omov), "exactly one mismatch and exactly one invalid.  Logical deducti
 mmbss = filter(oneIsSuperset, problemlinks)
 print
 print len(mmbss), "mismatched, but one object is a superset of the others"
+tingytang()
