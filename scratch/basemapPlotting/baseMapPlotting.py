4992de295dfc288c0064a5dfbc3716f011223f5b
dpinney <david.pinney@nreca.coop>
2014-03-09 16:43:18 -0400

    Pretty basemap solar mesh.

--- scratch/basemapPlotting/baseMapPlotting.py
+++ scratch/basemapPlotting/baseMapPlotting.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 from mpl_toolkits.basemap import maskoceans
 from random import uniform, choice
+import csv
 
 # Lambert Conformal Conic map.
 # create polar stereographic Basemap instance.
@@ -23,33 +24,64 @@ m.drawcoastlines()
 m.drawstates()
 m.drawcountries()
 
-# # Getting image from OSM to show map features. 
-# import urllib2
-# fname, headers = urllib2.urlretrieve('http://render.openstreetmap.org/cgi-bin/export?bbox=-80.05067825317383,38.39379044654673,-79.96004104614258,38.4377914419658&scale=27116&format=png', filename='test.png')
-# print 'Downloaded file:', fname
-# im = plt.imread(fname)
-# m.imshow(interpolation='lanczos', origin='upper')
-# #TODO: make the lat/lon coordinates agree. Because clearly the current behavior is wacky.
+def addOsmImage():
+	# Getting image from OSM to show map features. 
+	import urllib2
+	fname, headers = urllib2.urlretrieve('http://render.openstreetmap.org/cgi-bin/export?bbox=-80.05067825317383,38.39379044654673,-79.96004104614258,38.4377914419658&scale=27116&format=png', filename='test.png')
+	print 'Downloaded file:', fname
+	im = plt.imread(fname)
+	m.imshow(interpolation='lanczos', origin='upper')
+	#TODO: make the lat/lon coordinates agree. Because clearly the current behavior is wacky.
+	plt.show()
 
-randPoints = [(uniform(-130,-60),uniform(20,50),uniform(5,45),choice(['red','blue','green'])) for x in range(90)]
-lons = [p[0] for p in randPoints]
-lats = [p[1] for p in randPoints]
-sizes = [p[2] for p in randPoints]
-colors = [p[3] for p in randPoints]
+def testPlots():
+	randPoints = [(uniform(-130,-60),uniform(20,50),uniform(5,45),choice(['red','blue','green'])) for x in range(90)]
+	lons = [p[0] for p in randPoints]
+	lats = [p[1] for p in randPoints]
+	sizes = [p[2] for p in randPoints]
+	colors = [p[3] for p in randPoints]
+	# Try a scatter plot
+	m.scatter(lons, lats, latlon=True, c=colors, s=sizes)
+	# Try a mesh
+	m.pcolor(lons, lats, sizes, latlon=True, tri=True)
+	plt.show()
 
-# # Try a scatter plot
-# m.scatter(lons, lats, latlon=True, c=colors, s=sizes)
-# # Try a mesh
-# m.pcolor(lons, lats, sizes, latlon=True, tri=True)
+def tmy2Scatter():
+	# Try a TMY2 Scatter Plot.
+	from tmy2Wrangling import points
+	lons = [p[0] for p in points]
+	lats = [p[1] for p in points]
+	sizes = [p[2] for p in points]
+	m.scatter(lons, lats, latlon=True, c='red', s=sizes)
+	plt.show()
 
-# Try a TMY2 Scatter Plot.
-from tmy2Wrangling import points
-lons = [p[0] for p in points]
-lats = [p[1] for p in points]
-sizes = [p[2] for p in points]
+def readNsol():
+	nsolData = []
+	with open('nsolOutputs.csv', 'r') as csvFile:
+		read = csv.reader(csvFile, quoting=csv.QUOTE_NONNUMERIC)
+		for row in read:
+			nsolData.append(row)
+	headers = nsolData[0]
+	table = [dict(zip(headers,row)) for row in nsolData[1:]]
+	return table
 
-m.scatter(lons, lats, latlon=True, c='red', s=sizes)
+def nsolScatter():
+	# Output
+	nsolTable = readNsol()
+	lons = [p['Lon'] for p in nsolTable]
+	lats = [p['Lat'] for p in nsolTable]
+	sizes = [p['ArrayOutput'] for p in nsolTable]
+	m.scatter(lons, lats, latlon=True, c='gold', s=sizes)
+	plt.show()
 
-# Output
-plt.show()
+def nsolMesh():
+	# Output
+	nsolTable = readNsol()
+	lons = [p['Lon'] for p in nsolTable]
+	lats = [p['Lat'] for p in nsolTable]
+	sizes = [p['ArrayOutput'] for p in nsolTable]
+	m.pcolor(lons, lats, sizes, latlon=True, tri=True)
+	plt.show()
 
+if __name__ == '__main__':
+	nsolMesh()
