352ab735e8b191bdbbc215a1e7ae8143bc401637
dpinney <david.pinney@nreca.coop>
2014-03-24 15:43:45 -0400

    Additions to model.

---
+++ scratch/model/gridlabModel.py
@@ -0,0 +1,14 @@
+import json
+
+with open('metadata.js','r') as jsonFile:
+	jsonFile.readline() # Burn one.
+	print json.load(jsonFile)
+
+def beginModel():
+	pass
+
+def saveModel():
+	pass
+
+def runModel():
+	pass
--- scratch/simulation/run.py
+++
@@ -1,5 +0,0 @@
-import json
-
-with open('metadata.js','r') as jsonFile:
-	jsonFile.readline() # Burn one.
-	print json.load(jsonFile)
