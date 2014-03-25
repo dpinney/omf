ec3bf016774ea1a412c630dfbd415a5c5de92dda
David Pinney <dpinney@me.com>
2014-03-24 22:58:49 -0400

    Snazzy output hiding in incomplete models.

--- scratch/model/allInputData.js
+++ scratch/model/allInputData.js
@@ -1,4 +1,13 @@
 allInputData =
 {
-	"hasCherryOnTop":true
+    "status": "postRun", 
+    "created": "2013-10-03 11:59:24.514000", 
+    "modelType": "gridlabSingle",
+    "runTime": "0:00:20",
+    "modelName": "Single Gridlab Run",
+    "feederName": "Test Feeder",
+    "climateName": "Test Climate",
+    "simLength": "2048",
+    "simLengthUnits": "House",
+    "simStartDate": "2014-01-01"
 }
--- scratch/model/gridlabModel.py
+++ scratch/model/gridlabModel.py
@@ -4,11 +4,17 @@ with open('metadata.js','r') as jsonFile:
 	jsonFile.readline() # Burn one.
 	print json.load(jsonFile)
 
-def beginModel():
+def showModel():
+	''' Render the model template. '''
 	pass
 
 def saveModel():
+	''' Save the model template. '''
 	pass
 
 def runModel():
+	''' Run the model. '''
+	pass
+
+def deleteModel():
 	pass
