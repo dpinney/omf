b353b67714cc4497cac38e80c503fe5a7d252221
dpinney <david.pinney@nreca.coop>
2014-03-25 16:15:19 -0400

    TusconDebug Cleanup

--- scratch/model/models/gridlabModel.py
+++ scratch/model/models/gridlabModel.py
@@ -8,10 +8,13 @@ def showModel(inputDataDictionary, outputDataDictionary):
 	''' Render the model template. '''
 	pass
 
-def runModel(workingDirectory, inputDataDictionary):
+def run(workingDirectory, inputDataDictionary, writeTemplate=False):
 	''' Run the model. '''
 	# Make a directory. Put files into it. Translate files to needed format. Run Gridlab.
 	pass
+	if writeTemplate:
+		#TODO: output the template in such a way that it displays the output nicely.
+		pass
 
 def readingJsonExample():
 	with open('metadata.js','r') as jsonFile:
