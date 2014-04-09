6fda53ba5c3ceeb4910b7b989b0d16090703b324
dpinney <david.pinney@nreca.coop>
2014-04-09 13:55:38 -0400

    Little bit of template cleanup. Let's at least put head and body tags in the html. Jeez.

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,11 @@
 import os, sys
 
 # Make sure this module is on the path regardless of where its imported from.
-myDir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(myDir)
+_myDir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(_myDir)
 
 # Grab all our submodules and include them whenever we include this package:
-__all__ = [x.replace('.py','') for x in os.listdir(myDir)
+__all__ = [x.replace('.py','') for x in os.listdir(_myDir)
 	if x.endswith('.py') and not x.startswith('__')]
 
 # Import of all the sub-modules:
