7ae8cbec9543eb228c9d046e9e7c6a1d0c6b79c7
dpinney <david.pinney@nreca.coop>
2014-04-25 08:58:59 -0400

    Clean up.

--- getalldata.py
+++
@@ -1,6 +0,0 @@
-
-# So what needs to happen?
-# I need to get all the
-# Both the admin and the common user get access to their individual datas plus the public data
-# admin also gets all the individual user data
-
