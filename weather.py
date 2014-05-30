c0a49093c2b51971f0672d2b93d79df4c2b59fd5
dpinney <david.pinney@nreca.coop>
2014-05-30 18:29:33 -0400

    Weather.py is done. DONE.

--- weather.py
+++ weather.py
@@ -1,15 +1,192 @@
-from datetime import datetime, timedelta
-#from datetime import timezone # v3.0
-#from datetime import tzinfo #v2.7
-import math, os, re
+''' weather.py pulls weather data from wunderground.com, and formats it as a CSV that
+can be input to a Gridlab-D Climate object.  
 
-# custom imports
-import GetPeakSolar
-import ExtractWeather
+For example, to get a CSV that will play back 1 month of weather data
+for the region near the DCA airport, run in the terminal:
 
-# [0],    [1],         [2],       [3],     [4],                 [5],          [6],           [7],          [8],          [9],            [10],  [11],      [12],          [13] 
-# TimePST,TemperatureF,Dew PointF,Humidity,Sea Level PressureIn,VisibilityMPH,Wind Direction,Wind SpeedMPH,Gust SpeedMPH,PrecipitationIn,Events,Conditions,WindDirDegrees,DateUTC<br />
-# 12:53 AM,44.1,43.0,96,29.99,9.0,Calm,Calm,-,N/A,,Overcast,0,2010-03-01 08:53:00<br />
+python -c "import weather; makeClimateCsv('2010-07-01', '2010-08-01', 'DCA', './weatherDCA.csv')"
+
+To use that data as the climate simulation in a Gridlab model, include
+the following in a .glm:
+
+module tape;
+module climate;
+
+object csv_reader {
+	name "weatherReader";
+	filename "weatherDCA.csv";
+};
+
+object climate {
+	name "exampleClimate";
+	tmyfile "weatherDCA.csv";
+	reader weatherReader;
+}
+
+'''
+
+import os, urllib, json, csv, math, re, tempfile, shutil
+from os.path import join as pJoin
+from datetime import timedelta, datetime
+
+def makeClimateCsv(start, end, airport, outFilePath, cleanup=True):
+	''' Generate a climate timeseries CSV. See module docString for full help.'''
+	tempDir = tempfile.mkdtemp()
+	print "Working in", tempDir
+	_downloadWeather(start, end, airport=airport, workDir=tempDir)
+	_getPeakSolar(airport, workDir=tempDir)
+	_processWeather(start, end, airport=airport, workDir=tempDir)
+	shutil.copyfile(pJoin(tempDir,"weather.csv"), outFilePath)
+	print "Output CSV available at", outFilePath
+	if cleanup:
+		shutil.rmtree(tempDir)
+		print "Cleanup true, deleted", tempDir
+
+def _downloadWeather(start, end, airport, workDir):
+	''' Download weather CSV data to workDir. 1 file for each day between start and 
+	end (YYYY-MM-DD format). Location is set by airport (three letter airport code, e.g. DCA). '''
+	# Parse start and end dates.
+	start_dt = datetime.strptime(start, "%Y-%m-%d")
+	end_dt = datetime.strptime(end, "%Y-%m-%d")
+	# Calculate the number of days to fetch.
+	num_days = (end_dt - start_dt).days
+	work_day = start_dt
+	# Generate URLs and get data.
+	for i in range(num_days):
+		year = work_day.year
+		month = work_day.month
+		day = work_day.day
+		address = "http://www.wunderground.com/history/airport/{}/{:d}/{:d}/{:d}/DailyHistory.html?req_city=NA&reqstate=NA&req_statename=NA&format=1".format(airport, year, month, day)
+		filename = pJoin(workDir,"weather_{}_{:d}_{:d}_{:d}.csv".format(airport, year, month, day))
+		if os.path.isfile(filename):
+			continue # We have the file already, don't re-download it.
+		try:
+			f = urllib.urlretrieve(address, filename)
+		except:
+			print("ERROR: unable to get data from URL " + address)
+			continue # Just try to grab the next one.
+		work_day = work_day + timedelta(days = 1) # Advance one day
+
+def _airportCodeToLatLon(airport):
+	''' Airport three letter code -> lat/lon of that location. ''' 
+	api_key = 'AIzaSyDR9Iwhp1xbs31c6FpO-5g0bdEXCyN1JL8'
+	service_url = 'https://www.googleapis.com/freebase/v1/mqlread'
+	query = [{'id':None, 'name':None, 'type': '/aviation/airport', 'iata' : airport,
+		'/location/location/geolocation' : [{'latitude' : None, 'longitude':None, 'elevation':None}] }]
+	params = {'query' : json.dumps(query), 'key' : api_key}
+	url = service_url + '?' + urllib.urlencode(params)
+	# query Freebase for specified IATA airport code
+	response = json.loads(urllib.urlopen(url).read())
+	# if zero results, fail
+	if len(response['result']) == 0:
+		print("Failed to return any airport location for", airport)
+		return None
+	# if more than one result, get first one
+	if len(response['result']) > 1:
+		print("Multiple airport results (strange!), using the first result")
+	# get GPS from result
+	lat = response['result'][0]['/location/location/geolocation'][0]['latitude']
+	lon = response['result'][0]['/location/location/geolocation'][0]['longitude']
+	return (lat, lon)
+
+def _getPeakSolar(airport, workDir="./", dniScale=1.0, dhiScale=1.0, ghiScale=1.0):
+	''' get the peak non-cloudy solar data from a locale.  takes the ten most solar-energetic days and averages
+		the values out into one 24-hour TMY3 file.'''
+	lat, lon = _airportCodeToLatLon(airport)
+	# Get metadata file.
+	metaFileName="TMY3_StationsMeta.csv"
+	address = 'http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/tmy3/TMY3_StationsMeta.csv'
+	destination = pJoin(workDir, metaFileName)
+	urllib.urlretrieve(address, destination) # if this fails, we let it break.
+	metaFile = open(destination)
+	# Parse metadata file.
+	metaFileReader = csv.reader(metaFile, delimiter=',')
+	metaFileRows = [line for line in metaFileReader] # NOTE: first line is headers.
+	metaHeaders = metaFileRows.pop(0)
+	# find our desired column indices
+	latItem = next(x for x in metaHeaders if 'Latitude' in x)
+	longItem = next(x for x in metaHeaders if 'Longitude' in x)
+	idItem = next(x for x in metaHeaders if 'USAF' in x)
+	latIndex = metaHeaders.index(latItem)
+	longIndex = metaHeaders.index(longItem)
+	idIndex = metaHeaders.index(idItem)
+	stationDist = []
+	for row in metaFileRows:
+		try:
+			x = float(row[latIndex]) - lat
+			y = float(row[longIndex]) - lon
+			stationDist.append((x*x+y*y, row[idIndex]))
+		except:
+			pass # 'bad' line
+	# Find nearest station based on metadata CSV.
+	minDist = min([line[0] for line in stationDist])
+	stationResult = [line[1] for line in stationDist if line[0] == minDist]
+	stationId = stationResult[0] # ID string from the first result
+	# Get specified TMY csv file.
+	tmyURL = 'http://rredc.nrel.gov/solar/old_data/nsrdb/1991-2005/data/tmy3/' + stationId + 'TY.csv'
+	tmyResult = urllib.urlretrieve(tmyURL, pJoin(workDir,stationId + 'TY.csv'))
+	tmyFile = open(tmyResult[0])
+	tmyReader = csv.reader(tmyFile, delimiter=',')
+	tmyLines = [line for line in tmyReader]
+	tmyHeader = tmyLines.pop(0)
+	tmyColumns = tmyLines.pop(0)
+	seasonDict = {1:"Winter", 2:"Winter", 3:"Spring", 4:"Spring", 5:"Spring", 6:"Summer", 7:"Summer", 8:"Summer", 9:"Fall", 10:"Fall", 11:"Fall", 12:"Winter"}
+	seasonList = ["Winter", "Spring", "Summer", "Fall"]
+	dateItem = next(x for x in tmyColumns if 'Date' in x)
+	ghiItem = next(x for x in tmyColumns if 'GHI (W/m^2)' in x)
+	dniItem = next(x for x in tmyColumns if 'DNI (W/m^2)' in x)
+	dhiItem = next(x for x in tmyColumns if 'DHI (W/m^2)' in x)
+	dateIndex = tmyColumns.index(dateItem)
+	ghiIndex = tmyColumns.index(ghiItem)
+	dniIndex = tmyColumns.index(dniItem)
+	dhiIndex = tmyColumns.index(dhiItem)
+	dateFormat = "%m/%d/%Y"
+	dayDict = {}
+	for line in tmyLines:
+		lineDate = datetime.strptime(line[dateIndex], dateFormat)
+		if lineDate in dayDict:
+			dayDict[lineDate].append(line)
+		else:
+			dayDict[lineDate] = [line]
+	# Each entry in dayDict is now a 24-item list.
+	energyDict = {}
+	for season in seasonList:
+		energyDict[season] = []
+	for key in dayDict: # for each day,
+		day = dayDict[key]
+		# calculate
+		dhi = [float(value[dhiIndex]) for value in day]
+		dni = [float(value[dniIndex]) for value in day]
+		ghi = [float(value[ghiIndex]) for value in day]
+		values = (math.fsum(dni), math.fsum(dhi), math.fsum(ghi))
+		energy = values[0] * dniScale + values[1] * dhiScale + values[2] * ghiScale
+		season = seasonDict[key.month]
+		energyDict[season].append((key, (dni, dhi, ghi), energy))
+	# for each of four seasons,
+	solarHeader = "Time(HH:MM),GHI_Normal,DNI_Normal,DHI_Normal\n"
+	for season in seasonList:
+		data = sorted(energyDict[season], key=lambda dayItem: dayItem[2]) # sort by energy
+		#  * pick out top 10 days
+		topDays = data[-10:] 
+		#  * write file with average of those ten days
+		tDni = [0.0] * 25
+		tDhi = [0.0] * 25
+		tGhi = [0.0] * 25
+		for day in topDays:
+			dni, dhi, ghi = day[1]
+			for i in range(len(dni)):
+				tDni[i] += dni[i]
+				tDhi[i] += dhi[i]
+				tGhi[i] += ghi[i]
+		# write average day to file
+		fileName = "solar_{}_{}.csv".format(airport, season.lower())
+		outFile = open(pJoin(workDir, fileName), "w")
+		# Time(HH:MM), GHI_Normal, DNI_Normal and DHI_Normal
+		outFile.write(solarHeader)
+		smPsf = 10.7639104 # square feet ~ source Google
+		for i in range(24):
+			outFile.write("{}:00,{},{},{}\n".format(str(i), str(tGhi[i]/10/smPsf), str(tDni[i]/10/smPsf), str(tDhi[i]/10/smPsf) ) )        
+		outFile.close()
 
 class Weather:
 	Time = ""
@@ -39,20 +216,14 @@ class Weather:
 		self.Seas = seasonDict[self.Time.month]
 		self.Solar = 0
 		return self
-def ParseWeather(line, hdr):
-	rv = Weather()
-	rv.Time = datetime.strptime("%Y-%m-%d %H:%M:%S<br />", line)
-	rv.Temp = float(line[1])
-	
 
-def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
-	# sanity checks
-	startDate = datetime.strptime(start, "%m-%d-%Y")
-	endDate = datetime.strptime(end, "%m-%d-%Y")
-	if (startDate > endDate):
-		print("start date is after the end date!")
-		return -1
-
+def _processWeather(start, end, airport='', workDir='./', interpolate="linear"):
+	''' Take CSV files in workDir from _downloadWeather and _getPeakSolar, and combine them into a CSV that can be read into GLD's climate object. '''
+	startDate = datetime.strptime(start, "%Y-%m-%d")
+	endDate = datetime.strptime(end, "%Y-%m-%d")
+	# [0],    [1],         [2],       [3],     [4],                 [5],          [6],           [7],          [8],          [9],            [10],  [11],      [12],          [13] 
+	# TimePST,TemperatureF,Dew PointF,Humidity,Sea Level PressureIn,VisibilityMPH,Wind Direction,Wind SpeedMPH,Gust SpeedMPH,PrecipitationIn,Events,Conditions,WindDirDegrees,DateUTC<br />
+	# 12:53 AM,44.1,43.0,96,29.99,9.0,Calm,Calm,-,N/A,,Overcast,0,2010-03-01 08:53:00<br />
 	# condition dictionary
 	conditionDict = {"Clear" : (1.0, 1.0, 1.0),
 								"Partly Cloudy" : (0.9, 1.1, 0.95),
@@ -205,26 +376,25 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 											"Unknown Precipitation" : (1.0, 1.0, 1.0), #
 											"Unknown" : (1.0, 1.0, 1.0) #
 											}
-
-	#for item in moreConditionDict:
+	# for item in moreConditionDict:
 	#	a, b, c = moreConditionDict[item]
 	#	print("{}: {}, {}, {}".format(item, str(a), str(b), str(c))) 
 	seasonDict = {1 : "Winter", 2 : "Winter", 3 : "Spring", 4 : "Spring", 5 : "Spring", 6 : "Summer", 7 : "Summer", 8 : "Summer", 9 : "Fall", 10 : "Fall", 11 : "Fall", 12 : "Winter"}
 	# interpolation options
 	interpolateList = ["none", "linear", "quadratic"]
 	# scan for files
-	fileList = os.listdir(wdir)
+	fileList = os.listdir(workDir)
 	filePtrn = re.compile("weather_(?P<loc>[A-Z]+)_(?P<raw_date>[0-9]+_[0-9]+_[0-9]+).csv")
 	matchedFiles = list(filter(filePtrn.match, fileList))
 	if len(matchedFiles) is 0:
-		print("no weather files found in {}".format(wdir))
+		print("no weather files found in {}".format(workDir))
 		return -1
 	# identify desired files
 	matchedList = [filePtrn.match(x) for x in matchedFiles]
 	fileParts = [m.groupdict() for m in matchedList]
 	filteredParts = list(filter(lambda x: x["loc"] == airport, fileParts))
 	if len(filteredParts) is 0:
-		print("no weather files found in {} that match airport {}".format(wdir, airport))
+		print("no weather files found in {} that match airport {}".format(workDir, airport))
 		return -1
 	# filteredParts now contains a list of dictionaries where "loc" == airport
 	# process dates into datetime objects
@@ -246,15 +416,11 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 	# useFiles now has a list of all the files in the range that we want to use
 	myData = []
 	weatherData = []
-	
-	currDir = os.getcwd()
-	os.chdir(wdir)
 	for eachFile in useFiles:
-		myFile = open(eachFile["file"], "r")
+		myFile = open(pJoin(workDir, eachFile["file"]), "r")
 		myLines = myFile.readlines()
 		#myLines = [line+","+eachFile["file"] for line in myLinesPre]
 		myData.extend(myLines)
-	os.chdir(currDir)
 	weatherDataInt = list(filter(lambda x: len(x) > 1, myData))						# remove all "<br />" lines
 	weatherHeader = list(filter(lambda x: "Time" in x, weatherDataInt))			# capture all header lines
 	weatherDataTrim = list(filter(lambda x: "Time" not in x, weatherDataInt))	# remove all header lines
@@ -267,9 +433,7 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 	# (cont'd)
 	# Gust SpeedMPH, PrecipitationIn, Events,  Conditions,    WindDirDegrees, DateUTC<br />
 	# '-',           'N/A',           'Fog',   'Shallow Fog', '0',            '2010-03-01 09:53:00<br />\n']
-	
 	# record the time, temp, humidity, wind, conditions, and date.
-
 	#get time & date indices
 	timeIndex = 0
 	utcIndex = 0
@@ -280,13 +444,11 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 			utcIndex = index
 	# slice timezone from keys
 	tz = weatherKeys[timeIndex][4:7]
-	
 	# use the first sample to determine TZ offset
 	firstDt = datetime.strptime(weatherData[0][timeIndex], "%I:%M %p")
 	t1 = datetime(year=startDate.year, month=startDate.month, day=startDate.day, hour=firstDt.hour, minute=firstDt.minute) # local
 	t2 = datetime.strptime(weatherData[0][utcIndex].split("<")[0], "%Y-%m-%d %H:%M:%S") # GMT
 	tzDelta = t1 - t2
-	
 	# replace "Calm" wind with 0
 	windIndex = 0
 	windKey = "Wind SpeedMPH"
@@ -301,11 +463,9 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 			else:
 				#print("not replacing '"+str(entry)+"' at time "+str(weatherData[index][utcIndex]))
 				weatherData[index][windIndex] = float(weatherData[index][windIndex])
-			
 	else:
 		windIndex = -1
 		# error and explode
-	
 	# replace "N/A" humidity with 0
 	humidKey = "Humidity"
 	humidIndex = 0
@@ -317,7 +477,6 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 	else:
 		humidIndex = -1
 		# error and explode
-	
 	# replace 'Conditions" with dictionary value
 	condKey = "Conditions"
 	condIndex = 0
@@ -333,7 +492,6 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 	else:
 		condIndex = -1
 		# error and explode
-	
 	# get temperature index
 	heatKey = "TemperatureF"
 	heatIndex = 0
@@ -341,16 +499,12 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 		heatIndex = weatherKeys.index(heatKey)
 	else:
 		heatIndex = -1
-	
-	
-	
 	# convert to useful values
 	weatherList = []
 	# def Weather(self, tm, dt, tz, t, h, w, c, d):
 	for entry in weatherData:
 		sample = Weather().Build(entry[timeIndex], entry[utcIndex], tzDelta, entry[heatIndex], entry[humidIndex], entry[windIndex], entry[condIndex], entry)
 		weatherList.append(sample)
-	
 	# sanity-check numbers
 	for index,entry in enumerate(weatherList):
 		# * temperature
@@ -412,59 +566,36 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 				#except IndexError:
 				#	print("error getting condition value from sample at "+str(entry.Time))
 				#	return
-				
 	# add 00:00:00 to each day
-	
 	seasons = {	"Winter" : ([], "solar_{}_winter_csv".format(airport)),
 						"Spring" : ([], "solar_{}_spring.csv".format(airport)),
 						"Summer" : ([], "solar_{}_summer.csv".format(airport)),
 						"Fall" : ([], "solar_{}_fall.csv".format(airport))}
-	
-	# look for the solar files
-	if os.path.isdir(wdir):
-		os.chdir(wdir)
-	if not os.path.isfile(seasons["Winter"][1]):
-		os.chdir(currDir)
-		rv = GetPeakSolar.GetPeakSolar(airport, wdir)
-		os.chdir(currDir) # in case it's left dirty
-		if rv is None:
-			print("ERROR: unable to retrieve solar information")
-			return -1
-
-	# load and parse these files for key in seasons.keys():
-	seasonCount = {	"Winter" : 0,
-								"Spring" : 0,
-								"Summer" : 0,
-								"Fall" : 0}
+	# load and parse solar files for key in seasons.keys():
+	seasonCount = {	"Winter":0,	"Spring":0, "Summer":0, "Fall":0}
 	for line in weatherList:
 		seasonCount[line.Seas] += 1
-		
 	for season in seasonCount:
 		if seasonCount[season] == 0:
 			continue
 		# Time(HH:MM), GHI_Normal, DNI_Normal and DHI_Normal
 		#  * {2-4} used for 'real data'.
 		seasonData, seasonFileName = seasons[season]
-		if os.path.isdir(wdir):
-			os.chdir(wdir)
-		seasonFile = open(seasonFileName, "r")
-		os.chdir(currDir)
+		seasonFile = open(pJoin(workDir, seasonFileName), "r")
 		seasonFileLines = seasonFile.readlines()
 		seasonFileLines.pop(0) # header
 		seasonDataStr = [str.split(line, ",") for line in seasonFileLines]
 		#print(seasonDataStr)
 		seasonData.extend([(float(data[2]), float(data[3]), float(data[1])) for data in seasonDataStr]) # skip time in index 0
 	pass
-	
 	# find solar data per-season, interpolate from hourly to the sample time, add into weather dictionary
 	for sample in weatherList:
-		wdir, dif, glo = sample.Cond
+		wDir, dif, glo = sample.Cond
 		sampleHour = sample.Time.hour
 		seasonData, Season = seasons[sample.Seas]
 		dirMod, difMod, gloMod = seasonData[sampleHour]
-		sample.Solar = (wdir*dirMod, dif*difMod, glo*gloMod)
+		sample.Solar = (wDir*dirMod, dif*difMod, glo*gloMod)
 		#print(str(sample.Time)+": "+str(sample.Cond)+" * "+str(seasonData[sampleHour])+" = "+str(sample.Solar))
-	
 	# interpolate downloaded data into 
 	outData = []
 	interval = timedelta(minutes = 5)
@@ -503,7 +634,7 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 				nxEntry = weatherList[index+1]
 				#  * for each step, write an interpolated value.
 				for n in range(1, steps):
-					ratio = n * float(interval.seconds) / float(timeStep.seconds) # @todo verify this works and doesn't break on 'unit conversion'
+					ratio = n * float(interval.seconds) / float(timeStep.seconds)
 					sample = Weather()
 					sample.Temp = lerp(entry.Temp, nxEntry.Temp, ratio)
 					sample.Humi = lerp(entry.Humi, nxEntry.Humi, ratio)
@@ -552,18 +683,10 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 					if sample.Wind < 0:
 						sample.Wind = 0 # clip, since qerp can take values below zero
 					outData.append(sample)
-			pass
-		
-		pass
-	
-	### @todo: check that the timestamps make sense
-	
-	
-	
 	# open and write the output file
-	outFile = open("SCADA_weather_"+airport+"_gld.csv", "w")
+	outFile = open(pJoin(workDir,"weather.csv"), "w")
 	# write header
-	outFile.write('#DUKE weather file\n');
+	outFile.write('#weather file\n');
 	outFile.write('$state_name=North Carolina\n');
 	outFile.write('$city_name=Charlotte\n');
 	outFile.write('$lat_deg=35\n');
@@ -582,24 +705,16 @@ def ProcessWeather(start, end, airport='', wdir='.', interpolate="linear"):
 		pass
 	# clean up and exit
 	outFile.close()
-	
 	return 0
 
-def GenerateWeatherFiles(start, end, airport, wdir=''):
-	rv = ExtractWeather.GetWeather(start, end, airport, wdir)
-	if rv != None:
-		rv2 = ProcessWeather(start, end, airport, wdir)
-		if rv2 == None:
-			print("ERROR: ProcessWeather({}, {}, {}, {}) failed!".format(start, end, airport, wdir))
-			return None
-		return 0
-	else:
-		print("ERROR: ExtractWeather.GetWeather({}, {}, {}, {}) failed!".format(start, end, airport, wdir))
-		return None
-	
-if __name__ is '__main__':
-	ProcessWeather('06-01-2010', '07-01-2010', airport="PDX")
+def _tests():
+	# UNIT TESTS
+	#TODO: test _airportCodeToLatLon
+	# _downloadWeather("2010-03-01", "2010-04-01", airport="PDX")
+	# _getPeakSolar("PDX", dniScale=1.0, dhiScale=1.0, ghiScale=1.0)
+	# _processWeather("2010-03-01", "2010-04-01", airport="PDX")
+	# BLACKBOX TEST
+	makeClimateCsv("2010-07-01", "2010-08-01", "DCA", pJoin(tempfile.mkdtemp(),"weatherDCA.csv"), cleanup=True)
 
-#
-# EOF
-#
+if __name__ == "__main__":
+	_tests()
