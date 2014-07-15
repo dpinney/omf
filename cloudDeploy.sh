bbf8cef7cd21de47b60534bc3c18d054c59fb766
dpinney <david.pinney@nreca.coop>
2014-07-15 12:06:51 -0400

    Check for root, backup data, reset to purge .py changes.

--- cloudDeploy.sh
+++ cloudDeploy.sh
@@ -1,5 +1,11 @@
 # Update this OMF install to the latest from the source tree.
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+echo "** Backing up data folder."
+tar -czf ~/dataBackup.tgz /omf/omf/data
 echo "** Pulling the latest source from git."
+git reset --hard
 git pull
 echo "** Setting permissions."
 chown -R omfwsgi *
