590c57fc3b37b57c964a8d6929883cf5e5e80060
dpinney <david.pinney@nreca.coop>
2014-07-15 11:51:00 -0400

    Updates to cloudDeploy.sh.

--- cloudDeploy.sh
+++ cloudDeploy.sh
@@ -1,6 +1,5 @@
 # Update this OMF install to the latest from the source tree.
-echo "** Resetting to head and pulling the latest source from git."
-git reset --hard
+echo "** Pulling the latest source from git."
 git pull
 echo "** Setting permissions."
 chown -R omfwsgi *
