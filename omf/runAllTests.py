536911d66239a05837f31574827a2198a901764e
dpinney <david.pinney@nreca.coop>
2014-07-15 18:23:56 -0400

    A little cleanup and notification to the user what's going on.

--- omf/runAllTests.py
+++ omf/runAllTests.py
@@ -1,24 +1,11 @@
 ''' Walk the /omf/ directory, run _tests() in all modules. '''
 
-import os, sys, subprocess
+import os, sys, subprocess, imp
+from os.path import join as pJoin
+from os.path import split as pSplit
 
-IGNORE_DIRS = [
-	"calibration",
-	"data",
-	".git",
-	"static",
-	"templates",
-	"tests",
-	"uploads",
-	"venv",
-	"scratch"]
-
-# Useful for debugging this script (runAllTests.py) specifically.
-# milToGridlab.py, for example, usually does not fail, but running its tests
-# takes a long time.
-IGNORE_FILES = [
-	#"milToGridlab.py", 
-	"runAllTests.py" ] # to avoid infinite loop.
+IGNORE_DIRS = ["data", ".git", "static", "templates", "uploads", "scratch"]
+IGNORE_FILES = ["runAllTests.py"] # to avoid infinite loop.
 
 def runAllTests(startingdir):
 	os.chdir(startingdir)
@@ -27,6 +14,7 @@ def runAllTests(startingdir):
 	misfires = {}
 	for item in os.listdir("."):
 		if item not in IGNORE_FILES and item.endswith(".py") and "def _tests():" in open(item).read():
+			print "********** TESTING", item, "************"
 			p = subprocess.Popen(["python", item], stderr=subprocess.PIPE)
 			p.wait()
 			if p.returncode:
@@ -40,7 +28,7 @@ def runAllTests(startingdir):
 		misfires.update(m)
 	return the_errors, misfires
 
-if __name__ == "__main__":
+def testRunner():
 	os.chdir(sys.argv[1] if len(sys.argv) > 1 else ".") # You can just run tests in a specific dir if you want
 	i, mis = runAllTests(os.getcwd())
 	print "\n\n+------------------------+"
@@ -50,3 +38,6 @@ if __name__ == "__main__":
 	for fname, err in mis.items():
 		print fname
 		print err, "\n\n"
+
+if __name__ == "__main__":
+	testRunner()
