44b0183b785f042b7e428c782a0e641f02d905fd
Srivats Shukla <srivatsshukla@gmail.com>
2014-07-21 11:59:20 -0400

    calibrate.py in skunkworks
    1) added functions format in the file
    2) added script to automatically search for substation regulator instead
    of sending the name default
    3) added plot functions for output wrt input

--- omf/scratch/calibrateSkunkworks/calibrate.py
+++ omf/scratch/calibrateSkunkworks/calibrate.py
@@ -1,109 +1,156 @@
 import csv, datetime as dt, json, subprocess
-import omf
 from matplotlib import pyplot as plt
+import os, re, sys
 
-SCADA_FNAME = "colScada.tsv"
-PLAYER_FNAME = "scada.player"
-FEEDER_FNAME = "ABEC Columbia.json"
-SUB_REG_NAME = "Coloma Bus Regs"
+# Locational variables so we don't have to rely on OMF being in the system path.
+_myDir = os.path.dirname(os.path.abspath(__file__))
+_omfDir = os.path.dirname(_myDir)
+print _omfDir
+sys.path.append(_omfDir)
+
+# OMF imports
+sys.path.append(_omfDir)
+import feeder
+import feederCalibrate
+from solvers import gridlabd
+
 
 # Get SCADA data.
-with open(SCADA_FNAME,"r") as scadaFile:
-	scadaReader = csv.DictReader(scadaFile, delimiter='\t')
-	allData = [row for row in scadaReader]
+def getScadaData(SCADA_FNAME,PQPLAYER_FNAME,VPLAYER_FNAME=None):
+	'''generate a SCADA player file from raw SCADA data'''
+	with open(SCADA_FNAME,"r") as scadaFile:
+		scadaReader = csv.DictReader(scadaFile, delimiter='\t')
+		allData = [row for row in scadaReader]
 
-# Write the player.
-maxPower = max([float(row["power"]) for row in allData])
-with open(PLAYER_FNAME,"w") as playFile:
-	for row in allData:
-		timestamp = dt.datetime.strptime(row["timestamp"], "%m/%d/%Y %H:%M:%S")
-		power = float(row["power"]) / maxPower
-		line = timestamp.strftime("%Y-%m-%d %H:%M:%S") + " PST," + str(power) + "\n"
-		playFile.write(line)
+	inputData = [float(row["power"]) for row in allData]
+
+	# Write the player.
+	maxPower = max(inputData)
+	with open(PQPLAYER_FNAME,"w") as playFile:
+		for row in allData:
+			timestamp = dt.datetime.strptime(row["timestamp"], "%m/%d/%Y %H:%M:%S")
+			power = float(row["power"]) / maxPower
+			line = timestamp.strftime("%Y-%m-%d %H:%M:%S") + " PST," + str(power) + "\n"
+			playFile.write(line)
+
+	return inputData
 
 # Get tree.
-jsonIn = json.load(open(FEEDER_FNAME))
-tree = jsonIn.get("tree", {})
+def _calibrateFeeder(inputData,FEEDER_FNAME,PQPLAYER_FNAME,VPLAYER_FNAME=None):
+	'''calibrates a feeder and saves the calibrated tree at a location'''
+	jsonIn = json.load(open(FEEDER_FNAME))
+	tree = jsonIn.get("tree", {})
 
-# Attach player.
-classOb = {"class":"player", "variable_names":["value"], "variable_types":["double"]}
-playerOb = {"object":"player", "property":"value", "name":"scadaLoads", "file":PLAYER_FNAME, "loop":"0"}
-maxKey = omf.feeder.getMaxKey(tree)
-tree[maxKey+1] = classOb
-tree[maxKey+2] = playerOb
+	# Attach player.
+	classOb = {"class":"player", "variable_names":["value"], "variable_types":["double"]}
+	playerOb = {"object":"player", "property":"value", "name":"scadaLoads", "file":PQPLAYER_FNAME, "loop":"0"}
+	maxKey = feeder.getMaxKey(tree)
+	tree[maxKey+1] = classOb
+	tree[maxKey+2] = playerOb
 
-# Make loads reference player.
-loadTemplate = {"object": "triplex_load",
+	# Make loads reference player.
+	loadTemplate = {"object": "triplex_load",
 	"power_pf_12": "0.95",
 	"impedance_pf_12": "0.98",
 	"power_pf_12": "0.90",
 	"impedance_fraction_12": "0.7",
 	"power_fraction_12": "0.3"}
-for key in tree:
-	ob = tree[key]
-	if ob.get("object","") == "triplex_node" and ob.get("power_12","") != "":
-		newOb = dict(loadTemplate)
-		newOb["name"] = ob.get("name", "")
-		newOb["parent"] = ob.get("parent", "")
-		newOb["phases"] = ob.get("phases", "")
-		newOb["nominal_voltage"] = ob.get("nominal_voltage","")
-		newOb["latitude"] = ob.get("latitude","")
-		newOb["longitude"] = ob.get("longitude","")
-		oldPow = ob.get("power_12","").replace("j","d")
-		pythagPower = omf.solvers.gridlabd._strClean(oldPow)
-		newOb["base_power_12"] = "scadaLoads.value*" + str(pythagPower)
-		tree[key] = newOb
 
-# Give it a test run.
-recOb = {"object": "recorder",
+	for key in tree:
+		ob = tree[key]
+		if ob.get("object","") == "triplex_node" and ob.get("power_12","") != "":
+			newOb = dict(loadTemplate)
+			newOb["name"] = ob.get("name", "")
+			newOb["parent"] = ob.get("parent", "")
+			newOb["phases"] = ob.get("phases", "")
+			newOb["nominal_voltage"] = ob.get("nominal_voltage","")
+			newOb["latitude"] = ob.get("latitude","")
+			newOb["longitude"] = ob.get("longitude","")
+			oldPow = ob.get("power_12","").replace("j","d")
+			pythagPower = gridlabd._strClean(oldPow)
+			newOb["base_power_12"] = "scadaLoads.value*" + str(pythagPower)
+			tree[key] = newOb
+
+	#search for the substation regulator
+	for key in tree:
+		if tree[key].get('bustype','').lower() == 'swing':
+			swingIndex = key
+			swingName = tree[key].get('name')
+
+	for key in tree:
+		if tree[key].get('object','') == 'regulator' and tree[key].get('from','') == swingName:
+			regIndex = key
+			SUB_REG_NAME = tree[key]['name']
+
+
+	# Give it a test run.
+	recOb = {"object": "recorder",
 	"parent": SUB_REG_NAME,
 	"property": "power_in.real, power_in.imag",
 	"file": "outPower.csv",
 	"interval": "900"}
-tree[maxKey + 3] = recOb
-for key in tree.keys():
-	try:
-		del tree[key]["latitude"]
-		del tree[key]["longitude"]
-	except:
-		pass # No lat lons.
-omf.feeder.adjustTime(tree, 100, "hours", "2011-01-01")
-with open("out.glm","w") as outGlm:
-	outGlm.write(omf.feeder.sortedWrite(tree))
-proc = subprocess.Popen(['gridlabd', "-w", "out.glm"])
-proc.wait()
 
-# Do some plotting.
-# plt.plot([float(row["power"]) for row in allData[:1000]])
-# plt.show()
+	tree[maxKey + 3] = recOb
+	for key in tree.keys():
+		try:
+			del tree[key]["latitude"]
+			del tree[key]["longitude"]
+		except:
+			pass # No lat lons.
 
-# WHAT NEXT? csv to table format.
+	feeder.adjustTime(tree, 100, "hours", "2011-01-01")
+	with open("out.glm","w") as outGlm:
+		outGlm.write(feeder.sortedWrite(tree))
+	proc = subprocess.Popen(['gridlabd', "-w", "out.glm"])
+	proc.wait()
 
 
-'''
+	#calculate scaling constant here
+	SCAL_CONST = 300.0
 
-object triplex_node {
-	phases BS;
-	power_12 8845.18+4792.29j;
-	name S1807-25-012_B;
-	parent nodeS1807-25-0111807-25-003_B;
-	longitude 263.276266686;
-	nominal_voltage 120;
-	latitude 605.788600445;
-};
+	# Do some plotting.
+	listdict = []
+	with open("outPower.csv","r") as outcsvFile:
+		for i,line in enumerate(outcsvFile):
+			if i > 8:
+				listdict.append(line)
 
-object triplex_load {
-	phases BS;
-	name 99330306_B_loadshape;
-	parent node99330306T99330306_B;
-	power_pf_12 residential_power_pfB.value*0.946486;
-	impedance_pf_12 0.970000;
-	current_pf_12 0.970000;
-	impedance_fraction_12 0.000000;
-	current_fraction_12 0.000000;
-	nominal_voltage 120.0;
-	power_fraction_12 1.000000;
-	base_power_12 norm_feeder_loadshape.value*3291.537058;
-};
+	tempdata = []
+	for row in listdict:
+		tempdata.append(row.split(','))
 
-'''
+
+	powerdata = []
+	for rowt in tempdata:
+		powerdata.append(float(rowt[1])/SCAL_CONST)
+
+	plt.plot
+	plt.plot(range(len(powerdata)), powerdata,range(len(powerdata)),inputData[:len(powerdata)])
+	plt.show()
+
+	return None
+
+def omfCalibrate(FEEDER_FNAME,SCADA_FNAME,PQPLAYER_FNAME,VPLAYER_FNAME=None):
+	'''calls _calibrateFeeder and gets the work done'''
+	inputData = getScadaData(SCADA_FNAME,PQPLAYER_FNAME,VPLAYER_FNAME) 
+	_calibrateFeeder(inputData,FEEDER_FNAME,PQPLAYER_FNAME,VPLAYER_FNAME) #passing input data for scaling 
+
+	return None
+
+
+def _tests():
+	'''test function for ABEC Coloma and Frank feeders'''
+	SCADA_FNAME = "colScada.tsv"
+	PQPLAYER_FNAME = "scada.player"
+	FEEDER_FNAME = "ABEC Frank Lo.json"
+	#SUB_REG_NAME = "REG27"
+
+	omfCalibrate(FEEDER_FNAME,SCADA_FNAME,PQPLAYER_FNAME)
+
+	return None
+
+
+
+if __name__ == '__main__':
+	'''runs certain test functions for feeder calibration'''
+	_tests()
