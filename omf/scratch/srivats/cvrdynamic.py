5926e11ed5bba4e3eb314cd1cee57d8c29633980
Srivats Shukla <srivatsshukla@gmail.com>
2014-07-28 14:39:05 -0400

    cvrdynamic - required features
    -saving all the figures in workdir
    -reading tap positions and cap switch states

--- omf/scratch/srivats/cvrdynamic.py
+++ omf/scratch/srivats/cvrdynamic.py
@@ -19,6 +19,25 @@ import feeder
 import calibrate
 from solvers import gridlabd
 
+def realPart(complexStr):
+	''' real Part of a complex number.
+	handles string if the string starts with a '+' or a '-'
+	handles negative or positive, real and imaginary parts 
+	example realPart(-20.5-6j) = -20.5'''
+	if complexStr[0] == '+' or complexStr[0] == '-':
+		complexStr1 = complexStr[1:len(complexStr)+1]
+		sign = complexStr[0] + '1' 
+	else:
+		complexStr1 = complexStr
+		sign = 1
+	if complexStr1.find('+')>0:
+		return float(complexStr1[0:complexStr1.find('+')])*float(sign)
+	else:
+		if complexStr1.find('-')>0:
+			return float(complexStr1[0:complexStr1.find('-')])*float(sign)
+		else:
+			return float(complexStr1)*float(sign)
+
 def runModel(modelDir,localTree):
 	'''This reads a glm file, changes the method of powerflow and reruns'''
 	print "Testing GridlabD solver."
@@ -36,6 +55,23 @@ def runModel(modelDir,localTree):
 		if localTree[key].get('object','') == 'regulator' and localTree[key].get('from','') == swingName:
 			regIndex = key
 			regConfName = localTree[key]['configuration']
+	#find the regulator and capacitor names and combine to form a string for volt-var control object
+	regKeys = []
+	accum_reg = ""
+	for key in localTree:
+		if localTree[key].get("object","") == "regulator":
+			accum_reg += localTree[key].get("name","ERROR") + ","
+			regKeys.append(key)
+	regstr = accum_reg[:-1]
+	print regKeys
+	capKeys = []
+	accum_cap = ""
+	for key in localTree:
+		if localTree[key].get("object","") == "capacitor":
+			accum_cap += localTree[key].get("name","ERROR") + ","
+			capKeys.append(key)
+	capstr = accum_cap[:-1]
+	print capKeys
 	# Attach recorders relevant to CVR.
 	recorders = [
 			{'object': 'collector',
@@ -73,33 +109,25 @@ def runModel(modelDir,localTree):
 			'limit': '0',
 			'parent': localTree[regIndex]['to'],
 			'property': 'voltage_A,voltage_B,voltage_C'}]
+	#recorder object for capacitor switching - if capacitors exist
+	if capKeys != []:
+		for key in capKeys:
+			recorders.append({'object': 'recorder',
+			'file': 'ZcapSwitch' + str(key) + '.csv',
+			'limit': '0',
+			'parent': localTree[key]['name'],
+			'property': 'switchA,switchB,switchC'})
+	#attach recorder process
 	biggest = 1 + max([int(k) for k in localTree.keys()])
 	for index, rec in enumerate(recorders):
 		localTree[biggest + index] = rec
 	#run a reference load flow
-	HOURS = float(100)
+	HOURS = float(500)
 	feeder.adjustTime(localTree,HOURS,"hours","2011-01-01")	
 	output = gridlabd.runInFilesystem(localTree,keepFiles=False,workDir=modelDir)
 	os.remove(pJoin(modelDir,"PID.txt"))
 	p = output['Zregulator.csv']['power_in.real']
 	q = output['Zregulator.csv']['power_in.imag']
-	#find the regulator and capacitor names and combine to form a string for volt-var control object
-	regKeys = []
-	accum_reg = ""
-	for key in localTree:
-		if localTree[key].get("object","") == "regulator":
-			accum_reg += localTree[key].get("name","ERROR") + ","
-			regKeys.append(key)
-	regstr = accum_reg[:-1]
-	print regKeys
-	capKeys = []
-	accum_cap = ""
-	for key in localTree:
-		if localTree[key].get("object","") == "capacitor":
-			accum_cap += localTree[key].get("name","ERROR") + ","
-			capKeys.append(key)
-	capstr = accum_cap[:-1]
-	print capKeys
 	#time delays from configuration files
 	time_delay_reg = '30.0'  
 	time_delay_cap = '300.0'
@@ -135,10 +163,6 @@ def runModel(modelDir,localTree):
 	imagLoss = []
 	realLossnew = []
 	imagLossnew = []
-	subvolt = []
-	lowVoltage = []
-	highVoltage = []
-	lowVoltagenew = []
 	for element in range(int(HOURS)):
 		r = 0.0
 		i = 0.0
@@ -154,43 +178,73 @@ def runModel(modelDir,localTree):
 		imagLoss.append(i)
 		realLossnew.append(rnew)
 		imagLossnew.append(inew)
-		lowVoltage.append(float(output['ZvoltageJiggle.csv']['min(voltage_12.mag)'][element])/2.0)
-		highVoltage.append(float(output['ZvoltageJiggle.csv']['max(voltage_12.mag)'][element])/2.0)
-		lowVoltagenew.append(float(output1['ZvoltageJiggle.csv']['min(voltage_12.mag)'][element])/2.0)
-	#tap positions
+	#voltage calculations and tap calculations
+	lowVoltage = []
+	highVoltage = []
+	lowVoltagenew = []
 	tap = {'A':[],'B':[],'C':[]}
 	tapnew = {'A':[],'B':[],'C':[]}
+	volt = {'A':[],'B':[],'C':[]}
+	voltnew = {'A':[],'B':[],'C':[]}
+	switch = {'A':[],'B':[],'C':[]}
+	switchnew = {'A':[],'B':[],'C':[]}
 	for element in range(int(HOURS)):
 		for letter in ['A','B','C']:
 			tap[letter].append(output['Zregulator.csv']['tap_' + letter][element])
-			tapnew[letter].append(output1['Zregulator.csv']['tap_' + letter][element])	
+			tapnew[letter].append(output1['Zregulator.csv']['tap_' + letter][element])
+			volt[letter].append(realPart(output['ZsubstationBottom.csv']['voltage_'+letter][element])/60)
+			voltnew[letter].append(realPart(output1['ZsubstationBottom.csv']['voltage_'+letter][element])/60)
+			switch[letter].append(output['ZcapSwitch' + str(int(capKeys[0])) + '.csv']['switch'+ letter][element])
+			switchnew[letter].append(output1['ZcapSwitch' + str(int(capKeys[0])) + '.csv']['switch'+ letter][element])
+		lowVoltage.append(float(output['ZvoltageJiggle.csv']['min(voltage_12.mag)'][element])/2.0)
+		highVoltage.append(float(output['ZvoltageJiggle.csv']['max(voltage_12.mag)'][element])/2.0)
+		lowVoltagenew.append(float(output1['ZvoltageJiggle.csv']['min(voltage_12.mag)'][element])/2.0)
 	#for debug
 	plt.clf()
-	plt.plot(p,label="substation real power without regulation")
-	plt.plot(pnew,label="substation real power with regulation")
+	plt.plot(p,label="substation real power without IVVC")
+	plt.plot(pnew,label="substation real power with IVVC")
 	plt.legend(loc=1)
+	plt.savefig(pJoin(modelDir,"real power.png"))
 	plt.figure("imaginary power")
-	plt.plot(q,label="substation imag. power without regulation")
-	plt.plot(qnew,label="substation imag. power with regulation")
+	plt.plot(q,label="substation imag. power without IVVC")
+	plt.plot(qnew,label="substation imag. power with IVVC")
 	plt.legend(loc=1)
+	plt.savefig(pJoin(modelDir,"imaginary power.png"))
 	plt.figure("real losses")
-	plt.plot(realLoss,label="total losses real without regulation")
-	plt.plot(realLossnew,label="total losses real with regulation")
+	plt.plot(realLoss,label="total losses real without IVVC")
+	plt.plot(realLossnew,label="total losses real with IVVC")
 	plt.legend(loc=1)
+	plt.savefig(pJoin(modelDir,"real losses.png"))
 	plt.figure("imaginary losses")
-	plt.plot(imagLoss,label="total losses imag. without regulation")
-	plt.plot(imagLossnew,label="total losses imag. with regulation")
+	plt.plot(imagLoss,label="total losses imag. without IVVC")
+	plt.plot(imagLossnew,label="total losses imag. with IVVC")
 	plt.legend(loc=1)
+	plt.savefig(pJoin(modelDir,"imaginary losses.png"))
 	plt.figure("lowVoltage voltage as a function of time")
-	plt.plot(lowVoltage,label="EOL voltage without regulation as a function of time")
-	plt.plot(lowVoltagenew, label="EOL voltage with regulation as a function of time")
+	plt.plot(lowVoltage,label="EOL voltage without IVVC as a function of time")
+	plt.plot(lowVoltagenew,label="EOL voltage with IVVC as a function of time")
 	plt.legend(loc=1)
-	plt.show()
+	plt.axis([0,HOURS,50.0,250.0])
+	plt.savefig(pJoin(modelDir,"EOL voltage.png"))
 	plt.figure("TAP positions as a function of time")
-	plt.plot(tap['C'],label="TAP C without regulation as a function of time")
-	plt.plot(tapnew['C'], label="TAP C with regulation as a function of time")
-	plt.legend(loc=3)
-	plt.show()
+	plt.plot(tap['A'],label="TAP A without IVVC as a function of time")
+	plt.plot(tapnew['A'], label="TAP A with IVVC as a function of time")
+	plt.legend(loc=1)
+	plt.axis([0,HOURS,-20.0,20.0])
+	plt.savefig(pJoin(modelDir,"TAP A positions.png"))
+	plt.figure("substation voltage as a function of time")
+	plt.plot(volt['A'],label="sub. volt. A without IVVC as a function of time")
+	plt.plot(voltnew['A'],label="sub. volt. A with IVVC as a function of time")
+	plt.legend(loc=1)
+	plt.axis([0,HOURS,50.0,250.0])
+	plt.savefig(pJoin(modelDir,"substation volatage A.png"))
+	plt.figure("capacitor switch state as a function of time")
+	plt.plot(switch['A'],label="cap. switch A without IVVC as a function of time")
+	plt.plot(switchnew['A'],label="cap. switch A with IVVC as a function of time")
+	plt.legend(loc=1)
+	plt.axis([0,HOURS,-2.0,2.0])
+	plt.savefig(pJoin(modelDir,"capacitor switch A.png"))
+	#plt.show()
 	print "DONE RUNNING", modelDir
 
 if __name__ == '__main__':
