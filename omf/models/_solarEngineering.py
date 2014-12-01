2858d2ea7906d0f4d89b3c0931a0a1d224823185
Hongwei Jin <jinhw1989@gmail.com>
2014-12-01 12:40:14 -0600

    Enable climate data

--- omf/models/_solarEngineering.py
+++ omf/models/_solarEngineering.py
@@ -20,34 +20,17 @@ def renderTemplate(template, modelDir="", absolutePaths=False, datastoreNames={}
 	return __metaModel__.renderTemplate(template, modelDir, absolutePaths, datastoreNames)
 
 def run(modelDir, inputDict):
-	# ''' Run the model in a separate process. web.py calls this to run the model.
-	# This function will return fast, but results take a while to hit the file system.'''
-	# # Check whether model exist or not
-	# if not os.path.isdir(modelDir):
-	# 	os.makedirs(modelDir)
-	# 	inputDict["created"] = str(datetime.datetime.now())
-	# # MAYBEFIX: remove this data dump. Check showModel in web.py and renderTemplate()
-	# with open(pJoin(modelDir, "allInputData.json"),"w") as inputFile:
-	# 	json.dump(inputDict, inputFile, indent = 4)
-	# # If we are re-running, remove output:
-	# try:
-	# 	os.remove(pJoin(modelDir,"allOutputData.json"))
-	# except:
-	# 	pass
-	# backProc = multiprocessing.Process(target = runForeground, args = (modelDir, inputDict,))
-	# backProc.start()
-	# print "SENT TO BACKGROUND", modelDir
-	# with open(pJoin(modelDir, "PPID.txt"),"w+") as pPidFile:
-	# 	pPidFile.write(str(backProc.pid))
-	# 	
 	''' Run the model in its directory. '''
 	# Check whether model exist or not
 	if not os.path.isdir(modelDir):
 		os.makedirs(modelDir)
 		inputDict["created"] = str(datetime.datetime.now())
+	feederDir, feederName = inputDict['feederName'].split("___")
 	# MAYBEFIX: remove this data dump. Check showModel in web.py and renderTemplate()
 	with open(pJoin(modelDir, "allInputData.json"),"w") as inputFile:
 		json.dump(inputDict, inputFile, indent = 4)
+	shutil.copy(pJoin(__metaModel__._omfDir, "data", "Feeder", feederDir, feederName + ".json"),
+				pJoin(modelDir, "feeder.json"))
 	# Copy spcific climate data into model directory
 	shutil.copy(pJoin(__metaModel__._omfDir, "data", "Climate", inputDict["climateName"] + ".tmy2"), 
 		pJoin(modelDir, "climate.tmy2"))
@@ -56,16 +39,44 @@ def run(modelDir, inputDict):
 	simStartDate = inputDict.get("simStartDate","")
 	startDateTime = simStartDate + " 00:00:00 UTC"
 	startTime = datetime.datetime.now()
+	
 	###############################
 	# TODO: INSERT RUNNING CODE HERE
+	# RUN GRIDLABD IN FILESYSTEM (EXPENSIVE!)
+	feederJson = json.load(open(pJoin(modelDir, "feeder.json")))
+	tree = feederJson["tree"]
+	# Set up GLM with correct time and recorders:
+	feeder.attachRecorders(tree, "Regulator", "object", "regulator")
+	feeder.attachRecorders(tree, "Capacitor", "object", "capacitor")
+	feeder.attachRecorders(tree, "Inverter", "object", "inverter")
+	feeder.attachRecorders(tree, "Windmill", "object", "windturb_dg")
+	feeder.attachRecorders(tree, "CollectorVoltage", None, None)
+	feeder.attachRecorders(tree, "Climate", "object", "climate")
+	feeder.attachRecorders(tree, "OverheadLosses", None, None)
+	feeder.attachRecorders(tree, "UndergroundLosses", None, None)
+	feeder.attachRecorders(tree, "TriplexLosses", None, None)
+	feeder.attachRecorders(tree, "TransformerLosses", None, None)
+	feeder.groupSwingKids(tree)
+	feeder.adjustTime(tree=tree, simLength=float(inputDict["simLength"]),
+		simLengthUnits=inputDict["simLengthUnits"], simStartDate=inputDict["simStartDate"])
+	rawOut = gridlabd.runInFilesystem(tree, attachments=feederJson["attachments"], 
+		keepFiles=True, workDir=modelDir)
 	###############################
 	# Timestamp output.
 	outData = {}
 	outData["timeStamps"] = [datetime.datetime.strftime(
 		datetime.datetime.strptime(startDateTime[0:19],"%Y-%m-%d %H:%M:%S") + 
 		datetime.timedelta(**{simLengthUnits:x}),"%Y-%m-%d %H:%M:%S") + " UTC" for x in range(int(inputDict["simLength"]))]
+	level = inputDict.get('simLengthUnits','hours')
 	# Weather output.
-	outData["climate"] = {}
+	for key in rawOut:
+		if key.startswith('Climate_') and key.endswith('.csv'):
+			outData['climate'] = {}
+			outData['climate']['Rain Fall (in/h)'] = hdmAgg(rawOut[key].get('rainfall'), sum, level)
+			outData['climate']['Wind Speed (m/s)'] = hdmAgg(rawOut[key].get('wind_speed'), avg, level)
+			outData['climate']['Temperature (F)'] = hdmAgg(rawOut[key].get('temperature'), max, level)
+			outData['climate']['Snow Depth (in)'] = hdmAgg(rawOut[key].get('snowdepth'), max, level)
+			outData['climate']['Direct Insolation (W/m^2)'] = hdmAgg(rawOut[key].get('solar_direct'), sum, level)
 	# TODO: Stdout/stderr.
 	# Write the output.
 	with open(pJoin(modelDir,"allOutputData.json"),"w") as outFile:
@@ -84,11 +95,46 @@ def cancel(modelDir):
 	''' TODO: INSERT CANCEL CODE HERE '''
 	pass
 
+def avg(inList):
+	''' Average a list. Really wish this was built-in. '''
+	return sum(inList)/len(inList)
+
+def hdmAgg(series, func, level):
+	''' Simple hour/day/month aggregation for Gridlab. '''
+	if level in ['days','months']:
+		return aggSeries(stamps, series, func, level)
+	else:
+		return series
+
+def _aggData(key, aggFun, simStartDate, simLength, simLengthUnits, ssc, dat):
+	''' Function to aggregate output if we need something other than hour level. '''
+	u = simStartDate
+	# pick a common year, ignoring the leap year, it won't affect to calculate the initHour
+	d = datetime.datetime(2013, int(u[5:7]),int(u[8:10])) 
+	# first day of the year	
+	sd = datetime.datetime(2013, 01, 01) 
+	# convert difference of datedelta object to number of hours 
+	initHour = int((d-sd).total_seconds()/3600)
+	fullData = ssc.ssc_data_get_array(dat, key)
+	if simLengthUnits == "days":
+		multiplier = 24
+	else:
+		multiplier = 1
+	hourData = [fullData[(initHour+i)%8760] for i in xrange(simLength*multiplier)]
+	if simLengthUnits == "minutes":
+		pass
+	elif simLengthUnits == "hours":
+		return hourData
+	elif simLengthUnits == "days":
+		split = [hourData[x:x+24] for x in xrange(simLength)]
+		return map(aggFun, split)
+
 def _tests():
 	# Variables
 	workDir = pJoin(__metaModel__._omfDir,"data","Model")
 	inData = {"simStartDate": "2012-04-01",
 		"simLengthUnits": "hours",
+		"feederName": "public___Olin Barre Geo",
 		"modelType": "_solarEngineering",
 		"climateName": "AL-HUNTSVILLE",
 		"simLength": "100",
