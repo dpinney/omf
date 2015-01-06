805716bad5c4875824a730864fe40c9c8111b78f
David Pinney <dpinney@me.com>
2015-01-06 18:44:19 -0500

    Group_recorders added. We have a problem though; omf.feeder.parse can't handle 'class player {int blah}'.

--- omf/scratch/voltdumpDebug/timeTravelGen.py
+++ omf/scratch/voltdumpDebug/timeTravelGen.py
@@ -1,15 +1,24 @@
-''' Grab Simple Market System from our data store and write out the glms for it. '''
+''' Working towards a timeTravel chart. '''
 import omf, json, os
 
 def feedJsonToFiles():
-	feedJson = json.load(open("./ABEC Frank Calibrated.json"))
-	with open("AVEC Frank Calibrated.glm","w") as outFile:
-		outFile.write(omf.feeder.sortedWrite(feedJson["tree"]))
-	for name in feedJson["attachments"].keys():
-		with open(name, "w") as outFile:
-			outFile.write(feedJson["attachments"][name])
+	''' Convert our JSON feeder into GLMs and stuff. '''
+	feedJson = json.load(open('./ABEC Frank Calibrated.json'))
+	with open('ABEC Frank Calibrated.glm','w') as outFile:
+		# Add recorders here.
+		stub = {'object':'group_recorder', 'group':'"class=node"', 'property':'voltage_A', 'interval':3600, 'file':'aVoltDump.csv'}
+		for phase in ['A','B','C']:
+			copyStub = dict(stub)
+			copyStub['property'] = 'voltage_' + phase
+			copyStub['file'] = phase.lower() + 'VoltDump.csv'
+			feedJson['tree'][omf.feeder.getMaxKey(feedJson['tree']) + 1] = copyStub
+		# Then write.
+		outFile.write(omf.feeder.sortedWrite(feedJson['tree']))
+	for name in feedJson['attachments'].keys():
+		with open(name, 'w') as outFile:
+			outFile.write(feedJson['attachments'][name])
 
 if __name__ == '__main__':
 	feedJsonToFiles()
-
-# NOTE: you need to put a tmy file named "climate.tmy2" in this folder.
+	#addGroupRecorders()
+	pass
