5543499749a03929293ed63cfad4c4d4c877a153
dpinney <david.pinney@nreca.coop>
2015-01-08 09:26:06 -0500

    GLD solver running.

--- omf/scratch/voltdumpDebug/timeTravelGen.py
+++ omf/scratch/voltdumpDebug/timeTravelGen.py
@@ -1,23 +1,21 @@
 ''' Working towards a timeTravel chart. '''
 import omf, json, os
+from omf.solvers.gridlabd import runInFilesystem, anaDataTree
 
-def feedJsonToFiles():
-	''' Convert our JSON feeder into GLMs and stuff. '''
+def main():
+	''' JSON manipulation, Gridlab running, etc. goes here. '''
 	feedJson = json.load(open('./ABEC Frank Calibrated.json'))
-	with open('ABEC Frank Calibrated.glm','w') as outFile:
-		# Add recorders here.
-		stub = {'object':'group_recorder', 'group':'"class=node"', 'property':'voltage_A', 'interval':3600, 'file':'aVoltDump.csv'}
-		for phase in ['A','B','C']:
-			copyStub = dict(stub)
-			copyStub['property'] = 'voltage_' + phase
-			copyStub['file'] = phase.lower() + 'VoltDump.csv'
-			feedJson['tree'][omf.feeder.getMaxKey(feedJson['tree']) + 1] = copyStub
-		# Then write.
-		outFile.write(omf.feeder.sortedWrite(feedJson['tree']))
-	for name in feedJson['attachments'].keys():
-		with open(name, 'w') as outFile:
-			outFile.write(feedJson['attachments'][name])
+	# Add recorders here.
+	stub = {'object':'group_recorder', 'group':'"class=node"', 'property':'voltage_A', 'interval':3600, 'file':'aVoltDump.csv'}
+	for phase in ['A','B','C']:
+		copyStub = dict(stub)
+		copyStub['property'] = 'voltage_' + phase
+		copyStub['file'] = phase.lower() + 'VoltDump.csv'
+		feedJson['tree'][omf.feeder.getMaxKey(feedJson['tree']) + 1] = copyStub
+	# Run gridlab.
+	allOutputData = runInFilesystem(feedJson['tree'], attachments=feedJson['attachments'], keepFiles=True, workDir=".")
+	print allOutputData.keys()
 
 if __name__ == '__main__':
-	feedJsonToFiles()
+	main()
 	pass
