73dd87f9ed79fd48aa8df4e82fad3fa0c105344c
mannanj <mannanj90@gmail.com>
2015-01-21 11:47:35 -0500

    PVwatts exceptions all caught now.

--- omf/models/pvWatts.py
+++ omf/models/pvWatts.py
@@ -20,92 +20,106 @@ def renderTemplate(template, modelDir="", absolutePaths=False, datastoreNames={}
 
 def run(modelDir, inputDict):
 	''' Run the model in its directory. '''
+	# Delete output file every run if it exists
+	try:
+		os.remove(pJoin(modelDir,"allOutputData.json"))	
+	except Exception, e:
+		pass
 	# Check whether model exist or not
-	if not os.path.isdir(modelDir):
-		os.makedirs(modelDir)
-		inputDict["created"] = str(datetime.datetime.now())
-	# MAYBEFIX: remove this data dump. Check showModel in web.py and renderTemplate()
-	with open(pJoin(modelDir, "allInputData.json"),"w") as inputFile:
-		json.dump(inputDict, inputFile, indent = 4)
-	# Copy spcific climate data into model directory
-	shutil.copy(pJoin(__metaModel__._omfDir, "data", "Climate", inputDict["climateName"] + ".tmy2"), 
-		pJoin(modelDir, "climate.tmy2"))
-	# Ready to run
-	startTime = datetime.datetime.now()
-	# Set up SAM data structures.
-	ssc = nrelsam.SSCAPI()
-	dat = ssc.ssc_data_create()
-	# Required user inputs.
-	ssc.ssc_data_set_string(dat, "file_name", modelDir + "/climate.tmy2")
-	ssc.ssc_data_set_number(dat, "system_size", float(inputDict["systemSize"]))
-	ssc.ssc_data_set_number(dat, "derate", float(inputDict["derate"]))
-	ssc.ssc_data_set_number(dat, "track_mode", float(inputDict["trackingMode"]))
-	ssc.ssc_data_set_number(dat, "azimuth", float(inputDict["azimuth"]))
-	# Advanced inputs with defaults.
-	ssc.ssc_data_set_number(dat, "rotlim", float(inputDict["rotlim"]))
-	ssc.ssc_data_set_number(dat, "t_noct", float(inputDict["t_noct"]))
-	ssc.ssc_data_set_number(dat, "t_ref", float(inputDict["t_ref"]))
-	ssc.ssc_data_set_number(dat, "gamma", float(inputDict["gamma"]))
-	ssc.ssc_data_set_number(dat, "inv_eff", float(inputDict["inv_eff"]))
-	ssc.ssc_data_set_number(dat, "fd", float(inputDict["fd"]))
-	ssc.ssc_data_set_number(dat, "i_ref", float(inputDict["i_ref"]))
-	ssc.ssc_data_set_number(dat, "poa_cutin", float(inputDict["poa_cutin"]))
-	ssc.ssc_data_set_number(dat, "w_stow", float(inputDict["w_stow"]))
-	# Complicated optional inputs.
-	ssc.ssc_data_set_number(dat, "tilt_eq_lat", 1)
-	# ssc.ssc_data_set_array(dat, 'shading_hourly', ...) 	# Hourly beam shading factors
-	# ssc.ssc_data_set_matrix(dat, 'shading_mxh', ...) 		# Month x Hour beam shading factors
-	# ssc.ssc_data_set_matrix(dat, 'shading_azal', ...) 	# Azimuth x altitude beam shading factors
-	# ssc.ssc_data_set_number(dat, 'shading_diff', ...) 	# Diffuse shading factor
-	# ssc.ssc_data_set_number(dat, 'enable_user_poa', ...)	# Enable user-defined POA irradiance input = 0 or 1
-	# ssc.ssc_data_set_array(dat, 'user_poa', ...) 			# User-defined POA irradiance in W/m2
-	# ssc.ssc_data_set_number(dat, 'tilt', 999)
-	# Run PV system simulation.
-	mod = ssc.ssc_module_create("pvwattsv1")
-	ssc.ssc_module_exec(mod, dat)
-	# Setting options for start time.
-	simLengthUnits = inputDict.get("simLengthUnits","")
-	simStartDate = inputDict["simStartDate"]
-	# Set the timezone to be UTC, it won't affect calculation and display, relative offset handled in pvWatts.html 
-	startDateTime = simStartDate + " 00:00:00 UTC"
-	# Set aggregation function constants.
-	agg = lambda x,y:_aggData(x,y,inputDict["simStartDate"],
-		int(inputDict["simLength"]), inputDict["simLengthUnits"], ssc, dat)
-	avg = lambda x:sum(x)/len(x)
-	# Timestamp output.
-	outData = {}
-	outData["timeStamps"] = [datetime.datetime.strftime(
-		datetime.datetime.strptime(startDateTime[0:19],"%Y-%m-%d %H:%M:%S") + 
-		datetime.timedelta(**{simLengthUnits:x}),"%Y-%m-%d %H:%M:%S") + " UTC" for x in range(int(inputDict["simLength"]))]
-	# Geodata output.
-	outData["city"] = ssc.ssc_data_get_string(dat, "city")
-	outData["state"] = ssc.ssc_data_get_string(dat, "state")
-	outData["lat"] = ssc.ssc_data_get_number(dat, "lat")
-	outData["lon"] = ssc.ssc_data_get_number(dat, "lon")
-	outData["elev"] = ssc.ssc_data_get_number(dat, "elev")
-	# Weather output.
-	outData["climate"] = {}
-	outData["climate"]["Direct Irradiance (W/m^2)"] = agg("dn", avg)
-	outData["climate"]["Difuse Irradiance (W/m^2)"] = agg("df", avg)
-	outData["climate"]["Ambient Temperature (F)"] = agg("tamb", avg)
-	outData["climate"]["Cell Temperature (F)"] = agg("tcell", avg)
-	outData["climate"]["Wind Speed (m/s)"] = agg("wspd", avg)
-	# Power generation.
-	outData["Consumption"] = {}
-	outData["Consumption"]["Power"] = [x for x in agg("ac", avg)]
-	outData["Consumption"]["Losses"] = [0 for x in agg("ac", avg)]
-	outData["Consumption"]["DG"] = agg("ac", avg)
-	# Stdout/stderr.
-	outData["stdout"] = "Success"
-	outData["stderr"] = ""
-	# Write the output.
-	with open(pJoin(modelDir,"allOutputData.json"),"w") as outFile:
-		json.dump(outData, outFile, indent=4)
-	# Update the runTime in the input file.
-	endTime = datetime.datetime.now()
-	inputDict["runTime"] = str(datetime.timedelta(seconds=int((endTime - startTime).total_seconds())))
-	with open(pJoin(modelDir,"allInputData.json"),"w") as inFile:
-		json.dump(inputDict, inFile, indent=4)
+	try:
+		if not os.path.isdir(modelDir):
+			os.makedirs(modelDir)
+			inputDict["created"] = str(datetime.datetime.now())
+		# MAYBEFIX: remove this data dump. Check showModel in web.py and renderTemplate()
+		with open(pJoin(modelDir, "allInputData.json"),"w") as inputFile:
+			json.dump(inputDict, inputFile, indent = 4)
+		# Copy spcific climate data into model directory
+		shutil.copy(pJoin(__metaModel__._omfDir, "data", "Climate", inputDict["climateName"] + ".tmy2"), 
+			pJoin(modelDir, "climate.tmy2"))
+		# Ready to run
+		startTime = datetime.datetime.now()
+		# Set up SAM data structures.
+		ssc = nrelsam.SSCAPI()
+		dat = ssc.ssc_data_create()
+		# Required user inputs.
+		ssc.ssc_data_set_string(dat, "file_name", modelDir + "/climate.tmy2")
+		ssc.ssc_data_set_number(dat, "system_size", float(inputDict["systemSize"]))
+		ssc.ssc_data_set_number(dat, "derate", float(inputDict["derate"])*0.01)
+		ssc.ssc_data_set_number(dat, "track_mode", float(inputDict["trackingMode"]))
+		ssc.ssc_data_set_number(dat, "azimuth", float(inputDict["azimuth"]))
+		# Advanced inputs with defaults.
+		ssc.ssc_data_set_number(dat, "rotlim", float(inputDict["rotlim"]))
+		ssc.ssc_data_set_number(dat, "t_noct", float(inputDict["t_noct"]))
+		ssc.ssc_data_set_number(dat, "t_ref", float(inputDict["t_ref"]))
+		ssc.ssc_data_set_number(dat, "gamma", float(inputDict["gamma"]))
+		ssc.ssc_data_set_number(dat, "inv_eff", float(inputDict["inv_eff"]))
+		ssc.ssc_data_set_number(dat, "fd", float(inputDict["fd"]))
+		ssc.ssc_data_set_number(dat, "i_ref", float(inputDict["i_ref"]))
+		ssc.ssc_data_set_number(dat, "poa_cutin", float(inputDict["poa_cutin"]))
+		ssc.ssc_data_set_number(dat, "w_stow", float(inputDict["w_stow"]))
+		# Complicated optional inputs.
+		ssc.ssc_data_set_number(dat, "tilt_eq_lat", 1)
+		# ssc.ssc_data_set_array(dat, 'shading_hourly', ...) 	# Hourly beam shading factors
+		# ssc.ssc_data_set_matrix(dat, 'shading_mxh', ...) 		# Month x Hour beam shading factors
+		# ssc.ssc_data_set_matrix(dat, 'shading_azal', ...) 	# Azimuth x altitude beam shading factors
+		# ssc.ssc_data_set_number(dat, 'shading_diff', ...) 	# Diffuse shading factor
+		# ssc.ssc_data_set_number(dat, 'enable_user_poa', ...)	# Enable user-defined POA irradiance input = 0 or 1
+		# ssc.ssc_data_set_array(dat, 'user_poa', ...) 			# User-defined POA irradiance in W/m2
+		# ssc.ssc_data_set_number(dat, 'tilt', 999)
+		# Run PV system simulation.
+		mod = ssc.ssc_module_create("pvwattsv1")
+		ssc.ssc_module_exec(mod, dat)
+		# Setting options for start time.
+		simLengthUnits = inputDict.get("simLengthUnits","")
+		simStartDate = inputDict["simStartDate"]
+		# Set the timezone to be UTC, it won't affect calculation and display, relative offset handled in pvWatts.html 
+		startDateTime = simStartDate + " 00:00:00 UTC"
+		# Set aggregation function constants.
+		agg = lambda x,y:_aggData(x,y,inputDict["simStartDate"],
+			int(inputDict["simLength"]), inputDict["simLengthUnits"], ssc, dat)
+		avg = lambda x:sum(x)/len(x)
+		# Timestamp output.
+		outData = {}
+		outData["timeStamps"] = [datetime.datetime.strftime(
+			datetime.datetime.strptime(startDateTime[0:19],"%Y-%m-%d %H:%M:%S") + 
+			datetime.timedelta(**{simLengthUnits:x}),"%Y-%m-%d %H:%M:%S") + " UTC" for x in range(int(inputDict["simLength"]))]
+		# Geodata output.
+		outData["city"] = ssc.ssc_data_get_string(dat, "city")
+		outData["state"] = ssc.ssc_data_get_string(dat, "state")
+		outData["lat"] = ssc.ssc_data_get_number(dat, "lat")
+		outData["lon"] = ssc.ssc_data_get_number(dat, "lon")
+		outData["elev"] = ssc.ssc_data_get_number(dat, "elev")
+		# Weather output.
+		outData["climate"] = {}
+		outData["climate"]["Direct Irradiance (W/m^2)"] = agg("dn", avg)
+		outData["climate"]["Difuse Irradiance (W/m^2)"] = agg("df", avg)
+		outData["climate"]["Ambient Temperature (F)"] = agg("tamb", avg)
+		outData["climate"]["Cell Temperature (F)"] = agg("tcell", avg)
+		outData["climate"]["Wind Speed (m/s)"] = agg("wspd", avg)
+		# Power generation.
+		outData["Consumption"] = {}
+		outData["Consumption"]["Power"] = [x for x in agg("ac", avg)]
+		outData["Consumption"]["Losses"] = [0 for x in agg("ac", avg)]
+		outData["Consumption"]["DG"] = agg("ac", avg)
+		# Stdout/stderr.
+		outData["stdout"] = "Success"
+		outData["stderr"] = ""
+		# Write the output.
+		with open(pJoin(modelDir,"allOutputData.json"),"w") as outFile:
+			json.dump(outData, outFile, indent=4)
+		# Update the runTime in the input file.
+		endTime = datetime.datetime.now()
+		inputDict["runTime"] = str(datetime.timedelta(seconds=int((endTime - startTime).total_seconds())))
+		with open(pJoin(modelDir,"allInputData.json"),"w") as inFile:
+			json.dump(inputDict, inFile, indent=4)
+	except:
+		#if input range wasn't valid delete output and pass
+		try:
+			os.remove(pJoin(modelDir,"allOutputData.json"))	
+		except Exception, e:
+			pass			
+
+	
 
 def _aggData(key, aggFun, simStartDate, simLength, simLengthUnits, ssc, dat):
 	''' Function to aggregate output if we need something other than hour level. '''
