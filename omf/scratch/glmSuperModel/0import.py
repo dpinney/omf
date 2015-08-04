957b6956f3ffa6a294fb14f42c0939e0581c840f
David Pinney <dpinney@me.com>
2015-08-04 10:48:43 -0400

    glmSuperModel now in json format.

--- omf/scratch/glmSuperModel/0import.py
+++ omf/scratch/glmSuperModel/0import.py
@@ -1,8 +1,18 @@
-''' Try to import the GLD superModel. '''
+'''
+Try to import the GLD superModel.
 
-import omf
+'''
 
-print 'hello'
 
-myFeed = omf.feeder.parse("./R1_1247_1_t15.glm")
+
+import omf, os, json
+
+# Read in the file (or cache, if it's cached.)
+fName = './glmSuperModel.json'
+if not os.path.isfile(fName):
+	myFeed = omf.feeder.parse('./R1_1247_1_t15.glm')
+	with open(fName, 'w') as jFile:
+		json.dump(myFeed, jFile, indent=4)
+else:
+	myFeed = json.load(open(fName))
 
