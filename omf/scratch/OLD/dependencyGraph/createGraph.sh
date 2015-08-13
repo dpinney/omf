2af788137dad725625754e67ec9491c0cf71e2f4
David Pinney <dpinney@me.com>
2015-08-13 17:07:02 -0400

    Even more scratch clean up. Tech list for glmSuperModel.

--- omf/scratch/OLD/dependencyGraph/createGraph.sh
+++
@@ -1,3 +0,0 @@
-# I think this folder needs to be outside of the omf directory or it will be part of the dependency graph?
-# requires graphviz from http://www.graphviz.org/ (not a python module)
-python py2depgraph.py path/to/omf | python depgraph2dot.py | dot -T png -o depgraph.png
