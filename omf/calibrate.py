dc4f4fb3fb47e1586f98e4cfbf3cc67ed025d4f5
mannanj <mannanjavid@gmail.com>
2015-09-30 13:27:12 -0400

    Iteration added to power output.

--- omf/calibrate.py
+++ omf/calibrate.py
@@ -19,8 +19,9 @@ def omfCalibrate(workDir, feederPath, scadaPath):
 	classOb = {'omftype':'class player','argument':'{double value;}'}
 	playerOb = {"object":"player", "property":"value", "name":"scadaLoads", "file":"subScada.player", "loop":"0"}
 	maxKey = feeder.getMaxKey(tree)
+	playerKey = maxKey + 2
 	tree[maxKey+1] = classOb
-	tree[maxKey+2] = playerOb
+	tree[playerKey] = playerOb
 	# Make loads reference player.
 	loadTemplate = {"object": "triplex_load",
 		"power_pf_12": "0.95",
@@ -55,42 +56,53 @@ def omfCalibrate(workDir, feederPath, scadaPath):
 		"property": "power_in.real,power_in.imag",
 		"file": "caliSub.csv",
 		"interval": "900"}
-	tree[maxKey + 3] = recOb
+	outputRecorderKey = maxKey + 3
+	tree[outputRecorderKey] = recOb
 	HOURS = 100
 	feeder.adjustTime(tree, HOURS, "hours", firstDateTime.strftime("%Y-%m-%d"))
-	# Run Gridlabd.
+	# Run Gridlabd, calculate scaling constant.
 	output = gridlabd.runInFilesystem(tree, keepFiles=True, workDir=workDir)
-	# Calculate scaling constant.
 	outRealPow = output["caliSub.csv"]["power_in.real"]
 	outImagPower = output["caliSub.csv"]["power_in.imag"]
 	outAppPowerKw = [(x[0]**2 + x[1]**2)**0.5/1000 for x in zip(outRealPow, outImagPower)]
-	# HACK: ignore first time step in output and input because GLD sometimes breaks the first step.
-	SCAL_CONST = sum(scadaSubPower[1:HOURS])/sum(outAppPowerKw[1:HOURS])
-	# Rewrite the subScada.player file so all the power values are multiplied by the SCAL_CONSTANT.
-	newPlayData = []
-	with open(pJoin(workDir, "subScada.player"), "r") as playerFile:
-		for line in playerFile:
-			(key,val) = line.split(',')
-			newPlayData.append(str(key) + ',' + str(float(val)*SCAL_CONST) + "\n")
-	with open(pJoin(workDir, "subScadaCalibrated.player"), "w") as playerFile:
-		for row in newPlayData:
-			playerFile.write(row)
-	# Test by running a glm with subScadaCalibrated.player and caliSub.csv2.
-	tree[maxKey+2]["file"] = "subScadaCalibrated.player"
-	tree[maxKey + 3]["file"] = "caliSubCheck.csv"
-	secondOutput = gridlabd.runInFilesystem(tree, keepFiles=True, workDir=workDir)
+	previousFile = "subScada.player"
+	nextFile = "subScadaCalibrated.player"
+	nextPower = outAppPowerKw[1:HOURS]
+	iterationTimes = 2
+	for i in range(1, iterationTimes+1):
+		# HACK: ignore first time step in output and input because GLD sometimes breaks the first step.
+		SCAL_CONST = sum(scadaSubPower[1:HOURS])/sum(nextPower)
+		# Rewrite the subScada.player file so all the power values are multiplied by the SCAL_CONSTANT.
+		newPlayData = []
+		with open(pJoin(workDir, previousFile), "r") as playerFile:
+			for line in playerFile:
+				(key,val) = line.split(',')
+				newPlayData.append(str(key) + ',' + str(float(val)*SCAL_CONST) + "\n")
+		with open(pJoin(workDir, nextFile), "w") as playerFile:
+			for row in newPlayData:
+				playerFile.write(row)
+		# Test by running a glm with subScadaCalibrated.player and caliSub.csv2.
+		tree[playerKey]["file"] = nextFile
+		tree[outputRecorderKey]["file"] = "caliSubCheck.csv" #Note: this caliSubCheck.csv will be overwritten each iteration.
+		nextOutput = gridlabd.runInFilesystem(tree, keepFiles=True, workDir=workDir)
+		outRealPow2nd = nextOutput["caliSubCheck.csv"]["power_in.real"]
+		outImagPower2nd = nextOutput["caliSubCheck.csv"]["power_in.imag"]
+		nextAppKw = [(x[0]**2 + x[1]**2)**0.5/1000
+			for x in zip(outRealPow2nd, outImagPower2nd)]
+		# Set the in and out playerFiles and out power for the next iteration.
+		previousFile = nextFile
+		nextFile = "subScadaCalibrated"+str(i)+".player"
+		nextPower = nextAppKw[1:HOURS]
 	plt.figure()
 	plt.plot(outAppPowerKw[1:HOURS], label="initialGuess")
 	plt.plot(scadaSubPower[1:HOURS], label="scadaSubPower")
-	secondAppKw = [(x[0]**2 + x[1]**2)**0.5/1000
-		for x in zip(secondOutput["caliSubCheck.csv"]["power_in.real"], secondOutput["caliSubCheck.csv"]["power_in.imag"])]
-	plt.plot(secondAppKw[1:HOURS], label="finalGuess")
+	plt.plot(nextAppKw[1:HOURS], label="finalGuess")
 	plt.legend(loc=3)
 	plt.savefig(pJoin(workDir,"caliCheckPlot.png"))
 	# Write the final output.
 	with open(pJoin(workDir,"calibratedFeeder.json"),"w") as outJson:
-		playerString = open(pJoin(workDir,"subScadaCalibrated.player")).read()
-		feederJson["attachments"]["subScadaCalibrated.player"] = playerString
+		playerString = open(pJoin(workDir,previousFile)).read()
+		feederJson["attachments"][previousFile] = playerString
 		feederJson["tree"] = tree
 		json.dump(feederJson, outJson, indent=4)
 	return
@@ -117,7 +129,7 @@ def _tests():
 	workDir = tempfile.mkdtemp()
 	print "Currently working in: ", workDir
 	scadaPath = pJoin("uploads", "FrankScada.tsv")
-	feederPath = pJoin("data", "Feeder", "public","ABEC Frank LO.json")
+	feederPath = pJoin("data", "Feeder", "public","ABEC Frank pre calib.json")
 	assert None == omfCalibrate(workDir, feederPath, scadaPath), "feeder calibration failed"
 
 if __name__ == '__main__':
