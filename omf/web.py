52d09db88500582d93db3ba09bffeafbf6b022eb
mannanj <Mannan.Javid-contractor@nreca.coop>
2016-01-13 14:18:59 -0500

    Cross-browser access enabled. Grid+models cleaned.

--- omf/web.py
+++ omf/web.py
@@ -35,10 +35,7 @@ def getDataNames():
 
 @app.before_request
 def csrf_protect():
-	if request.user_agent.browser != "chrome":
-		return "<img style='width:400px;margin-right:auto; margin-left:auto;display:block;' \
-			src='http://goo.gl/1GvUMA'><br> \
-			<h2 style='text-align:center'>The OMF currently must be accessed by <a href='http://goo.gl/X2ZGhb''>Chrome</a></h2>"
+	pass
 	## NOTE: when we fix csrf validation this needs to be uncommented.
 	# if request.method == "POST":
 	#	token = session.get("_csrf_token", None)
