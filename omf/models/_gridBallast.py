332fbd74715924901498a353da801c6957bda72a
mhxi <mhxi91@gmail.com>
2017-06-21 15:16:06 -0400

    fix rmt metric, multiply decimal by 100 to get %

--- omf/models/_gridBallast.py
+++ omf/models/_gridBallast.py
@@ -447,7 +447,7 @@ def heavyProcessing(modelDir, inputDict):
 		totNetLoad = cleanOut['gridBallast']['totalNetworkLoad']
 		# loadZip = zip(availMag,totNetLoad)
 		# rmt = [x[0]/x[1] for x in loadZip]
-		rmt = 1000*sum(availMag)/sum(totNetLoad)
+		rmt = 100*1000*sum(availMag)/sum(totNetLoad)
 		cleanOut['gridBallast']['rmt'] = rmt
 		# Reserve Magnitude Variability Tolerance (RMVT)
 		avgAvailMag = sum(availMag)/len(availMag)
