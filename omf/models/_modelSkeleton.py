908ea3b34ea5ae61649b58924dd7874d5c8dae00
mhxi <mhxi91@gmail.com>
2017-09-07 17:24:09 -0400

    implemented runRefactor on _modelSkeleton

--- omf/models/_modelSkeleton.py
+++ omf/models/_modelSkeleton.py
@@ -3,11 +3,11 @@
 import json, os, sys, tempfile, webbrowser, time, shutil, subprocess, datetime, traceback
 from os.path import join as pJoin
 from jinja2 import Template
-import __metaModel__
-from __metaModel__ import *
+import __neoMetaModel__
+from __neoMetaModel__ import *
 
 # OMF imports
-sys.path.append(__metaModel__._omfDir)
+sys.path.append(__neoMetaModel__._omfDir)
 import feeder
 from solvers import nrelsam2013
 from weather import zipCodeToClimateName
@@ -17,8 +17,27 @@ fileName = os.path.basename(__file__)
 modelName = fileName[0:fileName.rfind('.')]
 
 # Our HTML template for the interface:
-with open(pJoin(__metaModel__._myDir,modelName+".html"),"r") as tempFile:
+with open(pJoin(__neoMetaModel__._myDir,modelName+".html"),"r") as tempFile:
 	template = Template(tempFile.read())
+
+def work(modelDir, inputDict):
+	''' Run the model in its directory. '''
+	# Delete output file every run if it exists
+	outData = {}		
+	# Model operations goes here.
+	inputOne = inputDict.get("input1", 123)
+	inputTwo = inputDict.get("input2", 867)
+	output = inputOne + inputTwo
+	outData["output"] = output
+	# Model operations typically ends here.
+	# Stdout/stderr.
+	outData["stdout"] = "Success"
+	outData["stderr"] = ""
+	return outData
+
+def cancel(modelDir):
+	''' PV Watts runs so fast it's pointless to cancel a run. '''
+	pass
 
 def new(modelDir):
 	''' Create a new instance of this model. Returns true on success, false on failure. '''
@@ -29,61 +48,11 @@ def new(modelDir):
 		"input2": "123 Or Simple as...",
 		"created":str(datetime.datetime.now())
 	}
-	return __metaModel__.new(modelDir, defaultInputs)
-
-
-def run(modelDir, inputDict):
-	''' Run the model in its directory. '''
-	# Delete output file every run if it exists
-	try:
-		os.remove(pJoin(modelDir,"allOutputData.json"))	
-	except Exception, e:
-		pass
-	# Check whether model exist or not
-	try:
-		if not os.path.isdir(modelDir):
-			os.makedirs(modelDir)
-			inputDict["created"] = str(datetime.datetime.now())
-		# MAYBEFIX: remove this data dump. Check showModel in web.py and renderTemplate()
-		with open(pJoin(modelDir, "allInputData.json"),"w") as inputFile:
-			json.dump(inputDict, inputFile, indent = 4)			
-		startTime = datetime.datetime.now()
-		outData = {}		
-		# Model operations goes here.
-		inputOne = inputDict.get("input1", 123)
-		inputTwo = inputDict.get("input2", 867)
-		output = inputOne + inputTwo
-		outData["output"] = output
-		# Model operations typically ends here.
-		# Stdout/stderr.
-		outData["stdout"] = "Success"
-		outData["stderr"] = ""
-		# Write the output.
-		with open(pJoin(modelDir,"allOutputData.json"),"w") as outFile:
-			json.dump(outData, outFile, indent=4)
-		# Update the runTime in the input file.
-		endTime = datetime.datetime.now()
-		inputDict["runTime"] = str(datetime.timedelta(seconds=int((endTime - startTime).total_seconds())))
-		with open(pJoin(modelDir,"allInputData.json"),"w") as inFile:
-			json.dump(inputDict, inFile, indent=4)
-	except:
-		# If input range wasn't valid delete output, write error to disk.
-		cancel(modelDir)				
-		thisErr = traceback.format_exc()
-		print 'ERROR IN MODEL', modelDir, thisErr
-		inputDict['stderr'] = thisErr
-		with open(os.path.join(modelDir,'stderr.txt'),'w') as errorFile:
-			errorFile.write(thisErr)
-		with open(pJoin(modelDir,"allInputData.json"),"w") as inFile:
-			json.dump(inputDict, inFile, indent=4)
-
-def cancel(modelDir):
-	''' PV Watts runs so fast it's pointless to cancel a run. '''
-	pass
+	return __neoMetaModel__.new(modelDir, defaultInputs)
 
 def _tests():
 	# Location
-	modelLoc = pJoin(__metaModel__._omfDir,"data","Model","admin","Automated pvWatts Testing")
+	modelLoc = pJoin(__neoMetaModel__._omfDir,"data","Model","admin","Automated pvWatts Testing")
 	# Blow away old test results if necessary.
 	try:
 		shutil.rmtree(modelLoc)
@@ -95,7 +64,7 @@ def _tests():
 	# Pre-run.
 	renderAndShow(modelLoc)
 	# Run the model.
-	run(modelLoc, inputDict=json.load(open(modelLoc + "/allInputData.json")))
+	runForeground(modelLoc, inputDict=json.load(open(modelLoc + "/allInputData.json")))
 	# Show the output.
 	renderAndShow(modelLoc)
 
