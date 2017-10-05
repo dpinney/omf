8d1be0001dddb67d2039e824bb66f0a7b116bb08
David Pinney <david@pinney.org>
2017-10-05 11:23:07 -0400

    Logging for prod server config.

--- omf/webProd.py
+++ omf/webProd.py
@@ -1,6 +1,7 @@
-import web
+import web, logging
 
 if __name__ == "__main__":
+	logging.basicConfig(filename='omf.log',level=logging.DEBUG)
 	template_files = ["templates/"+ x  for x in web.safeListdir("templates")]
 	model_files = ["models/" + x for x in web.safeListdir("models")]
 	web.app.run(debug=False, host="0.0.0.0", port=80, extra_files=template_files + model_files)
