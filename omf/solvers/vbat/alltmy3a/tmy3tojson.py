ac5d0eb6a4266a937a4511c8093a3264df0e6a81
Dylan S <simondylan@hotmail.com>
2017-10-16 12:36:20 -0400

    zipped data and updated py code

--- omf/solvers/vbat/alltmy3a/tmy3tojson.py
+++ omf/solvers/vbat/alltmy3a/tmy3tojson.py
@@ -1,30 +1,29 @@
-import csv
 import json
 import os.path
+import csv
+#filelist = os.listdir('C:\\Users\\simon\\Desktop\\NRECA\\alltmy3a')
+filelist = os.listdir('C:\\Users\\simon\\Documents\\GitHub\\omf\\omf\\solvers\\vbat\\alltmy3a') #put your own path
+#print filelist
 
-filelist = os.listdir('C:\\Users\\simon\\Desktop\\NRECA\\alltmy3a')
-print filelist
 results = []
-for filename in filelist:
-	if filename[-3:] == 'CVS':
-		with open('C:\\Users\\simon\\Desktop\\NRECA\\' + filename) as f:
-			rows = list(csv.reader(f, delimiter=','))
-			
-			temp = []
 
-			#print rows[0][2]
+for filename in filelist:
+	if filename[-3:] == 'CSV':
+		state = []
+		city = []
+		temp = []
+		with open (filename) as f:
+			rows = list(csv.reader(f, delimiter=','))
+
 			state = rows[0][2]
 			city = rows[0][1]
-			for row in rows[2:]: #32 is the index of bulb temp
+			for row in rows[2:]: #31 is the index of bulb temp
 				temp.append(row[31])
 
-				
-
-
 			results.append({
-				'State':state,
-				'City':city,
-				'Temperatures':temp})
+				'State' : state,
+				'City' : city,
+				'Temperatures' : temp})
 
 
 with open('data.json', 'w') as outfile:
