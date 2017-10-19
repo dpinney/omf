f1b552c204b0fc0c285d54d3bc843766556c5751
Dylan S <simondylan@hotmail.com>
2017-10-19 14:57:28 -0400

    added code that allows to have tmy3 data

---
+++ omf/solvers/vbat/geoDataListGenerator.py
@@ -0,0 +1,15 @@
+import json
+import os.path
+import csv
+
+filename = 'geodata.csv'
+
+text = []
+with open (filename) as f:
+	rows = list(csv.reader(f, delimiter=','))
+	for column in rows:
+		text.append( '<option value="' + "'" + column[2] + "'" + '">' + column[1] + ' - ' +  column[2] + '</option>' )
+
+with open('text.txt', 'w') as outfile:
+	for line in text:
+		outfile.write('%s\n' % line)
