e9193487d544cd4cfe921ecb0d6b860714b2fcab
Dylan S <simondylan@hotmail.com>
2017-10-27 13:02:12 -0400

    Fixed stuff, added comments

--- omf/solvers/vbat/HistoricalPessimistApproach.py
+++ omf/solvers/vbat/HistoricalPessimistApproach.py
@@ -1,25 +1,42 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 from operator import sub
+#The demand:
 demand = [1000,1000,1000,1000,1000,1025,1050,1075,1125,1200,1450,1750,1900,2000,1900,1750,1450,1200,1125,1075,1050,1025,1000,1000]
 plt.plot(demand)
-#plt.plot([1000,1000,1000,1000,1000,1025,1050,1075,1125,1200,1450,1750,1900,2000,1900,1750,1450,1200,1125,1075,1050,1025,1000,1000])
-#plt.plot([datapoints])
 plt.ylabel('Demand in kW')
 plt.xlabel('Hour of the day')
 plt.axis([0,23,0,2200])
+#peak demand
 peakDemand = max(demand)
+#time at which peak demand is reached
 peakDemandHour = demand.index(peakDemand)
-peakWidth = 2
+
+peakWidth = 3
 startingPeakHour = peakDemandHour - peakWidth/2
 endingPeakHour = peakDemandHour + peakWidth/2
-#print startingPeakHour
-#print endingPeakHour
-peakCutOff = [0]*24
-peakCutOff[startingPeakHour] = 0
+
+peakCutOff =[0]*24
+cutOff = demand[startingPeakHour]
+'''peakCutOff[startingPeakHour:endingPeakHour] = [demand[peakDemandHour] - demand[startingPeakHour]]
+print peakCutOff'''
+'''for x in peakCutOff[x]:
+	print x
+	if x>=startingPeakHour and x<=endingPeakHour:
+		peakCutOff[x] = demand[x] - demand[startingPeakHour]'''
+
+for x in xrange(0,23):
+	if demand[x] < cutOff:
+		peakCutOff[x] = 0
+	else:
+		peakCutOff[x] = demand[x]-cutOff
+
+'''peakCutOff[startingPeakHour] = 0
 peakCutOff[peakDemandHour] = demand[peakDemandHour] - demand[startingPeakHour]
-peakCutOff[endingPeakHour] = 0
-#print peakCutOff
+peakCutOff[endingPeakHour] = 0'''
+print demand
+print peakCutOff
+
 energyShaved = peakWidth*(peakDemand-demand[startingPeakHour])/2
 print "The energy shaved is: " + str(energyShaved) + " kWh"
 
