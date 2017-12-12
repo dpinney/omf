eeeab124d1bcfaea088e4bebd6964827a1938da8
Dylan S <simondylan@hotmail.com>
2017-12-12 12:44:50 -0500

    Quick fix

--- omf/weatherNoaa.py
+++ omf/weatherNoaa.py
@@ -13,7 +13,7 @@ def noaaWeather(token, zipCode, year, month, day):
 
 ############### Trying to pull only u'datatype': u'HLY-TEMP-NORMAL'
 # Query Parameters
-zipCode = '11430'#'22202'
+'''zipCode = '11430'#'22202'
 year = '2010'
 month = '01'
 day = '01'
@@ -22,8 +22,10 @@ day = '01'
 url = ('https://www.ncdc.noaa.gov/cdo-web/api/v2/data?datasetid=NORMAL_HLY&datatype=HLY-TEMP-NORMAL&locationid=ZIP:' + zipCode + 
 		'&units=metric&startdate=' + year + '-' + month + '-' + day + 'T00:00:00&enddate=' + year + '-' + month + '-' + day + 'T23:00:00&limit=1000')
 r = requests.get(url, headers=headers)
-print r.json()
+print r.json()'''
 
+zipCode = '11430'#'22202'
+url = ('https://www.ncdc.noaa.gov/cdo-web/api/v2/datasets?locationid=ZIP:' + zipCode + '&limit=1000')
 ########## This checks if data is available for the Zip entered and returns the dates it is available
 r = requests.get(url, headers=headers)
 x = str(r.text)
