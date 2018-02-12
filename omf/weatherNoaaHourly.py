bfbd6d2016b6c23c2c36ac5d290568628ef6a093
Dylan S <simondylan@hotmail.com>
2018-02-12 11:39:31 -0500

    functionalization of weatherNoaaHourly

--- omf/weatherNoaaHourly.py
+++ omf/weatherNoaaHourly.py
@@ -18,7 +18,23 @@ with open('weatherNoaaTemp.csv', 'wb') as myfile:
 		wr.writerow([tempData[x]])
 
 def pullWeather(year, station, outputPath):
-	# TODO: implement to return the 8760 .csv file to outputPath.
-	pass
+	url = 'https://www1.ncdc.noaa.gov/pub/data/uscrn/products/hourly02/' + year + '/CRNH0203-' + year + '-' + station + '.txt'
+	r = requests.get(url)
+	data = r.text
+	tempData = []
+	for x in range(0,8760):
+		temp = ''
+		for y in range(x*244+59,x*244+64):
+			temp+=data[y]
+		tempData.append(float(temp))
+	with open(outputPath, 'wb') as myfile:
+		wr = csv.writer(myfile,lineterminator = '\n')
+		for x in range(0,8760): 
+			wr.writerow([tempData[x]])
 
 #TODO: create a tests function.
+def _tests():
+	pullWeather('2017','KY_Versailles_3_NNW','weatherNoaaTemp.csv')
+
+if __name__ == '__main__':
+	_tests()
