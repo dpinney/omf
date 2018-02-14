81b165c62a87ce511e5b1e480e35e939ef40c801
David Pinney <david@pinney.org>
2018-02-14 12:00:30 -0500

    Backing out unneeded features in distScenGen.

--- omf/models/_distScenGen.py
+++ omf/models/_distScenGen.py
@@ -26,107 +26,12 @@ with open(pJoin(__neoMetaModel__._myDir,modelName+".html"),"r") as tempFile:
 def work(modelDir, inputDict):
 	''' Run the model in its directory. '''
 	outData = {}
-	# feederName = inputDict["feederName1"]
+	# Get feeder name.
 	feederName = [x for x in os.listdir(modelDir) if x.endswith('.omd')][0][:-4]
 	inputDict["feederName1"] = feederName
-	# Create voltage drop plot.
-	# print "*DEBUG: feederName:", feederName
-	omd = json.load(open(pJoin(modelDir,feederName + '.omd')))
-	if inputDict.get("layoutAlgorithm", "geospatial") == "geospatial":
-		neato = False
-	else:
-		neato = True 
-	chart = voltPlot(omd, workDir=modelDir, neatoLayout=neato)
-	chart.savefig(pJoin(modelDir,"output.png"))
-	with open(pJoin(modelDir,"output.png"),"rb") as inFile:
-		outData["voltageDrop"] = inFile.read().encode("base64")
+	# Make an output.
+	outData["test"] = 1
 	return outData
-		
-def voltPlot(omd, workDir=None, neatoLayout=False):
-	''' Draw a color-coded map of the voltage drop on a feeder.
-	Returns a matplotlib object. '''
-	tree = omd.get('tree',{})
-	# # Get rid of schedules and climate:
-	for key in tree.keys():
-		if tree[key].get("argument","") == "\"schedules.glm\"" or tree[key].get("tmyfile","") != "":
-			del tree[key]
-	# Make sure we have a voltDump:
-	def safeInt(x):
-		try: return int(x)
-		except: return 0
-	biggestKey = max([safeInt(x) for x in tree.keys()])
-	tree[str(biggestKey*10)] = {"object":"voltdump","filename":"voltDump.csv"}
-	# Run Gridlab.
-	if not workDir:
-		workDir = tempfile.mkdtemp()
-	gridlabOut = gridlabd.runInFilesystem(tree, attachments=omd.get('attachments',{}), workDir=workDir)
-	with open(pJoin(workDir,'voltDump.csv'),'r') as dumpFile:
-		reader = csv.reader(dumpFile)
-		reader.next() # Burn the header.
-		keys = reader.next()
-		voltTable = []
-		for row in reader:
-			rowDict = {}
-			for pos,key in enumerate(keys):
-				rowDict[key] = row[pos]
-			voltTable.append(rowDict)
-	# Calculate average node voltage deviation. First, helper functions.
-	def pythag(x,y):
-		''' For right triangle with sides a and b, return the hypotenuse. '''
-		return math.sqrt(x**2+y**2)
-	def digits(x):
-		''' Returns number of digits before the decimal in the float x. '''
-		return math.ceil(math.log10(x+1))
-	def avg(l):
-		''' Average of a list of ints or floats. '''
-		return sum(l)/len(l)
-	# Detect the feeder nominal voltage:
-	for key in tree:
-		ob = tree[key]
-		if type(ob)==dict and ob.get('bustype','')=='SWING':
-			feedVoltage = float(ob.get('nominal_voltage',1))
-	# Tot it all up.
-	nodeVolts = {}
-	for row in voltTable:
-		allVolts = []
-		for phase in ['A','B','C']:
-			phaseVolt = pythag(float(row['volt'+phase+'_real']),
-							   float(row['volt'+phase+'_imag']))
-			if phaseVolt != 0.0:
-				if digits(phaseVolt)>3:
-					# Normalize to 120 V standard
-					phaseVolt = phaseVolt*(120/feedVoltage)
-				allVolts.append(phaseVolt)
-		nodeVolts[row.get('node_name','')] = avg(allVolts)
-	# Color nodes by VOLTAGE.
-	fGraph = feeder.treeToNxGraph(tree)
-	voltChart = plt.figure(figsize=(15,15))
-	plt.axes(frameon = 0)
-	plt.axis('off')
-	#set axes step equal
-	voltChart.gca().set_aspect('equal')
-	if neatoLayout:
-		# HACK: work on a new graph without attributes because graphViz tries to read attrs.
-		cleanG = nx.Graph(fGraph.edges())
-		cleanG.add_nodes_from(fGraph)
-		positions = graphviz_layout(cleanG, prog='neato')
-	else:
-		positions = {n:fGraph.node[n].get('pos',(0,0)) for n in fGraph}
-	edgeIm = nx.draw_networkx_edges(fGraph, positions)
-	nodeIm = nx.draw_networkx_nodes(fGraph,
-		pos = positions,
-		node_color = [nodeVolts.get(n,0) for n in fGraph.nodes()],
-		linewidths = 0,
-		node_size = 30,
-		cmap = plt.cm.jet)
-	plt.sci(nodeIm)
-	plt.clim(110,130)
-	plt.colorbar()
-	return voltChart
-
-def cancel(modelDir):
-	''' Voltage drop runs so fast it's pointless to cancel a run. '''
-	pass
 
 def new(modelDir):
 	''' Create a new instance of this model. Returns true on success, false on failure. '''
@@ -134,7 +39,6 @@ def new(modelDir):
 		"feederName1": "Olin Barre Geo",
 		"modelType": modelName,
 		"runTime": "",
-		"layoutAlgorithm": "geospatial"
 	}
 	creationCode = __neoMetaModel__.new(modelDir, defaultInputs)
 	try:
