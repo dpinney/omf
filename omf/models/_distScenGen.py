02efa29205b1f58a0ce20e177876747bbf792f37
Tuomas Talvitie <tpt5cu@virginia.edu>
2018-03-13 16:11:51 -0400

    Labels for series fixed

--- omf/models/_distScenGen.py
+++ omf/models/_distScenGen.py
@@ -75,8 +75,8 @@ def work(modelDir, inputDict):
 			for name in os.listdir(pJoin(direc, filename)):
 				if name.endswith('.glm'):
 					proc = subprocess.Popen('gridlabd ' + name, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, cwd=pJoin(direc, filename))
-					# (out, err) = proc.communicate()
-					code = proc.wait()
+					(out, err) = proc.communicate()
+					#code = proc.wait()
 	#parse through gridlabd csv files
 	i=0 # counter to create dictionary
 	for filename in os.listdir(direc):
