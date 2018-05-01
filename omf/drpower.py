0716a182370d72ca5c3dceabcf472a132cc97981
David Pinney <david@pinney.org>
2018-05-01 17:39:43 -0400

    Overriding publication route example.

--- omf/drpower.py
+++ omf/drpower.py
@@ -3,12 +3,12 @@ import omf, os, web, json
 def sigh():
 	return 'SIGH'
 
-def transmissionConvert(userName, modelName):
+def transmissionConvert(owner, modelName):
 	'Assume that PNNL creates a new transmission model folder.' + \
 	'Also they need to place a .m file in that folder called convertMe.m' + \
 	'Then they send a get request to this route.'
 	# Model folder full path.
-	modelDir = os.path.join(omf.omfDir, 'data', 'Model', userName, modelName)
+	modelDir = os.path.join(omf.omfDir, 'data', 'Model', owner, modelName)
 	# Remove the existing file.
 	existingOmts = [x for x in os.listdir(modelDir) if x.endswith('.omt')]
 	for f in existingOmts:
@@ -31,16 +31,30 @@ def transmissionConvert(userName, modelName):
 		inFile.truncate()
 	return 'CONVERTED'
 
+def powerPublish(owner, modelName):
+	return 'PNNL PUBLICATION URL GOES HERE'
+
 if __name__ == '__main__':
 	template_files = ['templates/'+ x  for x in web.safeListdir('templates')]
 	model_files = ['models/' + x for x in web.safeListdir('models')]
 	# Add routes.
 	web.app.add_url_rule('/sigh', 'sigh', view_func=sigh)
-	web.app.add_url_rule('/transmissionConvert/<userName>/<modelName>/', 'transmissionConvert', view_func=transmissionConvert)
+	web.app.add_url_rule('/transmissionConvert/<owner>/<modelName>/', 'transmissionConvert', view_func=transmissionConvert)
+	web.app.add_url_rule('/publishModel/<owner>/<modelName>/', 'powerPublish', methods=["POST"], view_func=powerPublish)
+	# def crash(): raise Exception
+	# web.app.add_url_rule('/crash','crash',view_func=crash)
+	# Remove the bogus old publishModel route.
+	allRules = web.app.url_map._rules
+	del web.app.url_map._rules_by_endpoint['publishModel']
+	for index, rule in enumerate(allRules):
+		if rule.endpoint == 'publishModel':
+			delIndex = index
+	del allRules[delIndex]
 	# Start the server.
 	web.app.run(
 		host='0.0.0.0',
 		port=5001,
 		threaded=True,
-		extra_files=template_files + model_files
+		extra_files=template_files + model_files,
+		# debug=True
 	)
