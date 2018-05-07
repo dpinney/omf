5e6815b8e5b0ad35b89bebfb858c2ca9f90e5dd9
OppenheimerAndTheMartians <irgeorge@gmx.com>
2018-05-07 12:46:41 +0800

    Gridlab file needs serious debugging...

--- omf/scratch/timeCompare.py
+++ omf/scratch/timeCompare.py
@@ -1,10 +1,19 @@
+import timeit
 import os
+import opendssdirect as dss
 
-if __name__ == "__main__":
+glm_input = 'powerflow_IEEE_37node.glm'
+gridlab_sims = dict()
 
-### GRIDLAB-D
-
-### read in glm file.
-
+for i in range(1, 21):
   xlm_file = glm_input.replace('.glm', '.xml')
-  os.system('gridlabd %s - %s' % (glm_input, output_file))
+  os.system('gridlabd %s --output %s' % (glm_input, xlm_file))
+  gridlab_sims[i] = xlm_file
+
+'''
+dss_sims = dict()
+
+for j in range(1, 21):
+  os.system('dss.run_command(%s)' % (str(file_path))
+  dss_sims[j] = 
+'''
