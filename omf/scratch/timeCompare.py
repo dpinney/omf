737d004c1f5a7034ec4262994dc35b87e8c79f54
OppenheimerAndTheMartians <irgeorge@gmx.com>
2018-05-08 16:04:42 +0800

    Add OpenDSS process-something wrong with CPU command?

--- omf/scratch/timeCompare.py
+++ omf/scratch/timeCompare.py
@@ -4,7 +4,7 @@ import os
 import opendssdirect as dss
 
 
-def getGridlabMemory():
+def getMemory():
   process = psutil.Process(os.getpid())
   return process.memory_percent()
 
@@ -13,18 +13,23 @@ gridlab_sims = dict()
 
 t1 = time.time()
 for i in range(1, 21):
-  os.system('gridlabd %s --verbose' % (glm_input))
-  print "PROCESSED"
+  os.system('gridlabd %s --quiet' % (glm_input))
 
-print 'TOTAL TIME: %f' % float((time.time()-t1))
-print 'AVG TIME: %f' % float((time.time()-t1)/21.0)
-print 'CPU PERCENTAGE: %f' % psutil.cpu_percent()
-print 'MEMORY USAGE PERCENTAGE: %f' % getGridlabMemory()
+print 'TOTAL TIME FOR GRIDLAB: %f' % float(time.time()-t1)
+print 'AVG TIME FOR GRIDLAB: %f' % float((time.time()-t1)/21.0)
+print 'CPU USAGE PERCENTAGE FOR GRIDLAB: %f' % psutil.cpu_percent()
+print 'MEMORY USAGE PERCENTAGE FOR GRIDLAB: %f' % getMemory()
 
+print '\n\n\n'
 
 t2 = time.time()
 file_path = '../../../OpenDSS/Distrib/IEEETestCases/37Bus/ieee37.dss'
 for j in range(1, 21):
   dss.run_command('Redirect ' + file_path)
-  dss.run_command('Solve')
- 
+  dss.run_command('Compile ' + file_path)
+
+print 'TOTAL TIME FOR OPENDSS: %f' % float(time.time()-t2)
+print 'AVG TIME FOR OPENDSS: %f' % float((time.time()-t2)/21.0)
+print 'CPU USAGE PERCENTAGE FOR OPENDSS: %f' % psutil.cpu_percent()
+print 'MEMORY USAGE PERCENTAGE FOR OPENDSS: %f' % getMemory()
+
