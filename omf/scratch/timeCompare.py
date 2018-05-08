adf85453ae0d36f159e65a5113dd0d26bfb68a65
OppenheimerAndTheMartians <irgeorge@gmx.com>
2018-05-08 15:53:13 +0800

    Redownloaded OpenDSS files, writing DSS test.

--- omf/scratch/timeCompare.py
+++ omf/scratch/timeCompare.py
@@ -3,26 +3,28 @@ import psutil
 import os
 import opendssdirect as dss
 
-def getMemory():
+
+def getGridlabMemory():
   process = psutil.Process(os.getpid())
   return process.memory_percent()
 
 glm_input = 'powerflow_IEEE_37node.glm'
 gridlab_sims = dict()
 
-t = time.time()
+t1 = time.time()
 for i in range(1, 21):
   os.system('gridlabd %s --verbose' % (glm_input))
   print "PROCESSED"
 
-print 'TOTAL TIME: %f' % float((time.time()-t))
-print 'AVG TIME: %f' % float((time.time()-t)/21.0)
+print 'TOTAL TIME: %f' % float((time.time()-t1))
+print 'AVG TIME: %f' % float((time.time()-t1)/21.0)
 print 'CPU PERCENTAGE: %f' % psutil.cpu_percent()
-print 'MEMORY USAGE PERCENTAGE: %f' % getMemory()
-'''
-dss_sims = dict()
+print 'MEMORY USAGE PERCENTAGE: %f' % getGridlabMemory()
 
+
+t2 = time.time()
+file_path = '../../../OpenDSS/Distrib/IEEETestCases/37Bus/ieee37.dss'
 for j in range(1, 21):
-  os.system('dss.run_command(%s)' % (str(file_path))
-  dss_sims[j] = 
-'''
+  dss.run_command('Redirect ' + file_path)
+  dss.run_command('Solve')
+ 
