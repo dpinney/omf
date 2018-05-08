a3c38b6ccb7b76cc36df542a410fdfab9bdb9e68
OppenheimerAndTheMartians <irgeorge@gmx.com>
2018-05-08 14:10:13 +0800

    CPU and memory usage.

--- omf/scratch/timeCompare.py
+++ omf/scratch/timeCompare.py
@@ -1,6 +1,11 @@
 import time
+import psutil
 import os
 import opendssdirect as dss
+
+def getMemory():
+  process = psutil.Process(os.getpid())
+  return process.memory_percent()
 
 glm_input = 'powerflow_IEEE_37node.glm'
 gridlab_sims = dict()
@@ -9,9 +14,11 @@ t = time.time()
 for i in range(1, 21):
   os.system('gridlabd %s --verbose' % (glm_input))
   print "PROCESSED"
-print 'TOTAL TIME: %f' % (float((time.time()-t)))
-print 'AVG TIME: %f' % (float((time.time()-t)/21.0))
 
+print 'TOTAL TIME: %f' % float((time.time()-t))
+print 'AVG TIME: %f' % float((time.time()-t)/21.0)
+print 'CPU PERCENTAGE: %f' % psutil.cpu_percent()
+print 'MEMORY USAGE PERCENTAGE: %f' % getMemory()
 '''
 dss_sims = dict()
 
