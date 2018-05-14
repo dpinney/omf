07dfa2bb126bd95267f6eafa8ab0795f1a55705e
David Pinney <david@pinney.org>
2018-05-14 16:50:25 -0400

    Working agent-based cosim.

--- omf/scratch/CIGAR/cosim.py
+++ omf/scratch/CIGAR/cosim.py
@@ -1,33 +1,56 @@
-import os, urllib, urllib2, subprocess, time, warnings, HTML
+import os, urllib, urllib2, subprocess, time, warnings
+from datetime import datetime, timedelta
 
+def parseDt(dtString):
+	'Parse GridLAB-D date time strings'
+	return datetime.strptime(dtString, "%Y-%m-%d %H:%M:%S")
+
+def writeDt(dtString):
+	'Write GridLAB-D date time strings.'
+	return datetime.strftime(dtString, "%Y-%m-%d %H:%M:%S")
 
 class Coordinator(object):
-	agents = []
-	MIN_STEP = 15 # in minutes
-	
+	__slots__ = 'agents', 'glw', 'log'
+
 	def __init__(self, agents, cosimProps):
 		self.agents = agents
 		# Start the simulation
-		cosim.start(cosimProps)
-		stepNum = cosimProps.simLength / MIN_STEP
+		self.glw = GridLabWorld(cosimProps['port'],cosimProps['hostname'],cosimProps['glmPath'],cosimProps['startTime'])
+		self.glw.start()
+		# Make a list of all the timesteps we are communicating on.
+		startDt = parseDt(cosimProps['startTime'])
+		endDt = parseDt(cosimProps['endTime'])
+		stepDelta = timedelta(seconds=cosimProps['stepSizeSeconds'])
+		stepNum = (endDt - startDt).total_seconds() / cosimProps['stepSizeSeconds']
+		stepDts = [startDt + stepDelta * x  for x in range(int(stepNum))]
+		# Initialize the log.
+		self.log = []
 		# Step through each time step.
-		for s in range(stepNum):
+		for now in stepDts:
+			logEntry = [now]
 			# At each time step each agent acts.
 			for a in agents:
 				# Send agent's request to GridLAB-D
-				rez = cosim.reqList(a.req(s))
-				# Send results from GridLAB-D back to agent.
-				a.feedback(rez)
-			cosim.waitUntil(s + 1)
+				nowStr = writeDt(now)
+				readRequests = a.readStep(nowStr)
+				readResults = self.glw.doRequests(readRequests)
+				logEntry.append([readRequests, readResults])
+				# Send results from GridLAB-D back to agent for write step.
+				writeCommands = a.writeStep(nowStr, readResults)
+				writeResults = self.glw.doRequests(writeCommands)
+				logEntry.append([writeCommands,writeResults])
+			self.log.append(logEntry)
+			self.glw.waitUntil(writeDt(now + stepDelta))
 
 	def drawResults(self):
-		t_res = HTML.Table(h_row=["Agent", "some_category", "some_category", "some_category"])
-		for agent_x in agents:
-			#HTML.
-		pass #TODO: need an HTML table.
-
+		return self.log
+		# t_res = HTML.Table(h_row=["Agent", "some_category", "some_category", "some_category"])
+		# for agent_x in agents:
+		# 	pass #TODO: need an HTML table.
 
 class GridLabWorld(object):
+	__slots__ = 'PORT', 'HOST', 'GLM_PATH', 'START_PAUSE', 'baseUrl'
+
 	def __init__(self, PORT, HOST, GLM_PATH, START_PAUSE):
 		self.PORT = PORT
 		self.HOST = HOST
@@ -36,11 +59,22 @@ class GridLabWorld(object):
 		# Derived global.
 		self.baseUrl = 'http://' + HOST + ':' + PORT + '/'
 
-	def _request(self, reqType, arg1, arg2=None, arg3=None):
-		pass #TODO: refactor to use this for each other command.
-
-	def reqList(self):
-		pass #TODO: take a list of requests {}, do them.
+	def doRequests(self, reqList):
+		'Do multiple requests.'
+		# E.g. reqList = [{'cmd':'readClock|read|write', 'obName':'', 'propName':'', 'value':''}]
+		results = []
+		for req in reqList:
+			reqType = req.get('cmd','')
+			obName = req.get('obName', '')
+			propName = req.get('propName', '')
+			value = req.get('value', '')
+			if reqType == 'readClock':
+				results.append(self.readClock())
+			elif reqType == 'read':
+				results.append(self.read(obName, propName))
+			elif reqType == 'write':
+				results.append(self.write(obName, propName, value))
+		return results
 
 	def waitUntil(self, targetTime):
 		'Step to targetTime in current simulation.'
@@ -50,11 +84,11 @@ class GridLabWorld(object):
 			urllib2.urlopen(niceUrl).read()
 			currentTime = None
 			while currentTime != targetTime:
-				time.sleep(1)
+				time.sleep(0.2)
 				#HACK: Read clock and also drop the timezone because it causes problems.
 				currentTime = urllib2.urlopen(self.baseUrl + 'raw/clock').read()[0:-4]
 		except:
-			warnings.warn("Wait until failed!")
+			warnings.warn("Wait until " + targetTime + " failed!")
 
 	def read(self, obName, propName):
 		'Read a value from the GLD simulation.'
@@ -99,6 +133,7 @@ def _test():
 	glw.start()
 	# Read the clock, solar output voltage, battery state of charge, and inverter voltage input.
 	print '* Reading clock:', glw.readClock()
+	print '* Bunch of requests:', glw.doRequests([{'cmd':'read', 'obName':'solar_1', 'propName':'V_Out'},{'cmd':'readClock'}])
 	print '* Reading solar_1 output volatage (V_Out):', glw.read('solar_1', 'V_Out')
 	# print '* Reading battery_1 state of charge:', glw.read('battery_1' + 'battery_state')
 	print '* Reading inverter_1 input voltage (V_In):', glw.read('inverter_1','V_In')
@@ -124,5 +159,13 @@ def _test():
 	# shutdown()
 	# proc.kill() # For those hard-to-stop servers.
 
+def _test2():
+	import cyberAttack
+	cosimProps = {'port':'6267', 'hostname':'localhost', 'glmPath':'./smsSingle.glm', 'startTime':'2000-01-01 00:00:00','endTime':'2000-01-05 00:00:00', 'stepSizeSeconds':3600}
+	agents = [cyberAttack.AlertAgent('2000-01-03 12:00:00')]
+	print 'Starting co-sim with 1 agent.'
+	coord = Coordinator(agents, cosimProps)
+	# print coord.drawResults()
+
 if __name__ == '__main__':
-	_test()
+	_test2()
