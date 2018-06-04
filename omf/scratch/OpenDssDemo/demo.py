aa1affd371660e5a3db92673d5f45e817f24f931
Ian George <irgeorge@gmx.com>
2018-06-04 14:06:14 +0800

    Clean up directory.

--- omf/scratch/OpenDssDemo/demo.py
+++ omf/scratch/OpenDssDemo/demo.py
@@ -3,6 +3,7 @@ import types
 import inspect
 
 dss.run_command('Redirect ./short_circuit.dss')
+dss.run_command('Compile ./short_circuit.dss')
 dss.run_command('Solve ./short_circuit.dss')
 print dss.utils.vsources_to_dataframe()
 
