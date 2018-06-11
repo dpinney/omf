c5bbd6d9d3befa46031d660de1e04fbc36186c61
Ian George <irgeorge@gmx.com>
2018-06-11 16:00:14 +0800

    Disabling fstrings, error-free. Now need to show plot.

--- omf/scratch/OpenDssDemo/demoVoltage.py
+++ omf/scratch/OpenDssDemo/demoVoltage.py
@@ -1,10 +1,9 @@
-# -*- coding: future_fstrings -*-
-
 import opendssdirect as dss
 import networkx as nx
 import matplotlib.pyplot as plt
 import pip
 
+'''
 def checkFutureStrings():
 	futureStringsFlag = False
 	installed_packages = pip.get_installed_distributions()
@@ -13,19 +12,19 @@ def checkFutureStrings():
 		pip.main(['install', 'future-fstrings'])
 		futureStringsFlag = True
 	return futureStringsFlag
+'''
 
 def calculateGraph(df, phase=1):
 	graph = nx.Graph()
 	data = df[['Bus1', 'Bus2']].to_dict(orient="index")
 	for key in data:
 		voltage_line = data[key]
-		if f".{phase}" in voltage_line["Bus1"] and f".{phase}" in voltage_line["Bus2"]:
-			graph.add_edge(voltage_line["Bus1"].split(".")[0], voltage_line["Bus2"].split(".")[0])
+		graph.add_edge(voltage_line["Bus1"].split(".")[0], voltage_line["Bus2"].split(".")[0])
 	positions = {}
 	for name in dss.Circuit.AllBusNames():
-		dss.Circuit.SetActiveBus(f"{name}")
+		dss.Circuit.SetActiveBus(name)
 		if phase in dss.Bus.Nodes():
-			index = dss.Bus.Nodes().index(phase)
+			index = dss.Bus.Nodes().index(1)
 			re, im = dss.Bus.PuVoltage()[index:index+2]
 			voltage = abs(complex(re, im))
 			distance = dss.Bus.Distance()
@@ -42,13 +41,14 @@ def plotGraph():
 	ax.set_xlabel('Distances [km]')
 	ax.set_ylabel('Voltage [P.u]')
 	ax.set_title('VOLTAGE PROFILE')
+	fig.show()
 
 if __name__ == "__main__":
-	uninstallFlag = checkFutureStrings()
+	#uninstallFlag = checkFutureStrings()
 	dss.run_command('Redirect ./short_circuit.dss')
 	dss.run_command('New EnergyMeter.Main Line.650632 1')
 	dss.run_command('Solv ./short_circuit.dss')
 	plotGraph()
 
-	if uninstallFlag:
-		pip.main(['uninstall', 'future-fstrings'])
+	#if uninstallFlag:
+		#pip.main(['uninstall', 'future-fstrings'])
