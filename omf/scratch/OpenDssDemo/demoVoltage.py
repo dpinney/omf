9a4676e9ee41036b86e4e621a1998b8ef1ce0368
Ian George <irgeorge@gmx.com>
2018-06-11 16:19:54 +0800

    Debugging plots now.

--- omf/scratch/OpenDssDemo/demoVoltage.py
+++ omf/scratch/OpenDssDemo/demoVoltage.py
@@ -3,16 +3,6 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pip
 
-'''
-def checkFutureStrings():
-	futureStringsFlag = False
-	installed_packages = pip.get_installed_distributions()
-	installed_packages_list = sorted(["%s" % (installation.key) for installation in installed_packages])
-	if 'future_fstrings' not in installed_packages_list:
-		pip.main(['install', 'future-fstrings'])
-		futureStringsFlag = True
-	return futureStringsFlag
-'''
 
 def calculateGraph(df, phase=1):
 	graph = nx.Graph()
@@ -24,7 +14,7 @@ def calculateGraph(df, phase=1):
 	for name in dss.Circuit.AllBusNames():
 		dss.Circuit.SetActiveBus(name)
 		if phase in dss.Bus.Nodes():
-			index = dss.Bus.Nodes().index(1)
+			index = dss.Bus.Nodes().index(phase)
 			re, im = dss.Bus.PuVoltage()[index:index+2]
 			voltage = abs(complex(re, im))
 			distance = dss.Bus.Distance()
@@ -41,14 +31,10 @@ def plotGraph():
 	ax.set_xlabel('Distances [km]')
 	ax.set_ylabel('Voltage [P.u]')
 	ax.set_title('VOLTAGE PROFILE')
-	fig.show()
+	plt.show()
 
 if __name__ == "__main__":
-	#uninstallFlag = checkFutureStrings()
 	dss.run_command('Redirect ./short_circuit.dss')
 	dss.run_command('New EnergyMeter.Main Line.650632 1')
 	dss.run_command('Solv ./short_circuit.dss')
 	plotGraph()
-
-	#if uninstallFlag:
-		#pip.main(['uninstall', 'future-fstrings'])
