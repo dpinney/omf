da84a8c5331a4c3382c11370510f7100f50e6839
Ian George <irgeorge@gmx.com>
2018-07-04 16:13:50 +0800

    Add current script.

--- omf/scratch/OpenDssDemo/plots.py
+++ omf/scratch/OpenDssDemo/plots.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-'''
+
 dss.run_command('Redirect IEEE37.dss')
 voltage = pd.read_csv('volts.csv')
 volt_coord_cols = ['Bus', 'X', 'Y']
@@ -17,13 +17,18 @@ plt.scatter(voltageDF['radius'], voltageDF[' pu1'])
 plt.xlabel('RADIUS')
 plt.ylabel('VOLTS')
 plt.show()
-'''
+
 
 dss.run_command('Redirect short_circuit.dss')
 current = pd.read_csv('short_currents.csv')
-curr_coord_cols = ['Element', 'X', 'Y']
+curr_coord_cols = ['Index', 'X', 'Y']
 curr_coord = pd.read_csv('short_circuit_coords.csv', names=curr_coord_cols)
 curr_hyp = []
 for index, row in curr_coord.iterrows():
 	curr_hyp.append(math.sqrt(row['X']**2 + row['Y']**2))
 curr_coord['radius'] = curr_hyp
+currentDF = pd.concat([curr_coord, current], axis=1)
+plt.scatter(currentDF['radius'], currentDF[' I1_1'])
+plt.xlabel('RADIUS')
+plt.ylabel('CURRENT')
+plt.show()
