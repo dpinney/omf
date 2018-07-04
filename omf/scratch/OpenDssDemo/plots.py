70db91490cc7a0643bd172ac3161515d4790ab3a
Ian George <irgeorge@gmx.com>
2018-07-04 15:46:40 +0800

    Fix indentation, export current profile.

--- omf/scratch/OpenDssDemo/plots.py
+++ omf/scratch/OpenDssDemo/plots.py
@@ -5,14 +5,16 @@ import math
 
 dss.run_command('Redirect IEEE37.dss')
 voltage = pd.read_csv('volts.csv')
-coord_cols = ['Bus', 'X', 'Y']
+volt_coord_cols = ['Bus', 'X', 'Y']
 coord = pd.read_csv('IEEE37_BusXY.csv', names=coord_cols)
 hyp = []
 for index, row in coord.iterrows():
-  hyp.append(math.sqrt(row['X']**2 + row['Y']**2))
+	hyp.append(math.sqrt(row['X']**2 + row['Y']**2))
 coord['radius'] = hyp
 voltageDF = pd.merge(coord, voltage, on='Bus')
 plt.scatter(voltageDF['radius'], voltageDF[' pu1'])
 plt.xlabel('RADIUS')
 plt.ylabel('VOLTS')
 plt.show()
+
+dss.run_command('Redirect short_circuit.dss')
