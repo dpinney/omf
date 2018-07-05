97e29dec182be9e1b122091547a6e253cc29261f
Ian George <irgeorge@gmx.com>
2018-07-05 11:20:15 +0800

    Read pandas file.

--- omf/scratch/OpenDssDemo/linuxplot.py
+++ omf/scratch/OpenDssDemo/linuxplot.py
@@ -1,6 +1,13 @@
 from numpy import *
+from matplotlib.collections import LineCollection
+import pandas as pd
 import opendssdirect as dss
 import matplotlib.pyplot as pyplot
-from matplotlib.collections import LineCollection
 
-print type(dss.run_command('Redirect IEEE37.dss'))
+dss.run_command('Redirect IEEE37.dss')
+col_names = ['Bus', 'X', 'Y']
+coord = pd.read_csv('IEEE37_BusXY.csv', names=col_names)
+x_start, x_end, y_start, y_end = [zeros(len(coord))] * 4
+
+
+
