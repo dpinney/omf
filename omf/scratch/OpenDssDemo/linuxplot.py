b2a191a6cd68283248762b08efbaa05371b96ef8
Ian George <irgeorge@gmx.com>
2018-07-05 11:30:02 +0800

    Fixing seg fault error...

--- omf/scratch/OpenDssDemo/linuxplot.py
+++ omf/scratch/OpenDssDemo/linuxplot.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as pyplot
 dss.run_command('Redirect IEEE37.dss')
 col_names = ['Bus', 'X', 'Y']
 coord = pd.read_csv('IEEE37_BusXY.csv', names=col_names)
+bus_x, bus_y = coord.iloc[:, 1].values, coord.iloc[:, 2].values
 x_start, x_end, y_start, y_end = [zeros(len(coord))] * 4
-
+#print dss.Topology.AllLoopedPairs()
 
 
