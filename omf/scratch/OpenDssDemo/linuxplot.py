3ac49efd620024c1afe242a12a4003c451cf4a63
Ian George <irgeorge@gmx.com>
2018-07-05 10:57:24 +0800

    FINALLY found something that works for network topology.

---
+++ omf/scratch/OpenDssDemo/linuxplot.py
@@ -0,0 +1,6 @@
+from numpy import *
+import opendssdirect as dss
+import matplotlib.pyplot as pyplot
+from matplotlib.collections import LineCollection
+
+print type(dss.run_command('Redirect IEEE37.dss'))
