81414dc2bf24f8041cb6181e6717c365ae0170a8
David Pinney <david@pinney.org>
2018-07-11 16:23:49 -0400

    voltPlot imported to cigar workspace

--- omf/scratch/CIGAR/drawFeeder.py
+++ omf/scratch/CIGAR/drawFeeder.py
@@ -16,10 +16,92 @@ for obj in feed.values():
 print x
 
 # Draw it.
-# omf.feeder.latLonNxGraph(omf.feeder.treeToNxGraph(feed), labels=False, neatoLayout=True, showPlot=False)
-# plt.savefig('blah.png')
+omf.feeder.latLonNxGraph(omf.feeder.treeToNxGraph(feed), labels=False, neatoLayout=True, showPlot=False)
+plt.savefig('blah.png')
 
-# Viz it.
+def voltPlot(glmPath, workDir=None, neatoLayout=False):
+	''' Draw a color-coded map of the voltage drop on a feeder.
+	Returns a matplotlib object. '''
+	tree = omf.feeder.parse(glmPath)
+	# # Get rid of schedules and climate:
+	for key in tree.keys():
+		if tree[key].get("argument","") == "\"schedules.glm\"" or tree[key].get("tmyfile","") != "":
+			del tree[key]
+	# Make sure we have a voltDump:
+	def safeInt(x):
+		try: return int(x)
+		except: return 0
+	biggestKey = max([safeInt(x) for x in tree.keys()])
+	tree[str(biggestKey*10)] = {"object":"voltdump","filename":"voltDump.csv"}
+	# Run Gridlab.
+	if not workDir:
+		workDir = tempfile.mkdtemp()
+	gridlabOut = omf.solvers.gridlabd.runInFilesystem(tree, attachments=omd.get('attachments',{}), workDir=workDir)
+	with open(pJoin(workDir,'voltDump.csv'),'r') as dumpFile:
+		reader = csv.reader(dumpFile)
+		reader.next() # Burn the header.
+		keys = reader.next()
+		voltTable = []
+		for row in reader:
+			rowDict = {}
+			for pos,key in enumerate(keys):
+				rowDict[key] = row[pos]
+			voltTable.append(rowDict)
+	# Calculate average node voltage deviation. First, helper functions.
+	def pythag(x,y):
+		''' For right triangle with sides a and b, return the hypotenuse. '''
+		return math.sqrt(x**2+y**2)
+	def digits(x):
+		''' Returns number of digits before the decimal in the float x. '''
+		return math.ceil(math.log10(x+1))
+	def avg(l):
+		''' Average of a list of ints or floats. '''
+		return sum(l)/len(l)
+	# Detect the feeder nominal voltage:
+	for key in tree:
+		ob = tree[key]
+		if type(ob)==dict and ob.get('bustype','')=='SWING':
+			feedVoltage = float(ob.get('nominal_voltage',1))
+	# Tot it all up.
+	nodeVolts = {}
+	for row in voltTable:
+		allVolts = []
+		for phase in ['A','B','C']:
+			phaseVolt = pythag(float(row['volt'+phase+'_real']),
+							   float(row['volt'+phase+'_imag']))
+			if phaseVolt != 0.0:
+				if digits(phaseVolt)>3:
+					# Normalize to 120 V standard
+					phaseVolt = phaseVolt*(120/feedVoltage)
+				allVolts.append(phaseVolt)
+		nodeVolts[row.get('node_name','')] = avg(allVolts)
+	# Color nodes by VOLTAGE.
+	fGraph = omf.feeder.treeToNxGraph(tree)
+	voltChart = plt.figure(figsize=(15,15))
+	plt.axes(frameon = 0)
+	plt.axis('off')
+	#set axes step equal
+	voltChart.gca().set_aspect('equal')
+	if neatoLayout:
+		# HACK: work on a new graph without attributes because graphViz tries to read attrs.
+		cleanG = nx.Graph(fGraph.edges())
+		cleanG.add_nodes_from(fGraph)
+		positions = graphviz_layout(cleanG, prog='neato')
+	else:
+		positions = {n:fGraph.node[n].get('pos',(0,0)) for n in fGraph}
+	edgeIm = nx.draw_networkx_edges(fGraph, positions)
+	nodeIm = nx.draw_networkx_nodes(fGraph,
+		pos = positions,
+		node_color = [nodeVolts.get(n,0) for n in fGraph.nodes()],
+		linewidths = 0,
+		node_size = 30,
+		cmap = plt.cm.jet)
+	plt.sci(nodeIm)
+	plt.clim(110,130)
+	plt.colorbar()
+	return voltChart
+
+# Viz it interactively.
 sys.path.append('../distNetViz/')
 import distNetViz
 distNetViz.viz(FNAME, forceLayout=True, outputPath=None)
