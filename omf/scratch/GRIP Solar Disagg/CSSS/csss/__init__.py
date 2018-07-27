5834de405c8c48be4441f1c5fafbb7afd3155edc
ejtalbot <erikjamestalbot@gmail.com>
2018-07-27 16:07:54 -0400

    Adding csss files for testing

---
+++ omf/scratch/GRIP Solar Disagg/CSSS/csss/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+This is the csss Package. It contains
+csss.py and solarDisagg.py
+=================================
+This package contains the basic library modules, methods, classes and
+attributes to perform Contextually Supervise Source Separation, and the subset
+of this problem: Solar Disaggregation.
+"""
+
+from csss.CSSS import *
+import csss.SolarDisagg as SolarDisagg
