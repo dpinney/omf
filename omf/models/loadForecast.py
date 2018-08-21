faa9a7ad934aa3f8e369f9a488d8d24bdc895797
Dylan S <simondylan@hotmail.com>
2018-08-21 09:06:33 -0400

    load forecast

--- omf/models/loadForecast.py
+++ omf/models/loadForecast.py
@@ -6,15 +6,9 @@
 import csv,math
 import numpy as np
 import matplotlib.pyplot as plt
-
-#standard imports
-#TODO: check which imports can be removed
-import json, os, shutil, math, requests, __neoMetaModel__
-from os.path import join as pJoin
-from jinja2 import Template
+import  __neoMetaModel__, json
 from __neoMetaModel__ import *
-from dateutil.parser import parse as parseDt
-import datetime as dt
+from omf import loadForecast
 
 # Model metadata:
 modelName, template = metadata(__file__)
@@ -25,6 +19,8 @@ def work(modelDir, inputDict):
 	''' Run the model in its directory.'''
 	outData = {}
 	rawData = []
+	forecasted = []
+	actual = []
 	with open(pJoin(modelDir,"demandTemp.csv"),"w") as demandTempFile:
 		demandTempFile.write(inputDict['demandTemp'].replace('\r',''))
 	try:
@@ -32,36 +28,44 @@ def work(modelDir, inputDict):
 			reader = csv.reader(inFile)
 			for row in reader:
 				rawData.append(row)
-	 		# if len(demandList) != 8760:
-	 		# 	raise Exception
 	except:
 		errorMessage = "CSV file is incorrect format. Please see valid format definition at <a target='_blank' href = 'https://github.com/dpinney/omf/wiki/Models-~-storagePeakShave#demand-file-csv-format'>\nOMF Wiki storagePeakShave - Demand File CSV Format</a>"
 		raise Exception(errorMessage)
 	rawData = list(np.float_(rawData)) #converts all data from float to string
-	forecasted = []
-	actual = []
-	for w in range(8760):
-		# need to start at 4 weeks+1 hour to get enough data to train so 4*7*24 = 672, the +1 is not necessary due to indexing starting at 0
-		if w>=672:
-			x = np.array([rawData[w-168][1],rawData[w-336][1],rawData[w-504][1],rawData[w-672][1]]) #training temp
-			y = np.array([rawData[w-168][0],rawData[w-336][0],rawData[w-504][0],rawData[w-672][0]]) #training demand
-			z = np.polyfit(x, y, 1)
-			p = np.poly1d(z)
-			forecasted.append((p(rawData[w][1])))
-			actual.append((rawData[w][0]))
-	for i in range(len(forecasted)):
-		if forecasted[i]>float(inputDict["upBound"]):
-			forecasted[i] = 0
-		elif forecasted[i] <float(inputDict["lowBound"]):
-			forecasted[i] = 0
-	MAE = 0		#Mean Average Error calculation
-	for i in range(len(forecasted)):
-		MAE = MAE + abs(forecasted[i]-actual[i])
-	MAE = math.trunc(MAE/len(forecasted)) 
+	(forecasted, MAE) = loadForecast.pullHourlyDayOfWeekForecast(rawData,(inputDict["upBound"]),float(inputDict["lowBound"]))
+	for x in range(len(rawData)):
+		actual.append(float(rawData[x][0]))
 	outData["actual"] = actual
 	outData["forecasted"] = forecasted
 	outData["MAE"] = MAE
 	return outData
+
+# def pullHourlyDayOfWeekForecast(rawData,upBound,lowBound):	#pullHourlyDayOfWeekForecast(rawData,inputDict["upBound"],inputDict["lowBound"])
+# 	forecasted = []
+# 	actual = []
+# 	for w in range(8760):
+# 		# need to start at 4 weeks+1 hour to get enough data to train so 4*7*24 = 672, the +1 is not necessary due to indexing starting at 0
+# 		actual.append((rawData[w][0]))
+# 		if w>=672:
+# 			x = np.array([rawData[w-168][1],rawData[w-336][1],rawData[w-504][1],rawData[w-672][1]]) #training temp
+# 			y = np.array([rawData[w-168][0],rawData[w-336][0],rawData[w-504][0],rawData[w-672][0]]) #training demand
+# 			z = np.polyfit(x, y, 1)
+# 			p = np.poly1d(z)
+# 			forecasted.append(float((p(rawData[w][1]))))
+# 		else:
+# 			forecasted.append(None)
+# 	for i in range(len(forecasted)):
+# 		if forecasted[i]>float(upBound):
+# 			forecasted[i] = None
+# 		elif forecasted[i] <float(lowBound):
+# 			forecasted[i] = None
+# 	MAE = 0		#Mean Average Error calculation
+# 	for i in range(len(forecasted)):
+# 		if forecasted[i]!=None:
+# 			MAE = MAE + abs(forecasted[i]-actual[i])
+# 	MAE = math.trunc(MAE/len(forecasted)) 
+# 	return (forecasted,MAE)
+
 
 def new(modelDir):
 	''' Create a new instance of this model. Returns true on success, false on failure. '''
@@ -69,7 +73,7 @@ def new(modelDir):
 		"user": "admin",
 		"demandTemp": open(pJoin(__neoMetaModel__._omfDir,"static","testFiles","loadForecastDefault.csv")).read(),
 		"lowBound":500,
-		"upBound":5000,
+		"upBound":3550,
 		"modelType":modelName}
 	creationCode = __neoMetaModel__.new(modelDir, defaultInputs)
 	return creationCode
