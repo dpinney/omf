944358b05b39d1486082535156f90cc557a5df20
ejtalbot <erikjamestalbot@gmail.com>
2018-08-28 15:22:05 -0400

    formatting weather regressor and charts

--- omf/scratch/GRIP Solar Disagg/csvgen.py
+++ omf/scratch/GRIP Solar Disagg/csvgen.py
@@ -7,9 +7,16 @@ meterMu = 4
 sigma = sqrt(1)
 
 #meter data
-meterRaw = np.linspace(meterMu - 3*sigma, meterMu + 3*sigma, 48)
+#0-5
+meterRaw1 = np.ones(24)
+#6-11
+meterRaw2 = np.linspace(1 + 1*sigma, meterMu + 6*sigma, 24)
+#12-17
+meterRaw3 = np.linspace(meterMu + 6*sigma, meterMu + 6*sigma, 24)
+#18-23
+meterRaw4 = np.linspace(meterMu + 9*sigma, 1 + 1*sigma, 24)
 #print(meterRaw[::-1])
-meterRaw = np.append(meterRaw, meterRaw[::-1])
+meterRaw = np.concatenate((meterRaw1, meterRaw2, meterRaw3, meterRaw4))
 
 #randomized meters
 meter1 = meterRaw + np.random.normal(0,.5,96)
@@ -19,10 +26,10 @@ meter3 = meterRaw + np.random.normal(0,.5,96)
 #solar for house
 sigma = sqrt(1)
 solarMu = -5
-solarDay =  np.linspace(solarMu + 4*sigma, solarMu - 4*sigma, 32)
+solarDay =  np.linspace(solarMu + 5*sigma, solarMu - 20*sigma, 24)
 solarDay = np.append(solarDay, solarDay[::-1])
 #print(solarDay)
-solarRaw = np.concatenate((np.zeros(16), solarDay, np.zeros(16)))
+solarRaw = np.concatenate((np.zeros(24), solarDay, np.zeros(24)))
 
 meter3 = meter3 + solarRaw
 
