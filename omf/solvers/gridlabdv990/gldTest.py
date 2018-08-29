2d39e386c2a007d27cc81f6552cc3891048df074
osboxes.org <dpinney@me.com>
2018-08-29 13:51:07 -0400

    Real fix to resilientDist

--- omf/solvers/gridlabdv990/gldTest.py
+++ omf/solvers/gridlabdv990/gldTest.py
@@ -1,4 +1,6 @@
-import subprocess
+import subprocess, os
 
-proc = subprocess.Popen('./gridlabd feeder.glm', stdout=subprocess.PIPE, shell=True)
+myEnv = os.environ.copy()
+myEnv['GLPATH'] = '/home/osboxes/Desktop/omf/omf/solvers/gridlabdv990/'
+proc = subprocess.Popen('/home/osboxes/Desktop/omf/omf/solvers/gridlabdv990/gridlabd.bin /home/osboxes/Desktop/omf/omf/scratch/feeder.glm', stdout=subprocess.PIPE, shell=True, env=myEnv)
 (out, err) = proc.communicate()
