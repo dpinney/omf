81238aade1d7ca30523869e3660e19f05f02c687
David Pinney <david@pinney.org>
2018-08-29 12:36:43 -0400

    Fixes to v990 gridlab running script.

--- omf/solvers/gridlabdv990/gldTest.py
+++ omf/solvers/gridlabdv990/gldTest.py
@@ -1,11 +1,4 @@
-import json, os, sys, tempfile, webbrowser, time, shutil, subprocess, datetime as dt, csv, math
-import traceback
-import platform, re
-import subprocess, random, webbrowser, multiprocessing
-import pprint as pprint
-import copy
-import os.path
+import subprocess
 
-
-proc = subprocess.Popen(['./gridlabd', 'feeder.glm'], stdout=subprocess.PIPE, shell=True)
+proc = subprocess.Popen('./gridlabd feeder.glm', stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
