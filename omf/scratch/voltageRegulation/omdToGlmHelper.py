192b2d3ea1fc203fcaac9eb40ef43e590823ccc2
Tuomas Talvitie <tpt5cu@virginia.edu>
2018-10-29 16:00:09 -0400

    Interim version of omdToGlm sim code

--- omf/scratch/voltageRegulation/omdToGlmHelper.py
+++ omf/scratch/voltageRegulation/omdToGlmHelper.py
@@ -5,49 +5,85 @@ from os.path import join as pJoin
 import pandas as pd
 import numpy as np
 import csv
+import omf
 
-parser = argparse.ArgumentParser(description='Converts an OMD to GLM and runs it on gridlabd')
-parser.add_argument('file_path', metavar='base', type=str,
-                    help='Path to OMD.')
-args = parser.parse_args()
-filePath = args.file_path
+#Parse Command Line
+# parser = argparse.ArgumentParser(description='Converts an OMD to GLM and runs it on gridlabd')
+# parser.add_argument('file_path', metavar='base', type=str,
+#                     help='Path to OMD.')
+# args = parser.parse_args()
+# filePath = args.file_path
 
-# def gridlabdOMD(filePath):
-filePath = '/Users/tuomastalvitie/Desktop/UCS_Egan_Housed_Solar.omd'
+# omfDIR = '/Users/tuomastalvitie/omf/omf'
+# #Import OMD, add in frequency and gridballast control properties
+# filePath = '/Users/tuomastalvitie/Desktop/UCS_Egan_Housed_Solar.omd'
+# with open(filePath, 'r') as inFile:
+# 	inFeeder = json.load(inFile)
+# 	# inFeeder['tree']['8'].update({'module residential':'implicit_enduses NONE;'})
+# 	# inFeeder['tree']['8'].update({'class player': 'double value;'})
+# 	# inFeeder['tree']['8'].update({'object player':'name frequency_values; file frequency.PLAYER1;'})
+# 	name_volt_dict ={}
+# 	for key, value in inFeeder['tree'].iteritems():
+# 		try:
+# 			# if value['object'] == 'waterheater':
+# 			# 	inFeeder['tree'][key].update({'enable_freq_control':True})
+# 			# 	inFeeder['tree'][key].update({'freq_lowlimit':59.99})
+# 			# 	inFeeder['tree'][key].update({'freq_uplimit':60.01})
+# 			# 	inFeeder['tree'][key].update({'heat_mode':'ELECTRIC'})
+# 			# 	inFeeder['tree'][key].update({'measured_frequency':'frequency_values.value'}) 
+# 			name_volt_dict[value['name']] = {'Nominal_Voltage': value['nominal_voltage']}
+# 		except KeyError:
+# 				pass
+# with open('outGLM.glm', "w") as outFile:
+# 	outFile.write(feeder.sortedWrite(inFeeder['tree']))
 
-data=json.load(open(filePath))
+# os.system('/Users/tuomastalvitie/omf/omf/solvers/gridlabd_gridballast/local_gd/bin/gridlabd outGLM.glm')
 
-with open('outGLM.glm', 'w') as f:
-	f.write(feeder.sortedWrite(data['tree']))
-os.system('/Users/tuomastalvitie/utilitySimGB/gldcore/gridlabd /Users/tuomastalvitie/omf/omf/scratch/voltageRegulation/outGLM.glm')
+# data = pd.read_csv(('voltDump.csv'), skiprows=[0])
+# for i, row in data['voltA_real'].iteritems():
+# 	voltA_real = data.loc[i,'voltA_real']
+# 	voltA_imag = data.loc[i,'voltA_imag']
+# 	voltA_mag = np.sqrt(np.add((voltA_real*voltA_real), (voltA_imag*voltA_imag)))
+# 	name_volt_dict[data.loc[i, 'node_name']].update({'Volt_A':voltA_mag})
+# 	voltB_real = data.loc[i,'voltB_real']
+# 	voltB_imag = data.loc[i,'voltB_imag']
+# 	voltB_mag = np.sqrt(np.add((voltB_real*voltB_real), (voltB_imag*voltB_imag)))
+# 	name_volt_dict[data.loc[i, 'node_name']].update({'Volt_B':voltB_mag})
+# 	voltC_real = data.loc[i,'voltC_real']
+# 	voltC_imag = data.loc[i,'voltC_imag']
+# 	voltC_mag = np.sqrt(np.add((voltC_real*voltC_real), (voltC_imag*voltC_imag)))
+# 	name_volt_dict[data.loc[i, 'node_name']].update({'Volt_C':voltC_mag})
+
+# offenders = []
+# for name, volt in name_volt_dict.iteritems():
+# 	if (float(volt['Volt_A'])/float(volt['Nominal_Voltage'])) > 1.05:
+# 		offenders.append(name)
+# 	if (float(volt['Volt_B'])/float(volt['Nominal_Voltage'])) > 1.05:
+# 		offenders.append(name)
+# 	if (float(volt['Volt_C'])/float(volt['Nominal_Voltage'])) > 1.05:
+# 		offenders.append(name)
+
+# offenders = list(set(offenders))
+# offenders = "object offenders {" + str(offenders) + "};"
+
+# print offenders
+
+# with open('outGLM.glm','wr') as glmFile:
+# 			newfile = glmFile.read()
+# 			newfile = newfile + offenders
+# 			print data
+# 			print type(data)
 
 
-data = pd.read_csv('voltDump.csv', skiprows=[0])
 
-# print data.head()
-# print list(data)
+# with open('offenders.csv', 'w') as f:
+# 	wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+# 	wr.writerow(offenders)
 
-offenders = []
-for i, row in data['voltA_real'].iteritems():
-	voltA_real = data.loc[i,'voltA_real']
-	voltA_imag = data.loc[i,'voltA_imag']
-	voltA_mag = np.sqrt(np.add((voltA_real*voltA_real), (voltA_imag*voltA_imag)))
-	voltB_real = data.loc[i,'voltB_real']
-	voltB_imag = data.loc[i,'voltB_imag']
-	voltB_mag = np.sqrt(np.add((voltB_real*voltB_real), (voltB_imag*voltB_imag)))
-	voltC_real = data.loc[i,'voltC_real']
-	voltC_imag = data.loc[i,'voltC_imag']
-	voltC_mag = np.sqrt(np.add((voltC_real*voltC_real), (voltC_imag*voltC_imag)))
-	if (voltA_mag or voltB_mag or voltC_real) > (1.05 * 14400):
-		offenders.append(data['node_name'][i])
 
-		
-offenders = list(set(offenders))
+omf.distNetViz.viz('outGLM.glm') #or model.omd
 
-with open('offenders.csv', 'w') as f:
-	wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-	wr.writerow(offenders)
 
-print offenders
+
 
 
