ef81639a535b519e360ebda32e236a8b53693a87
kmcelwee <kevinrmcelwee@gmail.com>
2018-11-02 12:59:51 -0400

    Corrected LCOE calculation

--- omf/models/storagePeakShave.py
+++ omf/models/storagePeakShave.py
@@ -15,15 +15,13 @@ tooltip = ("The storagePeakShave model calculates the value of a distribution ut
 
 def work(modelDir, inputDict):
 	''' Model processing done here. '''
-	outData = {}  # See bottom of file for outData's structure
+	out = {}  # See bottom of file for out's structure
 	(cellCapacity, dischargeRate, chargeRate, cellQuantity, demandCharge, cellCost) = \
 		[float(inputDict[x]) for x in ('cellCapacity', 'dischargeRate', 'chargeRate',
 			'cellQuantity', 'demandCharge', 'cellCost')]
 	dispatchStrategy = str(inputDict.get('dispatchStrategy'))
 	retailCost = float(inputDict.get('retailCost', 0.07))
 	projYears = int(inputDict.get('projYears', 10))
-	startPeakHour = int(inputDict.get('startPeakHour', 18))
-	endPeakHour = int(inputDict.get('endPeakHour', 24))
 	batteryCycleLife = int(inputDict.get('batteryCycleLife', 5000))
 	discountRate = float(inputDict.get('discountRate', 2.5)) / 100.0
 	dodFactor = float(inputDict.get('dodFactor', 85)) / 100.0
@@ -33,23 +31,22 @@ def work(modelDir, inputDict):
 	# Note: inverterEfficiency is squared to get round trip efficiency.
 	# battEff = float(inputDict.get('batteryEfficiency', 92)) / 100.0 * (inverterEfficiency ** 2)
 
-	with open(pJoin(modelDir, 'demand.csv'), 'w') as demandFile:
-		demandFile.write(inputDict['demandCurve'])
+	with open(pJoin(modelDir, 'demand.csv'), 'w') as f:
+		f.write(inputDict['demandCurve'])
 	if dispatchStrategy == 'customDispatch':
-		with open(pJoin(modelDir, 'dispatchStrategy.csv'), 'w') as customDispatchFile:
-			customDispatchFile.write(inputDict['customDispatchStrategy'])
+		with open(pJoin(modelDir, 'dispatchStrategy.csv'), 'w') as f:
+			f.write(inputDict['customDispatchStrategy'])
 
 	dc = [] # main data table
 	try:
-		dates = [(dt(2011,1,1)+timedelta(hours=1)*x) for x in range(8760)]
-		with open(pJoin(modelDir, 'demand.csv')) as inFile:
-			reader = csv.reader(inFile)
+		dates = [(dt(2011,1,1) + timedelta(hours=1)*x) for x in range(8760)]
+		with open(pJoin(modelDir, 'demand.csv')) as f:
+			reader = csv.reader(f)
 			for row, date in zip(reader, dates):
-				dc.append({
-						'datetime': date, 
-						'power': float(row[0]), # row is a list of length 1
-						'month': date.month - 1,
-						'hour': date.hour })
+				dc.append({	'datetime': date, 
+							'power': float(row[0]), # row is a list of length 1
+							'month': date.month - 1,
+							'hour': date.hour })
 		assert len(dc) == 8760
 	except:
 		if str(sys.exc_info()[0]) != "<type 'exceptions.SystemExit'>":
@@ -66,37 +63,31 @@ def work(modelDir, inputDict):
 	battCharge = cellQuantity * chargeRate
 
 	# calculate battery's effect and input netpower and battSoC into dc
+	SoC = battCapacity  # Battery state of charge; begins full.
 	if dispatchStrategy == 'optimal':
-		battSoC = battCapacity  # Battery state of charge; begins full.
-		for row in dc:
-			powerUnderPeak = monthlyPeakDemand[row['month']] - row['power'] - battDischarge
-			if powerUnderPeak > 0:
-				charge = min(powerUnderPeak, battCharge, battCapacity - battSoC)
-			else:
-				charge = -1 * min(abs(powerUnderPeak), battDischarge, battSoC)
-			battSoC += charge
-			row['netpower'] = row['power'] + charge # could be positive or negative
-			row['battSoC'] = battSoC
+		for r in dc:
+			powerUnderPeak = monthlyPeakDemand[r['month']] - r['power'] - battDischarge
+			charge = (min(powerUnderPeak, battCharge, battCapacity-SoC) if powerUnderPeak > 0 
+				else -1 * min(abs(powerUnderPeak), battDischarge, SoC))
+			SoC += charge
+			r['netpower'] = r['power'] + charge
+			r['battSoC'] = SoC
 	elif dispatchStrategy == 'daily':
-		battSoC = battCapacity
-		for row in dc:
-			discharge = min(battDischarge, battSoC)
-			charge = min(battCharge, battCapacity - battSoC)
-			# If hour is within peak hours and the battery has charge
-			if startPeakHour <= row['hour'] <= endPeakHour:
-				row['netpower'] = row['power'] - discharge
-				battSoC -= discharge
-			else:
-			# If hour is outside peak hours and the battery isnt fully charged, charge it
-				row['netpower'] = row['power'] + charge
-				battSoC += charge
-			row['battSoC'] = battSoC
-	elif dispatchStrategy == 'customDispatch': # Custom dispatch.
+		start = int(inputDict.get('startPeakHour', 18))
+		end = int(inputDict.get('endPeakHour', 24))
+		for r in dc:
+			# Discharge if hour is within peak hours otherwise charge
+			charge = (-1*min(battDischarge, SoC) if start <= r['hour'] <= end 
+				else min(battCharge, battCapacity - SoC))
+			r['netpower'] = r['power'] + charge
+			SoC += charge
+			r['battSoC'] = SoC
+	elif dispatchStrategy == 'customDispatch':
 		try:
 			with open(pJoin(modelDir,'dispatchStrategy.csv')) as strategyFile:
 				reader = csv.DictReader(strategyFile)
-				for d, row in zip(dc, reader):
-					d['dispatch'] = int(row['dispatch'])
+				for d, r in zip(dc, reader):
+					d['dispatch'] = int(r['dispatch'])
 				assert all(['dispatch' in r for r in dc])  # ensure each row is filled
 		except:
 			if str(sys.exc_info()[0]) != "<type 'exceptions.SystemExit'>":
@@ -106,19 +97,13 @@ def work(modelDir, inputDict):
 					"Models-~-storagePeakShave#custom-dispatch-strategy-file-"
 					"csv-format'>\nOMF Wiki storagePeakShave - Custom "
 					"Dispatch Strategy File Format</a>")
-		battSoC = battCapacity
-		for row in dc:
-			discharge = min(battDischarge, battSoC)
-			charge = min(battCharge, battCapacity - battSoC)
-			# If there is a 1 in the dispatch strategy csv, the battery discharges
-			if row['dispatch'] == 1:
-				row['netpower'] = row['power'] - discharge
-				battSoC -= discharge
-			# Otherwise charge the battery.
-			else:
-				row['netpower'] = row['power'] + charge
-				battSoC += charge
-			row['battSoC'] = battSoC
+		for r in dc:
+			# Discharge f there is a 1 in the dispatch strategy csv, otherwise charge the battery.
+			charge = (-1*min(battDischarge, SoC) if r['dispatch'] == 1 
+				else min(battCharge, battCapacity-SoC))
+			r['netpower'] = r['power'] + charge
+			SoC += charge
+			r['battSoC'] = SoC
 	else:
 		raise Exception("Invalid dispatch input.")
 
@@ -129,51 +114,51 @@ def work(modelDir, inputDict):
 	dischargeByMonth = [[i-j for i, j in zip(k, l) if i-j > 0] for k, l in zip(netByMonth, demandByMonth)]
 
 	# Monthly Cost Comparison Table
-	outData['monthlyDemand'] = [sum(lDemand)/1000 for lDemand in demandByMonth]
-	outData['monthlyDemandRed'] = [t - p for t, p in zip(outData['monthlyDemand'], ps)]
-	outData['ps'] = ps
-	outData['kWhtoRecharge'] = [sum(discharges) for discharges in dischargeByMonth]
-	outData['benefitMonthly'] = [x * demandCharge for x in ps]
-	outData['costtoRecharge'] = [retailCost * x for x in outData['kWhtoRecharge']]
-	outData['benefitNet'] = [b - c for b, c in zip(outData['benefitMonthly'], outData['costtoRecharge'])]
+	out['monthlyDemand'] = [sum(lDemand)/1000 for lDemand in demandByMonth]
+	out['monthlyDemandRed'] = [t-p for t, p in zip(out['monthlyDemand'], ps)]
+	out['ps'] = ps
+	out['kWhtoRecharge'] = [sum(d) for d in dischargeByMonth]
+	out['benefitMonthly'] = [x*demandCharge for x in ps]
+	out['costtoRecharge'] = [x*retailCost for x in out['kWhtoRecharge']]
+	out['benefitNet'] = [b-c for b, c in zip(out['benefitMonthly'], out['costtoRecharge'])]
 
 	# Demand Before and After Storage Graph
-	outData['demand'] = [t['power']*1000.0 for t in dc]
-	outData['demandAfterBattery'] = [t['netpower']*1000.0 for t in dc]
-	outData['batteryDischargekW'] = [d - dab for d, dab in zip(outData['demand'], outData['demandAfterBattery'])]
-	outData['batteryDischargekWMax'] = max(outData['batteryDischargekW'])
+	out['demand'] = [t['power']*1000.0 for t in dc]
+	out['demandAfterBattery'] = [t['netpower']*1000.0 for t in dc]
+	out['batteryDischargekW'] = [d-b for d, b in zip(out['demand'], out['demandAfterBattery'])]
+	out['batteryDischargekWMax'] = max(out['batteryDischargekW'])
 
 	# Battery State of Charge Graph
 	# Turn dc's SoC into a percentage, with dodFactor considered.
-	outData['batterySoc'] = SoC = [t['battSoC']/battCapacity*100.0*dodFactor + (100-100*dodFactor) for t in dc]
+	out['batterySoc'] = SoC = [t['battSoC']/battCapacity*100*dodFactor + (100-100*dodFactor) for t in dc]
 	# Estimate number of cyles the battery went through.
 	cycleEquivalents = len([c for i, c in enumerate(SoC[:-1]) if SoC[i] < 100 and SoC[i+1] == 100])
-	outData['cycleEquivalents'] = cycleEquivalents
-	outData['batteryLife'] = batteryCycleLife / cycleEquivalents
+	out['cycleEquivalents'] = cycleEquivalents
+	out['batteryLife'] = batteryCycleLife / cycleEquivalents
 
 	# Cash Flow Graph
 	# cashFlowCurve is $ in from peak shaving minus the cost to recharge the battery every day of the year
-	totalYearlyCharge = sum(outData['kWhtoRecharge'])
+	totalYearlyCharge = sum(out['kWhtoRecharge'])
 	cashFlowCurve = [(sum(ps) * demandCharge)-(totalYearlyCharge*retailCost) for year in range(projYears)]
 	cashFlowCurve.insert(0, -1 * cellCost * cellQuantity)  # insert initial investment
 	# simplePayback is also affected by the cost to recharge the battery every day of the year
-	outData['SPP'] = (cellCost*cellQuantity)/((sum(ps)*demandCharge)-(totalYearlyCharge*retailCost))
-	outData['netCashflow'] = cashFlowCurve
-	outData['cumulativeCashflow'] = [sum(cashFlowCurve[:i+1]) for i, d in enumerate(cashFlowCurve)]
-	outData['NPV'] = npv(discountRate, cashFlowCurve)
+	out['SPP'] = (cellCost*cellQuantity)/((sum(ps)*demandCharge)-(totalYearlyCharge*retailCost))
+	out['netCashflow'] = cashFlowCurve
+	out['cumulativeCashflow'] = [sum(cashFlowCurve[:i+1]) for i, d in enumerate(cashFlowCurve)]
+	out['NPV'] = npv(discountRate, cashFlowCurve)
 
-	battCostPerCycle = cellQuantity * cellCapacity * cellCost / batteryCycleLife
+	battCostPerCycle = cellQuantity * cellCost / batteryCycleLife
 	lcoeTotCost = cycleEquivalents*retailCost + battCostPerCycle*cycleEquivalents
-	outData['LCOE'] = lcoeTotCost / (cycleEquivalents * cellQuantity * cellCapacity)
+	out['LCOE'] = lcoeTotCost / (cycleEquivalents*battCapacity)
 
 	# Other
-	outData['startDate'] = '2011-01-01'  # dc[0]['datetime'].isoformat()
-	outData['stderr'] = ''
+	out['startDate'] = '2011-01-01'  # dc[0]['datetime'].isoformat()
+	out['stderr'] = ''
 	# Seemingly unimportant. Ask permission to delete.
-	outData['stdout'] = 'Success' 
-	outData['months'] = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+	out['stdout'] = 'Success' 
+	out['months'] = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
 
-	return outData
+	return out
 
 def new(modelDir):
 	''' Create a new instance of this model. Returns true on success, false on failure. '''
@@ -191,7 +176,7 @@ def new(modelDir):
 		'dischargeRate': '5',
 		'chargeRate': '5',
 		'demandCurve': open(pJoin(__neoMetaModel__._omfDir,'static','testFiles','FrankScadaValidCSV_Copy.csv')).read(),
-		'dispatchStrategy': 'optimal',
+		'dispatchStrategy': 'daily',
 		'cellCost': '7140',
 		'cellQuantity': '10',
 		'projYears': '15',
