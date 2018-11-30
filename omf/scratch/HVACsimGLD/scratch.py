1987580f0d2d19400d0997ec3f0d5fd030fbeed9
Tuomas Talvitie <tpt5cu@virginia.edu>
2018-11-30 19:01:27 +1000

    Changes to allow reading a .glm
    With gridballast control

---
+++ omf/scratch/HVACsimGLD/scratch.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.system('/Users/tuomastalvitie/omf/omf/solvers/gridlabd_gridballast/local_gd/bin/gridlabd hvac_gridlabd_sim.glm')
--- omf/scratch/voltageRegulation/omdToGlmHelper.py
+++ omf/scratch/voltageRegulation/omdToGlmHelper.py
@@ -20,27 +20,28 @@ filePath = '/Users/tuomastalvitie/Desktop/UCS_Egan_Housed_Solar.omd'
 with open(filePath, 'r') as inFile:
 	inFeeder = json.load(inFile)
 	# inFeeder['tree']['25103'] = {'#include "hot_water_demand1.glm";':""}
-	# inFeeder['tree'][u'25103'] = {u'omftype': u'#include', u'argument': u'"hot_water_demand1.glm"'}
+	inFeeder['tree'][u'01'] = {u'omftype': u'#include', u'argument': u'"hot_water_demand1.glm"'}
 	# inFeeder['tree']['25105'] = {'class player': 'double value;'}
-	# inFeeder['tree'][u'25105'] = {u'class': u'player', u'double': u'value'}# add in manually for now
+	inFeeder['tree'][u'001'] = {u'class': u'player', u'double': u'value'}# add in manually for now
 	name_volt_dict ={}
 	for key, value in inFeeder['tree'].iteritems():
 		try:#disable freq control
-		# 	if (value['object'] == 'waterheater'):
-		# 		inFeeder['tree'][key].update({'heat_mode':'ELECTRIC'})
-		# 		inFeeder['tree'][key].update({'enable_volt_control':'true'})
-		# 		inFeeder['tree'][key].update({'volt_lowlimit':'113.99'})
-		# 		inFeeder['tree'][key].update({'volt_uplimit':'126.99'}) 
-		# 	elif (value['object']== 'ZIPload'):
-		# 		inFeeder['tree'][key].update({'enable_volt_control':'true'})
-		# 		inFeeder['tree'][key].update({'volt_lowlimit':'113.99'})
-		# 		inFeeder['tree'][key].update({'volt_uplimit':'126.99'})
+			if (value['object'] == 'waterheater'):
+				inFeeder['tree'][key].update({'heat_mode':'ELECTRIC'})
+				inFeeder['tree'][key].update({'enable_volt_control':'true'})
+				inFeeder['tree'][key].update({'volt_lowlimit':'113.99'})
+				inFeeder['tree'][key].update({'volt_uplimit':'126.99'}) 
+			elif (value['object']== 'ZIPload'):
+				inFeeder['tree'][key].update({'enable_volt_control':'true'})
+				inFeeder['tree'][key].update({'volt_lowlimit':'113.99'})
+				inFeeder['tree'][key].update({'volt_uplimit':'126.99'})
 			name_volt_dict[value['name']] = {'Nominal_Voltage': value['nominal_voltage']}
 		except KeyError:
 				pass
-# with open('outGLM.glm', "w") as outFile:
-# 	outFile.write(feeder.sortedWrite(inFeeder['tree']))
-os.system('/Users/tuomastalvitie/omf/omf/solvers/gridlabd_gridballast/local_gd/bin/gridlabd outGLM.glm')
+with open('outGLMtest.glm', "w") as outFile:
+	outFile.write(feeder.sortedWrite(inFeeder['tree']))
+os.system('/Users/tuomastalvitie/omf/omf/solvers/gridlabd_gridballast/local_gd/bin/gridlabd outGLMtest.glm')
+
 
 data = pd.read_csv(('voltDump.csv'), skiprows=[0])
 for i, row in data['voltA_real'].iteritems():
@@ -60,18 +61,40 @@ for i, row in data['voltA_real'].iteritems():
 offenders = []
 for name, volt in name_volt_dict.iteritems():
 	if (float(volt['Volt_A'])/float(volt['Nominal_Voltage'])) > 1.05:
-		offenders.append(name)
+		offenders.append(tuple([name, float(volt['Volt_A'])/float(volt['Nominal_Voltage'])]))
 	if (float(volt['Volt_B'])/float(volt['Nominal_Voltage'])) > 1.05:
-		offenders.append(name)
+		offenders.append(tuple([name, float(volt['Volt_B'])/float(volt['Nominal_Voltage'])]))
 	if (float(volt['Volt_C'])/float(volt['Nominal_Voltage'])) > 1.05:
-		offenders.append(name)
+		offenders.append(tuple([name, float(volt['Volt_C'])/float(volt['Nominal_Voltage'])]))
+
+# #Old Code for name recording only
+# # offenders = []
+# # for name, volt in name_volt_dict.iteritems():
+# # 	if (float(volt['Volt_A'])/float(volt['Nominal_Voltage'])) > 1.05:
+# # 		offenders.append(name)
+# # 	if (float(volt['Volt_B'])/float(volt['Nominal_Voltage'])) > 1.05:
+# # 		offenders.append(name)
+# # 	if (float(volt['Volt_C'])/float(volt['Nominal_Voltage'])) > 1.05:
+# # 		offenders.append(name)
+
 
 offenders = list(set(offenders))
 print len(offenders)
 
-with open('offenders.csv', 'w') as f:
-	wr = csv.writer(f, quoting=csv.QUOTE_ALL)
-	wr.writerow(offenders)
+isum = 0
+offendersNames = []
+for i in range(len(offenders)):
+	isum = isum + offenders[i][1]
+	offendersNames.append(offenders[i][0])
+print ("average voltage overdose is by a factor of", isum/(len(offenders)))
+print len(offendersNames)
+
+
+
+
+# with open('offenders.csv', 'w') as f:
+# 	wr = csv.writer(f, quoting=csv.QUOTE_ALL)
+# 	wr.writerow(offenders)
 
 
 omf.distNetViz.viz('outGLM.glm') #or model.omd
