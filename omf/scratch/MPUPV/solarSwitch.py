669e2320352d171ad53e42dc5cf8527403c23f25
David Pinney <david@pinney.org>
2019-01-10 16:39:51 -0500

    Using the CIGAR version of cosim.py

--- omf/scratch/MPUPV/solarSwitch.py
+++ omf/scratch/MPUPV/solarSwitch.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../CIGAR')
 from cosim import GridLabWorld
 
 glw = GridLabWorld('6267', 'localhost', 'GC-solarAdd.glm', '2000-01-01 0:00:00')
@@ -13,4 +15,3 @@ print (glw.readClock())
 glw.resume()
 print (glw.readClock())
 #glw.shutdown()
-proc.kill()
