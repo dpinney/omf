49883b6df2c4b1514c667d8747e956714ed02cb8
kmcelwee <kevinrmcelwee@gmail.com>
2019-02-18 15:01:48 -0500

    combined models, deleted where necessary

--- omf/loadForecast.py
+++ omf/loadForecast.py
@@ -733,10 +733,10 @@ def pulp24hrBattery(demand, power, energy, battEff):
 	model = pulp.LpProblem("Daily demand charge minimization problem", pulp.LpMinimize)
 	VBpower = pulp.LpVariable.dicts(
 		"ChargingPower", range(24)
-	)  # decision variable of VB charging power; dim: 8760 by 1
+	)  # decision variable of VB charging power; dim: 24 by 1
 	VBenergy = pulp.LpVariable.dicts(
 		"EnergyState", range(24)
-	)  # decision variable of VB energy state; dim: 8760 by 1
+	)  # decision variable of VB energy state; dim: 24 by 1
 
 	for i in range(24):
 		VBpower[i].lowBound = -power
