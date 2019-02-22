3f959e201144847c4c13af5ea2bb1c34ec0a51a5
Katrina Francis <klf2133@columbia.edu>
2019-02-22 08:44:45 +0000

    Fleshed out anomalyDetector

--- omf/anomalyDetection.py
+++ omf/anomalyDetection.py
@@ -4,37 +4,45 @@ from omf.loadForecast import suppress_stdout_stderr
 from os.path import join as pJoin
 import os
 
-def train_prophet(df, modelDir, interval_width=0.99):
+
+def train_prophet(df, modelDir, confidence=0.99):
 	# train and cache into modelDir
 	m = Prophet(
-		yearly_seasonality=True, daily_seasonality=True, interval_width=interval_width
+		yearly_seasonality=True, daily_seasonality=True, interval_width=confidence
 	)
-	m.add_country_holidays(country_name='US')
 	with suppress_stdout_stderr():
 		m.fit(df)
 
-	# Predict the future.
+		# Predict the future.
 	print "PREDICTING!"
 	future = m.make_future_dataframe(periods=0)
 	forecast = m.predict(future)
 	# Merge in the historical data.
-	forecast["y"] = [float(x) for x in df["y"]]
-	forecast["outlier"] = [False for x in xrange(len(forecast))]
+	forecast["y"] = df.y.astype(float)
 	# Backup the model.
-	forecast.to_csv(pJoin(modelDir, "forecasted.csv"))
+	forecast.to_csv(
+		pJoin(modelDir, "forecasted_{}.csv".format(confidence)), index=False
+	)
 	return forecast
 
-def detect_outliers(df, modelDir):
+
+def prophet(df, modelDir, confidence=0.99, cached=""):
 	# load cached df if exists, or call train_prophet
-	try:
-		forecast = pd.read_csv(pJoin(modelDir, "forecasted.csv"))
-	except IOError:
-		forecast = train_prophet(df, modelDir)
-	# detect outliers
-	for index, row in forecast.iterrows():
-		yVal = float(row["y"])
-		yHigh = float(row["yhat_upper"])
-		yLow = float(row["yhat_lower"])
-		if (yVal > yHigh) or (yVal < yLow):
-			forecast.at[index, "outlier"] = True
+	if cached and os.path.isfile(pJoin(modelDir, cached)):
+		forecast = pd.read_csv(pJoin(modelDir, cached))
+	else:
+		forecast = train_prophet(df, modelDir, confidence)
+		# detect outliers
+	cols = ['y', 'yhat', 'yhat_lower', 'yhat_upper']
+	forecast[cols] = forecast[cols].astype(float)
+	forecast['outlier'] = (forecast.y > forecast.yhat_upper) | (forecast.y < forecast.yhat_lower)
 	return forecast
+
+def elliptic_envelope(df, modelDir):
+	from sklearn.covariance import EllipticEnvelope
+	if 'ds' in df.columns:
+		del df['ds']
+	model = EllipticEnvelope()
+	df.outlier = model.fit_predict(df.values)
+	df.outlier = df.outlier < 0 # 1 if inlier, -1 if outlier
+	return df
--- omf/models/anomalyDetector.py
+++ omf/models/anomalyDetector.py
@@ -1,4 +1,4 @@
-''' Calculate the costs and benefits of energy storage from a distribution utility perspective. '''
+""" Calculate the costs and benefits of energy storage from a distribution utility perspective. """
 
 import os, sys, shutil, csv, StringIO
 import omf.anomalyDetection
@@ -11,50 +11,114 @@ from __neoMetaModel__ import *
 
 # Model metadata:
 modelName, template = metadata(__file__)
-tooltip = ("The storagePeakShave model calculates the value of a distribution utility " 
-	"deploying energy storage based on three possible battery dispatch strategies.")
+tooltip = (
+	"The storagePeakShave model calculates the value of a distribution utility "
+	"deploying energy storage based on three possible battery dispatch strategies."
+)
+
 
 def work(modelDir, inputDict):
-	''' Model processing done here. '''
-	out = {}  # See bottom of file for out's structure
-	f = StringIO.StringIO(inputDict['file'])
+	""" Model processing done here. """
+
+	cached_file_name = "input_data_{}.csv".format(inputDict["confidence"])
+	cached_file_path = pJoin(modelDir, cached_file_name)
+
+	# try to get our cached input
+	try:
+		with open(cached_file_path, "r") as f:
+			cached_str = f.read()
+	except IOError:
+		cached_str = ""
+
+	confidence = float(inputDict["confidence"]) if inputDict.get("confidence") else 0.99
+	if confidence >= 1:
+		raise Exception(
+			"Value for Confidence Level Out of Bounds (must be less than 1)"
+		)
+
+		# do we have a cached forecast?
+	cached = (
+		"forecasted_{}.csv".format(confidence)
+		if cached_str == inputDict["file"]
+		else None
+	)
+
+	with open(cached_file_path, "w") as f:
+		f.write(inputDict["file"])
+
+	out = {}
+
+	# load our csv to df
+	f = StringIO.StringIO(inputDict["file"])
 	df = pd.read_csv(f)
-	df.columns = ['y']
-	df['ds'] = pd.date_range(start = inputDict['startDate'], freq = "H", periods = df.shape[0])
-	forecast = omf.anomalyDetection.detect_outliers(df, modelDir)	
-	out['y'] = list(forecast.y.values)
-	out['yhat'] = list(forecast.yhat.values)
-	out['yhat_upper'] = list(forecast.yhat_upper.values)
-	out['yhat_lower'] = list(forecast.yhat_lower.values)
-	out['outlier'] = list(forecast.outlier.values.astype(int))
-	out['startDate'] = inputDict['startDate']
+
+	# try to use user input to remap column
+	df = df.rename(columns={inputDict["yLabel"]: "y"})
+	if "y" not in df.columns:
+		df = df.rename(columns = {df.columns[0]: "y"})
+
+	# add our boy for prophet
+	df["ds"] = pd.date_range(
+		start=inputDict["startDate"], freq="H", periods=df.shape[0]
+	)
+
+	prophet_df = omf.anomalyDetection.prophet(
+		df[["ds", "y"]], modelDir, confidence=confidence, cached=cached
+	)
+
+	elliptic_df = omf.anomalyDetection.elliptic_envelope(df, modelDir)
+
+	out["y"] = list(prophet_df.y.values)
+	out["yhat"] = list(prophet_df.yhat.values)
+	out["yhat_upper"] = list(prophet_df.yhat_upper.values)
+	out["yhat_lower"] = list(prophet_df.yhat_lower.values)
+	out["prophet_outlier"] = list(prophet_df.outlier.values.astype(int))
+	out["elliptic_outlier"] = list(elliptic_df.outlier.astype(int))
+	out["startDate"] = inputDict["startDate"]
 	return out
 
+
 def new(modelDir):
-	''' Create a new instance of this model. Returns true on success, false on failure. '''
+	""" Create a new instance of this model. Returns true on success, false on failure. """
 	defaultInputs = {
-		'created': '2015-06-12 17:20:39.308239',
-		'modelType': modelName,
-		'file': open(pJoin(__neoMetaModel__._omfDir,'static','testFiles','ERCOT_south_shortened_no_temp.csv')).read(),
-		'fileName': 'ERCOT_south_shortened_no_weather.csv',
-		'startDate': '2002-01-01',
+		"created": "2015-06-12 17:20:39.308239",
+		"modelType": modelName,
+		"file": open(
+			pJoin(
+				__neoMetaModel__._omfDir,
+				"static",
+				"testFiles",
+				"ERCOT_south_shortened.csv",
+			)
+		).read(),
+		"fileName": "ERCOT_south_shortened.csv",
+		"confidence": "0.99",
+		"startDate": "2002-01-01",
 	}
 	return __neoMetaModel__.new(modelDir, defaultInputs)
 
+
 def _tests():
-	modelLoc = pJoin(__neoMetaModel__._omfDir,'data','Model','admin','Automated Testing of ' + modelName)
+	modelLoc = pJoin(
+		__neoMetaModel__._omfDir,
+		"data",
+		"Model",
+		"admin",
+		"Automated Testing of " + modelName,
+	)
 	# Blow away old test results if necessary.
 	if isdir(modelLoc):
-		shutil.rmtree(modelLoc)	
+		shutil.rmtree(modelLoc)
 	new(modelLoc)  # Create New.
 	renderAndShow(modelLoc)  # Pre-run.
 	runForeground(modelLoc)  # Run the model.
 	renderAndShow(modelLoc)  # Show the output.
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 	_tests()
 
-'''
+"""
 outDic {
 	startdate: str
 	stdout: "Success"
@@ -94,4 +158,4 @@ outDic {
 	#   plt.axvline(hoursThroughTheMonth[month])
 	# plt.savefig(pJoin(modelDir,"plot.png"))
 
-'''
+"""
