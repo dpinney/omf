e1a2e1c874a95ee41d6d5b6b67465442c80cc1fc
Austin Chang <16790624+ASC95@users.noreply.github.com>
2019-03-09 19:34:49 -0500

    wrote script to transform glm files into component.json files

---
+++ omf/scratch/glmToJSON.py
@@ -0,0 +1,69 @@
+import os, glob, json
+from omf import feeder
+
+output_dir = "/Users/austinchang/pycharm/omf/omf/data/Component/testDump"
+created = []
+overwritten = []
+not_overwritten = []
+lacked_name = []
+
+# TODO: improve comments, add tests with weird file names (e.g. file names with spaces, slashes, etc.)
+
+def main(src_directory, overwrite=False):
+	dictionaries = read_glm_files(src_directory)
+	for d in dictionaries:
+		create_components(d, overwrite)
+	print_report(overwrite)
+
+def read_glm_files(directory):
+	""" Read each .glm file and return a dictionary of dictionaries
+	"""
+	os.chdir(directory)
+	return [feeder.parse(file_path) for file_path in glob.glob("*.glm")]
+
+def create_components(dictionary, overwrite):
+	""" Create component.json files based on the provided dictionary.
+	Existing component.json files with matching file names can be preserved or overwritten.
+	"""
+	for d in dictionary.values():
+		object_type = d.get("object")
+		if object_type is not None:
+			# I cannot have the "/" in a file name.
+			name = d.get("name", "no_name").strip('"')
+			if name != "no_name":
+				d["name"] = name
+			name = name.replace("/", ",")
+			file_name = object_type + "-" + name + ".json"
+			if name == "no_name":
+				lacked_name.append(file_name)
+			path = os.path.join(output_dir, file_name)
+			if not os.path.exists(path):
+				created.append(file_name)
+				with open(path, "w") as f:
+					json.dump(d, f)
+			elif not overwrite:
+				not_overwritten.append(file_name)
+			else:
+				overwritten.append(file_name)
+				with open(path, "w") as f:
+					json.dump(d, f)
+
+def print_report(overwrite):
+	if len(lacked_name) > 0:
+		print("\n********** The following objects lacked the 'name' property **********\n")
+		for path in lacked_name:
+			print(path)
+	if overwrite:
+		print("\n********** The following files were overwritten **********\n")
+		for path in overwritten:
+			print(path)
+	else:
+		print("\n********** The following files existed and were not overwritten **********\n")
+		for path in not_overwritten:
+			print(path)
+	print("\n********** The following files were created **********\n")
+	for path in created:
+		print(path)
+
+if __name__ == "__main__":
+	main("/Users/austinchang/Desktop/testfiles/components", True)
