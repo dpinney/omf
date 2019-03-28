062e3ba25cda980f7a9ab9faaaac029030ef58e9
David Pinney <david@pinney.org>
2019-03-28 13:28:43 -0400

    more switching stuff

--- omf/scratch/smartSwitching/hello.py
+++ omf/scratch/smartSwitching/hello.py
@@ -1,7 +1,18 @@
 import json, omf
 
 # Visualize the circuit.
-omf.distNetViz.viz('trip37.glm')
+# omf.distNetViz.viz('trip37.glm')
 
 # Read it in.
 tree = omf.feeder.parse('trip37.glm')
+
+# Modify all line lengths.
+for key in tree:
+	if tree[key].get('object','') == 'overhead_line':
+		print tree[key]['name']
+		tree[key]['length'] = '5'
+
+# Write new output.
+with open('trip37tinyLines.glm','w') as outFile:
+	myStr = omf.feeder.sortedWrite(tree)
+	outFile.write(myStr)
