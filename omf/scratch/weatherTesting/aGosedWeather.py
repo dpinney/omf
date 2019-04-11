7865d0a432833e35ad3a648f62426e6b9b24d04a
David Pinney <david@pinney.org>
2019-04-11 12:42:41 -0400

    Pretty decent weather player script.

--- omf/scratch/weatherTesting/aGosedWeather.py
+++ omf/scratch/weatherTesting/aGosedWeather.py
@@ -1,27 +1,55 @@
 ''' Pull down historical weather data, put it on a SCADA-calibrated feeder. '''
 
 from matplotlib import pyplot as plt
-import sys, json
+import sys, json, csv
 from omf import feeder, weather
 from omf.solvers.gridlabd import runInFilesystem
+from datetime import datetime, timedelta
+
+# Globals
+INIT_TIME = datetime(2017,1,1,0,0,0)
+CSV_NAME = 'weatherNewKy.csv'
 
 # Make a weather file.
-# weather.makeClimateCsv('2014-01-01', '2014-01-11', 'MSP', './mspWeather.csv')
-with open('@newWeather.csv','w') as wFile:
-	# data = weather.pullUscrn('2017', 'KY_Versailles_3_NNW', 'T_CALC')
-	data = weather.pullAsos('2017', 'CHO', 'tmpc')
-	print data
-raise Exception
+data_temp = weather.pullUscrn('2017', 'KY_Versailles_3_NNW', 'T_CALC')
+data_hum = weather.pullUscrn('2017', 'KY_Versailles_3_NNW', 'RH_HR_AVG')
+data_solar = weather.pullUscrn('2017', 'KY_Versailles_3_NNW', 'SOLARAD')
+init_time = datetime(2017,1,1,0,0,0)
+data_full = []
+for i in range(8760):
+	step_time = INIT_TIME + timedelta(hours=i)
+	row = [
+		'{}:{}:{}:{}:{}'.format(step_time.month, step_time.day, step_time.hour, step_time.minute, step_time.second),
+		# str(step_time), 
+		data_temp[i],
+		0.0, # TODO: get a windspeed
+		data_hum[i],
+		data_solar[i] * 0.75, # TODO: better solar direct
+		data_solar[i] * 0.25, # TODO: better solar diffuse
+		data_solar[i]
+	]
+	data_full.append(row)
+
+with open(CSV_NAME,'w') as wFile:
+	weather_writer = csv.writer(wFile)
+	weather_writer.writerow(['temperature','wind_speed','humidity','solar_dir','solar_diff','solar_global'])
+	for row in data_full:
+		weather_writer.writerow(row)
 
 # Add stuff to the feeder.
 myTree = feeder.parse('IEEE_quickhouse.glm')
 
-# HACK:doesn't matter if we import modules twice, so just stick everything on the end.
+# HACK: delete all climate then reinsert.
+reader_name = 'weatherReader'
+climate_name = 'MyClimate'
+for key in myTree.keys():
+	if myTree[key].get('name','') in [reader_name, climate_name]:
+		del myTree[key]
 oldMax = feeder.getMaxKey(myTree)
 myTree[oldMax + 1] = {'omftype':'module', 'argument':'tape'}
 myTree[oldMax + 2] = {'omftype':'module', 'argument':'climate'}
-myTree[oldMax + 3] = {'object':'csv_reader', 'name':'weatherReader', 'filename':'weatheryearDCA.csv'}
-myTree[oldMax + 4] = {'object':'climate', 'name':'MyClimate', 'tmyfile':'weatheryearDCA.csv', 'reader':'weatherReader'}
+myTree[oldMax + 3] = {'object':'csv_reader', 'name':reader_name, 'filename':CSV_NAME}
+myTree[oldMax + 4] = {'object':'climate', 'name':climate_name, 'reader': reader_name}
 
 # Add a few panels too to test.
 # myTree[oldMax + 5] = {'name':'solEngInverter', 
@@ -45,7 +73,8 @@ myTree[oldMax + 4] = {'object':'climate', 'name':'MyClimate', 'tmyfile':'weather
 # 	'property':'power_A,power_B,power_C',
 # 	'object': 'recorder'}
 
-feeder.adjustTime(myTree, 24, 'hours', '2014-01-01')
+# Set the time correctly.
+feeder.adjustTime(myTree, 240, 'hours', '2017-01-01')
 
 # Run here to test.
 rawOut = runInFilesystem(myTree, attachments=[], keepFiles=True, workDir='.', glmName='./outFile.glm')
