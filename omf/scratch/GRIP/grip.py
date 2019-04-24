adcc6dd6b15e3113919ac1a4c96cfc4d7cdee366
Austin Chang <16790624+ASC95@users.noreply.github.com>
2019-04-23 23:35:48 -0400

    made a bunch more routes asynchronous

--- omf/scratch/GRIP/grip.py
+++ omf/scratch/GRIP/grip.py
@@ -58,7 +58,8 @@ def get_status(func):
 		temp_dir = get_abs_path(kwargs["temp_dir"])
 		if not os.path.isdir(temp_dir):
 			abort(404)
-		return func(temp_dir)
+		response = func(temp_dir)
+		return json.jsonify(get_task_metadata(temp_dir)) if response is None else response
 	return wrapper
 
 def run_process(func):
@@ -73,14 +74,23 @@ def run_process(func):
 				f.write(str(sys.exc_info()[1]))	
 	return wrapper
 
+# Insert all other routes here!
+@app.route("/milsoftToGridlab/<path:temp_dir>", methods=["DELETE"])
+@app.route("/oneLineGridlab/<path:temp_dir>", methods=["DELETE"])
+def delete(temp_dir):
+	temp_dir = get_abs_path(temp_dir)
+	if not os.path.isdir(temp_dir):
+		abort(404)
+	metadata = get_task_metadata(temp_dir)
+	if "Failure message" in metadata:
+		del metadata["Failure message"]
+	metadata["Status"] = "Stopped"
+	metadata["Stopped at"] = time.ctime(time.time())
+	shutil.rmtree(temp_dir)
+	return json.jsonify(metadata)
+
 @app.route("/oneLineGridlab", methods=["POST"])
 def oneLineGridlab_start():
-	''' Data Params: {glm: [file], useLatLons: Boolean}
-	OMF fuction: omf.feeder.latLonNxGraph()
-	Runtime: should be around 1 to 30 seconds.
-    Result: Create a one line diagram of the input glm. Return a .png of it. If useLatLons is True then draw using the lat/lons, otherwise force
-    layout the graph.
-    '''
 	temp_dir = tempfile.mkdtemp() 
 	p = Process(target=oneLineGridlab, args=(temp_dir,))
 	p.start()
@@ -88,6 +98,11 @@ def oneLineGridlab_start():
 
 @run_process
 def oneLineGridlab(temp_dir):
+	''' Data Params: {glm: [file], useLatLons: Boolean}
+	OMF fuction: omf.feeder.latLonNxGraph()
+	Runtime: should be around 1 to 30 seconds.
+    Result: Create a one line diagram of the input glm. Return a .png of it. If useLatLons is True then draw using the lat/lons, otherwise force
+    layout the graph. '''
 	f = request.files['glm']
 	glm_path = os.path.join(temp_dir, "in.glm")
 	f.save(glm_path)
@@ -110,23 +125,6 @@ def oneLineGridlab(temp_dir):
 def oneLineGridlab_status(temp_dir):
 	if os.path.isfile(os.path.join(temp_dir, "out.png")):
 		return redirect(url_for("oneLineGridlab_download", temp_dir=get_rel_path(temp_dir)), code=303)
-	else:
-		return json.jsonify(get_task_metadata(temp_dir))
-
-# Insert all other routes here!
-@app.route("/milsoftToGridlab/<path:temp_dir>", methods=["DELETE"])
-@app.route("/oneLineGridlab/<path:temp_dir>", methods=["DELETE"])
-def delete(temp_dir):
-	temp_dir = get_abs_path(temp_dir)
-	if not os.path.isdir(temp_dir):
-		abort(404)
-	metadata = get_task_metadata(temp_dir)
-	if "Failure message" in metadata:
-		del metadata["Failure message"]
-	metadata["Status"] = "Stopped"
-	metadata["Stopped at"] = time.ctime(time.time())
-	shutil.rmtree(temp_dir)
-	return json.jsonify(metadata)
 
 @app.route("/oneLineGridlab/<path:temp_dir>/download")
 @get_download
@@ -161,15 +159,14 @@ def milsoftToGridlab(temp_dir):
 	tree = omf.milToGridlab.convert(stdFile, seqFile, rescale=True)
 	glmName = 'out.glm'
 	glmPath = os.path.join(temp_dir, glmName)
-	with open(glmPath, 'w') as outFile: outFile.write(omf.feeder.sortedWrite(tree))
+	with open(glmPath, 'w') as outFile:
+		outFile.write(omf.feeder.sortedWrite(tree))
 
 @app.route("/milsoftToGridlab/<path:temp_dir>")
 @get_status
 def milsoftToGridlab_status(temp_dir):
 	if os.path.isfile(os.path.join(temp_dir, "out.glm")):
 		return redirect(url_for("milsoftToGridlab_download", temp_dir=get_rel_path(temp_dir)), code=303)
-	else:
-		return json.jsonify(get_task_metadata(temp_dir))
 
 @app.route("/milsoftToGridlab/<path:temp_dir>/download")
 @get_download
@@ -178,111 +175,152 @@ def milsoftToGridlab_download(temp_dir):
 		abort(404)
 	return send_from_directory(temp_dir, "out.glm", mimetype="text/plain")
 
-
-
-
-
-
-
-
-
-
-
-@app.route('/cymeToGridlab', methods=['POST'])
-def cymeToGridlab():
+@app.route("/cymeToGridlab", methods=["POST"])
+def cymeToGridlab_start():
 	'''Data Params: {mdb: [file]}
 	OMF function: omf.cymeToGridlab.convertCymeModel()
 	Result: a .glm file converted from the input file.'''
-	workDir = tempfile.mkdtemp()
+	temp_dir = tempfile.mkdtemp()
+	p = Process(target=cymeToGridlab, args=(temp_dir,))
+	p.start()
+	return ("SEE OTHER", 303, {"Location": "cymeToGridlab" + temp_dir})
+
+@run_process
+def cymeToGridlab(temp_dir):
 	mdbFileName = 'in.mdb'
 	mdbFile = request.files['mdb']
-	mdbPath = os.path.join(workDir, mdbFileName)
+	mdbPath = os.path.join(temp_dir, mdbFileName)
 	mdbFile.save(mdbPath)
 	import locale
 	locale.setlocale(locale.LC_ALL, 'en_US')
-	try:
-		tree = omf.cymeToGridlab.convertCymeModel(mdbPath, workDir)
-		glmName = 'out.glm'
-		glmPath = os.path.join(workDir, glmName)
-		with open(glmPath, 'w') as outFile:
-			outFile.write(omf.feeder.sortedWrite(tree))
-		# TODO: delete the tempDir.
-		return send_from_directory(workDir, glmName, mimetype="text/plain")
-	except:
-		abort(415)
+	tree = omf.cymeToGridlab.convertCymeModel(mdbPath, temp_dir)
+	glmName = 'out.glm'
+	glmPath = os.path.join(temp_dir, glmName)
+	with open(glmPath, 'w') as outFile:
+		outFile.write(omf.feeder.sortedWrite(tree))
 
-@app.route('/gridlabRun', methods=['POST'])
-def gridlabRun():
+@app.route("/cymeToGridlab/<path:temp_dir>")
+@get_status
+def cymeToGridlab_status(temp_dir):
+	if os.path.isfile(os.path.join(temp_dir, "out.glm")):
+		return redirect(url_for("cymeToGridlab_download", temp_dir=get_rel_path(temp_dir)), code=303)
+
+@app.route("/cymeToGridlab/<path:temp_dir>/download")
+@get_download
+def cymeToGridlab_download(temp_dir):
+	if not os.path.isfile(os.path.join(temp_dir, "out.glm")):
+		abort(404)
+	return send_from_directory(temp_dir, "out.glm", mimetype="text/plain")
+	
+@app.route("/gridlabRun", methods=["POST"])
+def gridlabRun_start():
+	temp_dir = tempfile.mkdtemp()
+	p = Process(target=gridlabRun, args=(temp_dir,))
+	p.start()
+	return ("SEE OTHER", 303, {"Location": "gridlabRun" + temp_dir})
+
+@run_process
+def gridlabRun(temp_dir):
 	'''Data Params: {glm: [file]}
 	Runtime: could take hours. Jeepers.
 	OMF fuction: omf.solvers.gridlabd.runInFileSystem()
 	Result: Run a GridLAB-D model and return the results as JSON.
 	TODO: think about attachment support.'''
-	workDir = tempfile.mkdtemp()
 	fName = 'in.glm'
 	f = request.files['glm']
-	glmOnDisk = os.path.join(workDir, fName)
+	glmOnDisk = os.path.join(temp_dir, fName)
 	f.save(glmOnDisk)
-	try:
-		feed = omf.feeder.parse(glmOnDisk)
-		outDict = omf.solvers.gridlabd.runInFilesystem(feed, attachments=[], keepFiles=True, workDir=workDir, glmName='out.glm')
-		#TODO: delete the tempDir.
-		return json.jsonify(outDict)
-	except:
-		abort(415)
+	feed = omf.feeder.parse(glmOnDisk)
+	outDict = omf.solvers.gridlabd.runInFilesystem(feed, attachments=[], keepFiles=True, workDir=temp_dir, glmName='out.glm')
+	with open(os.path.join(temp_dir, "gridlab-output.json"), 'w') as f:
+		json.dump(outDict, f)
+
+@app.route("/gridlabRun/<path:temp_dir>")
+@get_status
+def gridlabRun_status(temp_dir):
+	if os.path.isfile(os.path.join(temp_dir, "gridlab-output.json")):
+		return redirect(url_for("gridlabRun_download", temp_dir=get_rel_path(temp_dir)), code=303)
+
+@app.route("/gridlabRun/<path:temp_dir>/download")
+@get_download
+def gridlabRun_download(temp_dir):
+	if not os.path.isfile(os.path.join(temp_dir, "gridlab-output.json")):
+		abort(404)
+	return send_from_directory(temp_dir, "gridlab-output.json", mimetype="application/json")
 
 @app.route('/gridlabdToGfm', methods=['POST'])
-def gridlabdToGfm():
+def gridlabdToGfm_start():
+	temp_dir = tempfile.mkdtemp()
+	p = Process(target=gridlabdToGfm, args=(temp_dir,))
+	p.start()
+	return ("SEE OTHER", 303, {"Location": "gridlabdToGfm" + temp_dir})
+
+@run_process
+def gridlabdToGfm(temp_dir):
 	'''Data Params: {glm: [file], other_inputs: see source}
 	OMF function: omf.models.resilientDist.convertToGFM()
 	Runtime: should only be a couple seconds.
-	Result: Convert the GridLAB-D model to a GFM model. Return the new id for the converted model. Note that this is not the main fragility model for GRIP.'''
-	workDir = tempfile.mkdtemp()
+	Result: Convert the GridLAB-D model to a GFM model. Return the new id for the converted model. Note that this is not the main fragility model for
+	GRIP.'''
 	fName = 'in.glm'
 	f = request.files['glm']
-	glmPath = os.path.join(workDir, fName)
+	glmPath = os.path.join(temp_dir, fName)
 	f.save(glmPath)
-	try:
-		gfmInputTemplate = {
-			'phase_variation': float(request.form.get('phase_variation')),
-			'chance_constraint': float(request.form.get('chance_constraint')),
-			'critical_load_met': float(request.form.get('critical_load_met')),
-			'total_load_met': float(request.form.get('total_load_met')),
-			'maxDGPerGenerator': float(request.form.get('maxDGPerGenerator')),
-			'dgUnitCost': float(request.form.get('dgUnitCost')),
-			'generatorCandidates': request.form.get('generatorCandidates'),
-			'criticalLoads': request.form.get('criticalLoads')
-		}
-		for key, val in gfmInputTemplate.items():
-			if val is None:
-				abort(400)
-	except:
-		abort(400)
-	try:
-		feederModel = {
-			'nodes': [], # Don't need these.
-			'tree': omf.feeder.parse(glmPath)
-		}
-		gfmDict = omf.models.resilientDist.convertToGFM(gfmInputTemplate, feederModel)
-		# TODO: delete the tempDir.
-		return json.jsonify(gfmDict)
-	except:
-		abort(415)
+	gfmInputTemplate = {
+		'phase_variation': float(request.form.get('phase_variation')),
+		'chance_constraint': float(request.form.get('chance_constraint')),
+		'critical_load_met': float(request.form.get('critical_load_met')),
+		'total_load_met': float(request.form.get('total_load_met')),
+		'maxDGPerGenerator': float(request.form.get('maxDGPerGenerator')),
+		'dgUnitCost': float(request.form.get('dgUnitCost')),
+		'generatorCandidates': request.form.get('generatorCandidates'),
+		'criticalLoads': request.form.get('criticalLoads')
+	}
+	for key, val in gfmInputTemplate.items():
+		if val is None:
+			raise Exception(("gridlabdToGfm was expecting a valid value for key: {key},"
+				"but it received value: {value}").format(key=key, value=val))
+	feederModel = {
+		'nodes': [], # Don't need these.
+		'tree': omf.feeder.parse(glmPath)
+	}
+	gfmDict = omf.models.resilientDist.convertToGFM(gfmInputTemplate, feederModel)
+	with open(os.path.join(temp_dir, "out.gfm"), 'w') as f:
+		json.dump(gfmDict, f)
 
-# Don't touch this for now
+@app.route("/gridlabdToGfm/<path:temp_dir>")
+@get_status
+def gridlabdToGfm_status(temp_dir):
+	if os.path.isfile(os.path.join(temp_dir, "out.gfm")):
+		return redirect(url_for("gridlabdToGfm_download", temp_dir=get_rel_path(temp_dir)), code=303)
+
+@app.route("/gridlabdToGfm/<path:temp_dir>/download")
+@get_download
+def gridlabdToGfm_download(temp_dir):
+	if not os.path.isfile(os.path.join(temp_dir, "out.gfm")):
+		abort(404)
+	return send_from_directory(temp_dir, "out.gfm", mimetype="application/json")
+
 @app.route('/runGfm', methods=['POST'])
-def runGfm():
-	'''Data Params: {gfm: [file], asc: [file]}
+def runGfm_start():
+	temp_dir = tempfile.mkdtemp() 
+	p = Process(target=runGfm, args=(temp_dir,))
+	p.start()
+	return ("SEE OTHER", 303, {"Location": "runGfm" + temp_dir})
+
+@run_process
+def runGfm(temp_dir):
+	''' Data Params: {gfm: [file], asc: [file]}
 	OMF function: omf.solvers.gfm.run()
 	Runtime: should be around 1 to 30 seconds.
-	Result: Return the results dictionary/JSON from running LANL's General Fragility Model (GFM) on the input model and .asc hazard field. Note that this is not the main fragility model for GRIP.'''
-	workDir = tempfile.mkdtemp()
+	Result: Return the results dictionary/JSON from running LANL's General Fragility Model (GFM) on the input model and .asc hazard field. Note that
+	this is not the main fragility model for GRIP. '''
 	fName = 'gfm.json'
-	gfmPath = os.path.join(workDir, fName)
+	gfmPath = os.path.join(temp_dir, fName)
 	f = request.files['gfm']
 	f.save(gfmPath)
 	hName = 'hazard.asc'
-	hazardPath = os.path.join(workDir, hName)
+	hazardPath = os.path.join(temp_dir, hName)
 	h = request.files['asc']
 	h.save(hazardPath)
 	# Run GFM
@@ -299,19 +337,30 @@ def runGfm():
 		[javaCmd,'-jar', gfmBinaryPath, '-r', fName, '-wf', hName, '-num', '3', '-ro', outName],
 		stdout = subprocess.PIPE,
 		stderr = subprocess.PIPE,
-		cwd = workDir
+		cwd = temp_dir
 	)
 	(stdout,stderr) = proc.communicate()
-	gfmOutPath = os.path.join(workDir, outName)
+	gfmOutPath = os.path.join(temp_dir, outName)
+	try:
+		with open(gfmOutPath) as f:
+			out = json.load(f)
+	except:
+		out = stdout
+	with open(os.path.join(temp_dir, "rungfm-output.txt"), 'w') as f:
+		f.write(out)
 
-	with open(gfmOutPath) as f:
-		out = json.load(f)
+@app.route("/runGfm/<path:temp_dir>")
+@get_status
+def runGfm_status(temp_dir):
+	if os.path.isfile(os.path.join(temp_dir, "rungfm-output.txt")):
+		return redirect(url_for("runGfm_download", temp_dir=get_rel_path(temp_dir)), code=303)
 
-	#try:
-	#	out = json.load(open(gfmOutPath))
-	#except:
-	#	out = stdout
-	return out
+@app.route("/runGfm/<path:temp_dir>/download")
+@get_download
+def runGfm_download(temp_dir):
+	if not os.path.isfile(os.path.join(temp_dir, "rungfm-output.txt")):
+		abort(404)
+	return send_from_directory(temp_dir, "rungfm-output.txt")
 
 @app.route('/samRun', methods=['POST'])
 def samRun():
