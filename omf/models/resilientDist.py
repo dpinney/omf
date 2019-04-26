d7a90243b14630b943fdc7e4a4fde345bb583672
David Pinney <david@pinney.org>
2019-04-26 12:34:22 -0400

    Removing junk

--- omf/models/resilientDist.py
+++ omf/models/resilientDist.py
@@ -126,19 +126,6 @@ def _testHazards():
 	# hazard.exportHazardObj("modWindFile.asc")
 	# hazard.drawHeatMap()
 
-def gen_dict_extract(key, var):
-    if hasattr(var,'iteritems'):
-        for k, v in var.iteritems():
-            if k == key:
-                yield v
-            if isinstance(v, dict):
-                for result in gen_dict_extract(key, v):
-                    yield result
-            elif isinstance(v, list):
-                for d in v:
-                    for result in gen_dict_extract(key, d):
-                        yield result
-
 def getNodePhases(obj, maxRealPhase):
 	''' Convert phase info in GridLAB-D obj (e.g. ABC) to GFM phase format (e.g. [True,True,True].'''
 	numPhases = 0
