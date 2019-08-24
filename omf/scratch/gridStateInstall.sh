98a3594312999c814a2ee5586f2d3cd0f8155605
Austin Chang <16790624+ASC95@users.noreply.github.com>
2019-08-24 16:36:34 -0400

    tested the script to ensure it works

--- omf/scratch/gridStateInstall.sh
+++ omf/scratch/gridStateInstall.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# This script installs the OMF on top of the existing GridState installation
+# This script installs the OMF on top of the existing GridState installation. Every Jupyter notebook user gets their own Docker container via
+# JupyterHub, so I can't mess up the GridState installation
 
 # Install missing packages
-apt-get -y update && apt-get install -y git python-pip wget sudo locales
+sudo apt-get -y update && sudo apt-get install -y git python-pip locales
 # Generate a locale required by Cyme conversion in the OMF
-sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen && locale-gen
+sudo sed -i 's/^# *\(en_US.UTF-8\)/\1/' /etc/locale.gen && sudo locale-gen
 # Get the repository
 cd /home/jovyan
 git clone https://github.com/dpinney/omf.git
@@ -19,6 +20,11 @@ sudo ln -s /usr/bin/python /usr/local/bin/python
 sudo ln -s /usr/bin/pip2 /usr/local/bin/pip
 # Run the install script
 sudo python install.py
+# Configure Jupyter notebook
+sudo python2 -m pip install ipykernel
+# Installing the kernelspec with sudo, but don't use sudo. There is only one user per container, so a system-wide kernelspec vs. a user-specific
+# kernelspec is irrelevant regardless
+python2 -m ipykernel install --user
 # Restore the original GridState environment
 sudo mv /usr/local/bin/python.bkp /usr/local/bin/python
 sudo mv /usr/local/bin/pip.bkp /usr/local/bin/pip
