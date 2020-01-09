dd6866343d7e9ac611deefdad3588432bdcd11da
ASC95 <16790624+ASC95@users.noreply.github.com>
2020-01-08 19:45:29 -0500

    omf/solvers/__init__.py manual refactor

--- omf/solvers/__init__.py
+++ omf/solvers/__init__.py
@@ -4,7 +4,7 @@ import os, sys
 
 # Make sure this module is on the path regardless of where its imported from.
 _myDir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(_myDir)
+#sys.path.append(_myDir)
 
 # Grab all our submodules and include them whenever we include this package:
 __all__ = [x.replace('.py','') for x in os.listdir(_myDir)
@@ -14,6 +14,5 @@ __all__ = [x.replace('.py','') for x in os.listdir(_myDir)
 # for name in __all__: exec('import ' + name)
 
 # HACK: manual import for now due to incompatibility of Debian and NREL SAM 2015.
-import gridlabd
-import nrelsam2013
-
+from omf.solvers import gridlabd
+from omf.solvers import nrelsam2013
