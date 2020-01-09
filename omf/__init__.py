8346c5edc50f7d7a6b7d3d824850f20b2e7c6d40
ASC95 <16790624+ASC95@users.noreply.github.com>
2020-01-08 21:51:33 -0500

    omf/__init__.py manual refactor. No longer import every sub-module

--- omf/__init__.py
+++ omf/__init__.py
@@ -2,26 +2,27 @@
 
 __version__ = 0.2
 
-import os as _os, sys as _sys
+import os as _os
+#import sys as _sys
 
 # Make sure we're on the path.
 omfDir = _os.path.dirname(__file__)
-_sys.path.append(omfDir)
+#_sys.path.append(omfDir)
 
 # Import sub-packages.
-import solvers
-import models
-import anonymization
-import calibrate
-import cymeToGridlab
-import feeder
-import loadModeling
-import loadModelingAmi
-import milToGridlab
-import network
-import weather
-import distNetViz
-import cosim
-import cyberAttack
-import geo
-import forecast
+#import solvers
+#import models
+#import anonymization
+#import calibrate
+#import cymeToGridlab
+#import feeder
+#import loadModeling
+#import loadModelingAmi
+#import milToGridlab
+#import network
+#import weather
+#import distNetViz
+#import cosim
+#import cyberAttack
+#import geo
+#import forecast
