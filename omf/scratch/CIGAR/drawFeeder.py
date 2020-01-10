a2769a63561d2e1629892670bbfb78dab6f84747
ASC95 <16790624+ASC95@users.noreply.github.com>
2020-01-09 21:31:59 -0500

    omf/scratch/CIGAR/drawFeeder.py import refactor

--- omf/scratch/CIGAR/drawFeeder.py
+++ omf/scratch/CIGAR/drawFeeder.py
@@ -1,4 +1,4 @@
-import omf
+from omf.models import voltageDrop
 from matplotlib import pyplot as plt
 
 FNAME = 'test_base_R4-25.00-1.glm_CLEAN.glm'
@@ -31,7 +31,7 @@ FNAME = 'test_base_R4-25.00-1.glm_CLEAN.glm'
 # tree[35]['name'] = 'OH NO CHANGED'
 
 # chart = omf.models.voltageDrop.drawPlot(FNAME, neatoLayout=True, edgeCol=True, nodeLabs="VoltageImbalance", customColormap=True, perUnitScale=False)
-chart = omf.models.voltageDrop.drawPlot(FNAME, neatoLayout=True, edgeCol=True, nodeLabs="Voltage", edgeLabs="Current", perUnitScale=False, rezSqIn=600)
+chart = voltageDrop.drawPlot(FNAME, neatoLayout=True, edgeCol=True, nodeLabs="Voltage", edgeLabs="Current", rezSqIn=600)
 chart.savefig("./VOLTOUT.png")
 
 #testAllVarCombos()
