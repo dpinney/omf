1d2c7a41283aab7be9ab097f5502233da57c85a5
ASC95 <16790624+ASC95@users.noreply.github.com>
2020-01-12 23:12:52 -0500

    moved some helper scripts around in GRIP API

--- omf/scratch/GRIP/helper/dockerBuild.sh
+++ omf/scratch/GRIP/helper/dockerBuild.sh
@@ -6,6 +6,6 @@
 
 cwd="$(pwd)"
 cd "$(dirname "${BASH_SOURCE[0]}")"
-build_context="$(pwd)"/../../..
-docker build -f grip.Dockerfile -t omf_grip "$build_context"
+build_context="$(pwd)"/../../../..
+docker build -f ../grip.Dockerfile -t omf_grip "$build_context"
 cd "$cwd"
