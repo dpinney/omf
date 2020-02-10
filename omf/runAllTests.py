5cff19377ccc72f0042d3341f946e2f69bfdbefa
ASC95 <16790624+ASC95@users.noreply.github.com>
2020-02-09 20:02:32 -0500

    More precise test detection

--- omf/runAllTests.py
+++ omf/runAllTests.py
@@ -1,6 +1,7 @@
 ''' Walk the /omf/ directory, run _tests() in all modules. '''
 
-import os, sys, subprocess
+import os, sys, subprocess, re
+from pathlib import PurePath, Path
 
 IGNORE_DIRS = ["data", ".git", "static", "templates", "testFiles", "scratch"]
 IGNORE_FILES = ["runAllTests.py"] # to avoid infinite loop.
@@ -10,13 +11,24 @@ def runAllTests(startingdir):
 	nextdirs = []
 	the_errors = 0
 	misfires = {}
-	for item in os.listdir("."):
-		if item not in IGNORE_FILES and item.endswith(".py"):
+	test_function_signature = re.compile(r'def\s+_tests\s*\(')
+	main_function_signature = re.compile(r"if\s+__name__\s+==\s+'__main__'")
+	for item in os.listdir('.'):
+		if item not in IGNORE_FILES and item.endswith('.py'):
 			with open(item) as f:
-				file_content = f.read()
-			if 'def _tests():' in file_content:
-				print("********** TESTING", item, "************")
-				p = subprocess.Popen(["python3", item], stderr=subprocess.PIPE)
+				file_content = f.readlines()
+			has_main = None
+			has_tests = False
+			for idx, line in enumerate(file_content):
+				test_mo = test_function_signature.search(line)
+				main_mo = main_function_signature.search(line)
+				if main_mo:
+					has_main = True
+				if test_mo:
+					has_tests = True
+			if has_tests and has_main:
+				print(f'********** TESTING {item} ************')
+				p = subprocess.Popen(['python3', item], stderr=subprocess.PIPE)
 				p.wait()
 				if p.returncode:
 					the_errors += 1
@@ -29,6 +41,7 @@ def runAllTests(startingdir):
 		misfires.update(m)
 	return the_errors, misfires
 
+
 def testRunner():
 	os.chdir(sys.argv[1] if len(sys.argv) > 1 else ".") # You can just run tests in a specific dir if you want
 	i, mis = runAllTests(os.getcwd())
@@ -37,9 +50,11 @@ def testRunner():
 	print("\n\n+------------------------+")
 	print(i, "tests failed:\n\n")
 	for fname, err in mis.items():
-		print(fname)
-		print(err, "\n\n")
-	if i>0: raise Exception # Fail if there were errors.
+		print(PurePath(fname).name)
+		for line in re.split(r'\n+', err.decode('utf-8')):
+			print(line)
+	if i > 0:
+		raise Exception # Fail if there were errors.
 
 if __name__ == "__main__":
 	testRunner()
