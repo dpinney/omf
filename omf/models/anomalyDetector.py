66bba8d9f9b3e5860d22dd495998b112cf95e3c1
ASC95 <16790624+ASC95@users.noreply.github.com>
2020-03-01 15:12:43 -0500

    disable anomalyDetector tests

--- omf/models/anomalyDetector.py
+++ omf/models/anomalyDetector.py
@@ -498,8 +498,7 @@ def new(modelDir):
 	return __neoMetaModel__.new(modelDir, defaultInputs)
 
 
-@neoMetaModel_test_setup
-def _tests():
+def _debugging():
 	modelLoc = pJoin(
 		__neoMetaModel__._omfDir,
 		"data",
@@ -516,4 +515,4 @@ def _tests():
 
 
 if __name__ == "__main__":
-	_tests()
+	_debugging()
