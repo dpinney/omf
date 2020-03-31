f40e2be016cc324738455f83edd692deb158b6cf
nanyaD <nanyadhawan@gmail.com>
2020-03-31 11:38:34 -0400

    separated out theft classification in faultLabeledMeterData; changed heating/cooling demand to hvac_load in NetZeroEnergyHouseSim

--- omf/scratch/faultLabeledMeterData/code/twoFileClassification.py
+++ omf/scratch/faultLabeledMeterData/code/twoFileClassification.py
@@ -18,11 +18,12 @@ from sklearn.metrics import confusion_matrix
 # constants -------------------------------------------------------------------
 
 TRAIN_FRACTION = 0.9
+TIMEPOINTS_PER_THEFT = 8
 
-TRAIN_FILE = '../data/dataOlin-3-6mo.csv'
+TRAIN_FILE = '../data/dataOlin-1b-6mo.csv'
 TEST_FILE = '../data/dataOlin-1-6mo.csv'
 
-IGNORE_LABELS = ['theft']
+IGNORE_LABELS = ['']
 
 # TRAIN_FILE = '../data/dataABEC-1mo.csv'
 # TEST_FILE = '../data/dataOlin-DEC-1mo.csv'
@@ -193,30 +194,66 @@ numPoints = testX.shape[0]
 split = int(TRAIN_FRACTION * numPoints)
 xTest = testX[split:,:]
 yTest = testY[split:]
+    
+labels = np.unique(yTest)
+labels = np.sort(labels)
 
-print(xTrain.shape)
-print(yTrain.shape)
-print(xTest.shape)
-print(yTest.shape)
+
+xTrainTheft = []
+for index in range( xTrain.shape[0]-TIMEPOINTS_PER_THEFT ):
+    datapoint = []
+    for pointNum in range(TIMEPOINTS_PER_THEFT):
+        datapoint = np.concatenate( (datapoint,xTrain[index+pointNum,1:]) )
+    xTrainTheft.append(datapoint)
+
+xTestTheft = []
+for index in range( xTest.shape[0]-TIMEPOINTS_PER_THEFT ):
+    datapoint = []
+    for pointNum in range(TIMEPOINTS_PER_THEFT):
+        datapoint = np.concatenate( (datapoint,xTest[index+pointNum,1:]) )
+    xTestTheft.append(datapoint)
+
+xTrainTheft = np.array(xTrainTheft)
+yTrainTheft = np.array(yTrain[:-TIMEPOINTS_PER_THEFT])
+yTrainTheft[ yTrainTheft != 'theft' ] = 'notTheft'
+
+xTrainNotTheft = xTrain[ yTrain != 'theft', : ]
+yTrainNotTheft = yTrain[ yTrain != 'theft' ]
 
 # iterate over classifiers
 for name, clf in zip(names, classifiers):
-    
-    start = time.time()
-    clf.fit(xTrain, yTrain)
-    endTrain = time.time()
-    yPredicted = clf.predict(xTest)
-    endTest = time.time()
-    totalTrain = endTrain-start
-    totalTest = endTest-endTrain
-    print('Train: {} seconds, Predict: {} seconds'.format(totalTrain, totalTest))        
-    
-    confMat = confusion_matrix(yTest, yPredicted, labels=uniqueLabels)
-    score = clf.score(xTest, yTest)
-    
-    plt.figure()
-    plot_confusion_matrix(confMat, uniqueLabels, normalize=True, title=name)
-    print(name,score)
-    print('')
 
-plt.show()
+    # classify all conditions    
+    clf.fit(xTrain,yTrain)
+    yPredicted = clf.predict(xTest)
+    confMat = confusion_matrix(yTest, yPredicted)
+    score1 = np.sum(yPredicted==yTest) / float(yTest.shape[0])
+
+    # classify theft vs non-theft first
+    clf.fit(xTrainTheft, yTrainTheft)
+    yPredicted = clf.predict(xTestTheft)
+    yPredictedTheft = yPredicted[ yPredicted == 'theft' ]
+    # filter out predicted non-thefts 
+    xTestNotTheft = xTest[ :-TIMEPOINTS_PER_THEFT, : ]
+    xTestNotTheft = xTestNotTheft[ yPredicted != 'theft', : ]
+    # classify non thefts further 
+    clf.fit(xTrainNotTheft, yTrainNotTheft)
+    yPredictedNotTheft = clf.predict(xTestNotTheft)
+    # comparmentalize out ground truths
+    yTestTheft = yTest[ :-TIMEPOINTS_PER_THEFT ]
+    yTestTheft = yTestTheft[ yPredicted == 'theft' ]
+    yTestNotTheft = yTest[ :-TIMEPOINTS_PER_THEFT ]
+    yTestNotTheft = yTestNotTheft[ yPredicted != 'theft' ]
+    # generate confmat
+    yPredictedNew = np.concatenate((yPredictedTheft,yPredictedNotTheft))
+    yTestNew = np.concatenate((yTestTheft,yTestNotTheft))
+    confMatNew = confusion_matrix(yTestNew, yPredictedNew)
+    score2 = np.sum(yPredictedNew==yTestNew) / float(yTestNew.shape[0])
+    
+    print(name,round(score1,2),round(score2,2))
+    # plt.figure()
+    # plt.subplot(1,2,1)
+    # plot_confusion_matrix(confMat, labels, normalize=True, title=name)  
+    # plt.subplot(1,2,2)
+    # plot_confusion_matrix(confMatNew, labels, normalize=True, title=name)
+    # plt.show()
