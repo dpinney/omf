76a27dc210c34534cec9b72d09e1475e1be419f5
delysis <georgewalkeriv@gmail.com>
2020-05-20 20:17:41 -0400

    Updated install command for brew. Removed check for already installed brew (as the install command is idempotent and updates from incompatible old versions).

--- install.py
+++ install.py
@@ -82,8 +82,7 @@ elif platform.system()=='Windows':
 elif platform.system()=="Darwin": # MacOS
 	# Install homebrew
 	brew_exit_code = os.system("brew --version")
-	if brew_exit_code != 0:
-		os.system('/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"')
+	os.system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"')
 	os.system("HOMEBREW_NO_AUTO_UPDATE=1 brew install wget ffmpeg git graphviz octave mdbtools") # Set no-update to keep homebrew from blowing away python3.
 	os.system("wget -O gridlabd.dmg --no-check-certificate https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd_4.0.0.dmg")
 	os.system("sudo hdiutil attach gridlabd.dmg")
