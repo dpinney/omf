5cf1e9d44a70bd36d012d057797ffdcd25d7da72
Tuomas Talvitie <ttalvitie49@gmail.com>
2020-07-22 11:43:04 -0400

    changes made to fire_backend

--- omf/scratch/wind/fire_backend.py
+++ omf/scratch/wind/fire_backend.py
@@ -4,6 +4,7 @@ Backend for fire map work.
 
 import multiprocessing, time
 from flask import Flask
+from omf.weather import getSubGridData
 
 app = Flask(__name__)
 
@@ -19,7 +20,12 @@ def test():
 @app.route('/firedata/<coords>')
 def firedata(coords):
 	# x = omf.weather.get_ndfd(33,53)
-	return coords
+	lat = 40.758701
+	lon = -111.876183
+	dist = 20
+	resolution = 20
+	x = getSubGridData(str(lat), str(lon), str(dist), str(dist), str(resolution))
+	return x
 
 #FRONTEND JAVASCRIPT
 # Sending requests from the frontend: https://www.w3schools.com/xml/ajax_xmlhttprequest_send.asp
@@ -28,6 +34,37 @@ def firedata(coords):
 # xhttp.open("GET", "firedata/33,44", false);
 # xhttp.send();
 # console.log(xhttp.responseText);
+
+"""
+This is sample stuff we want to put in front end
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<div id="demo">
+<h2>The XMLHttpRequest Object</h2>
+<button type="button" onclick="loadDoc()">Change Content</button>
+</div>
+
+<script>
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_info.txt", true);
+  xhttp.send();
+}
+</script>
+
+</body>
+</html>
+
+"""
 
 if __name__ == '__main__':
 	app.run(debug=True)
