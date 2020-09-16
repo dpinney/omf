09a904b4b4993135923e0b069baf209c279a627a
David Pinney <david@pinney.org>
2020-09-16 17:18:57 -0400

    Fixing break due to reopt silent api change.

--- omf/solvers/REopt/__init__.py
+++ omf/solvers/REopt/__init__.py
@@ -5,53 +5,49 @@ from omf.solvers.REopt import results_poller
 
 def run(inJSONPath, outputPath):
 	API_KEY = 'WhEzm6QQQrks1hcsdN0Vrd56ZJmUyXJxTJFg6pn9'  # REPLACE WITH YOUR API KEY
-
 	root_url = 'https://developer.nrel.gov/api/reopt'
 	post_url = root_url + '/v1/job/?api_key=' + API_KEY
 	results_url = root_url + '/v1/job/<run_uuid>/results/?api_key=' + API_KEY
-
 	post = json.load(open(inJSONPath))
-
 	resp = requests.post(post_url, json=post)
-
 	if not resp.ok:
 		logger.log.error("Status code {}. {}".format(resp.status_code, resp.content))
 	else:
 		logger.log.info("Response OK from {}.".format(post_url))
 		run_id_dict = json.loads(resp.text)
-
 		try:
 			run_id = run_id_dict['run_uuid']
 		except KeyError:
 			msg = "Response from {} did not contain run_uuid.".format(post_url)
 			logger.log.error(msg)
 			raise KeyError(msg)
-
 		results = results_poller.poller(url=results_url.replace('<run_uuid>', run_id))
 		with open(outputPath, 'w') as fp:
 			json.dump(obj=results, fp=fp, indent=4)
-
 		logger.log.info("Saved results to {}".format(outputPath))
 
 def runResilience(runID, outputPath):
 	API_KEY = 'WhEzm6QQQrks1hcsdN0Vrd56ZJmUyXJxTJFg6pn9'  # REPLACE WITH YOUR API KEY
-
 	root_url = 'https://developer.nrel.gov/api/reopt'
-	results_url = root_url + '/v1/job/' + runID +'/resilience_stats/?api_key=' + API_KEY
-
-	resp = requests.get(results_url)
-
+	post_url = root_url + '/v1/outagesimjob/?api_key=' + API_KEY
+	results_url = root_url + '/v1/job/<RUN_ID>/resilience_stats/?api_key=' + API_KEY
+	resp = requests.post(post_url, json={'run_uuid':runID, 'bau':False})
 	if not resp.ok:
 		logger.log.error("Status code {}. {}".format(resp.status_code, resp.content))
 	else:
-		logger.log.info("Response OK from {}.".format(results_url))
-		respDict = json.loads(resp.text)
-
-	with open(outputPath, 'w') as fp:
-		json.dump(obj=respDict, fp=fp, indent=4)
-
-	logger.log.info("Saved results to {}".format(outputPath))
-
+		logger.log.info("Response OK from {}.".format(post_url))
+		run_id_dict = json.loads(resp.text)
+		print(resp.text)
+		try:
+			run_id = run_id_dict['run_uuid']
+		except KeyError:
+			msg = "Response from {} did not contain run_uuid.".format(post_url)
+			logger.log.error(msg)
+			raise KeyError(msg)
+		results = results_poller.rez_poller(url=results_url.replace('<RUN_ID>', run_id))
+		with open(outputPath, 'w') as fp:
+			json.dump(obj=results["outage_sim_results"], fp=fp, indent=4)
+		logger.log.info("Saved results to {}".format(outputPath))
 
 def _test():
 	run('Scenario_test_POST.json', 'results.json')
--- omf/solvers/REopt/results_poller.py
+++ omf/solvers/REopt/results_poller.py
@@ -38,3 +38,36 @@ def poller(url, poll_interval=2):
             time.sleep(poll_interval)
 
     return resp_dict
+
+def rez_poller(url, poll_interval=2):
+    """
+    Function for polling the REopt API results URL until status is not "Optimizing..."
+    :param url: results url to poll
+    :param poll_interval: seconds
+    :return: dictionary response (once status is not "Optimizing...")
+    """
+    key_error_count = 0
+    key_error_threshold = 4
+    status = ""
+    logger.log.info("Polling {} for results with interval of {}s...".format(url, poll_interval))
+    while True:
+
+        resp = requests.get(url=url)
+        resp_dict = json.loads(resp.text)
+
+        try:
+            status = str(resp_dict['outage_sim_results'])
+        except KeyError:
+            key_error_count += 1
+            logger.log.info('KeyError count: {}'.format(key_error_count))
+            if key_error_count > key_error_threshold:
+                logger.log.info('Breaking polling loop due to KeyError count threshold of {} exceeded.'
+                         .format(key_error_threshold))
+                break
+
+        if status != "":
+            break
+        else:
+            time.sleep(poll_interval)
+
+    return resp_dict
