aa556db3749c45398bdb76771c8c3dbceed7b67c
David Pinney <david@pinney.org>
2020-09-25 15:12:31 -0400

    Fix for kmz.

--- omf/scratch/wind/fire_backend.py
+++ omf/scratch/wind/fire_backend.py
@@ -15,15 +15,6 @@ app = Flask(__name__)
 def root():
 	return open('satellitemap_editablePopup.html').read()
 
-@app.route('/getkmz')
-def kmz():
-    return render_template('satellitemap_editablePopup.html')
-
-@app.route('/test')
-def test():
-	x = 1 + 2
-	return '<b>bold world</b> ' + str(x)
-
 @app.route('/firedata/<lat>/<lon>/<distLat>/<distLon>/<resolution>')
 def firedata(lat, lon, distLat, distLon, resolution):
 	print(lat, lon, distLat, distLon, resolution)
@@ -33,14 +24,6 @@ def firedata(lat, lon, distLat, distLon, resolution):
 	# print (type(x)) # x is a python dictionary 
 	return json.dumps(x) 
 
-@app.route('/circuit.geojson')
-def circuit():
-	return open('circuit.geojson').read()
-
-# @app.route('/L.KML.js')
-# def kml_lib():
-# 	return open('L.KML.js').read()
-
 @app.route('/my_kmz')
 def my_kmz():
 	# return open('day1fireotlk.kmz').read()
@@ -48,18 +31,10 @@ def my_kmz():
 	# url = 'https://www.spc.noaa.gov/products/fire_wx/day2fireotlk.kmz'
 	# url = 'https://www.spc.noaa.gov/products/fire_wx/day38fireotlk.kmz'
 	r = requests.get(url, allow_redirects=True)
-	return send_file(r.content,
-		# as_attachment = True,
-		mimetype="application/vnd",
-		)
-
-@app.route('/https://www.spc.noaa.gov/products/fire_wx/day1fireotlk.kmz')
-def kmz_address():
-	return render_template('https://www.spc.noaa.gov/products/fire_wx/day1fireotlk.kmz')
-
-@app.route('/data.geojson')
-def data():
-	return open('data.geojson').read()
+	return send_file(
+		io.BytesIO(r.content),
+		attachment_filename='fire_risk.kmz'
+	)
 
 if __name__ == '__main__':
 	app.run(debug=True)
