392703d9bebd01087fe2072ed1f0bff1379a08a3
David Pinney <david@pinney.org>
2021-01-30 14:15:23 -0500

    just ignore ms packages for now

--- install.py
+++ install.py
@@ -14,10 +14,12 @@ def pipInstallInOrder(pipCommandString):
 if platform.system() == "Linux" and platform.linux_distribution()[0] in ["Ubuntu"]:
 	os.system("sudo apt-get -y update && sudo apt-get -y upgrade") # Make sure apt-get is updated to prevent any weird package installation issues
 	os.system("sudo apt-get -y install language-pack-en") # Install English locale 
-	os.system("echo msodbcsql17 msodbcsql/ACCEPT_EULA boolean true | sudo debconf-set-selections; sudo ACCEPT_EULA=Y apt-get -yq install msodbcsql") # workaround for the package EULA.
-	# os.system("sudo ACCEPT_EULA=Y apt-get -yq install mssql-tools") # workaround for the package EULA.
+	try:
+		os.system("sudo ACCEPT_EULA=Y apt-get -yq install mssql-tools msodbcsql mdbtools") # workaround for the package EULA.
+	except:
+		pass
 	os.system("sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python3-pip python3-dev python3-numpy python3-pygraphviz graphviz \
-		unixodbc-dev libfreetype6-dev pkg-config alien libgraphviz-dev python3-pydot mdbtools python3-tk octave libblas-dev liblapack-dev \
+		unixodbc-dev libfreetype6-dev pkg-config alien libgraphviz-dev python3-pydot python3-tk octave libblas-dev liblapack-dev \
 		libatlas-base-dev gfortran wget splat")
 	os.system("sudo apt-get -y install ffmpeg python3-cairocffi") # Separate to better support debian.
 	os.system("wget https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd-4.0.0-1.el6.x86_64.rpm")
