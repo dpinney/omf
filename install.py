a84cc603998737c432a5b4f45f6e0cd5b9d01d19
David Pinney <david@pinney.org>
2021-02-15 13:29:36 -0500

    setuptools issues with a req

--- install.py
+++ install.py
@@ -15,7 +15,7 @@ if platform.system() == "Linux" and platform.linux_distribution()[0] in ["Ubuntu
 	os.system("sudo ACCEPT_EULA=Y apt-get -yq install mssql-tools msodbcsql mdbtools") # workaround for the package EULA, which otherwise breaks upgrade!!
 	os.system("sudo apt-get -y update && sudo apt-get -y upgrade") # Make sure apt-get is updated to prevent any weird package installation issues
 	os.system("sudo apt-get -y install language-pack-en") # Install English locale 
-	os.system("sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python3-pip python3-dev python3-numpy python3-pygraphviz graphviz \
+	os.system("sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python3-pip python3-dev python3-numpy python3-pygraphviz python3-setuptools graphviz \
 		unixodbc-dev libfreetype6-dev pkg-config alien libgraphviz-dev python3-pydot python3-tk octave libblas-dev liblapack-dev \
 		libatlas-base-dev gfortran wget splat")
 	os.system("sudo apt-get -y install ffmpeg python3-cairocffi") # Separate to better support debian.
