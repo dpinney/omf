bdc93de709ef7ef8bb6e8aa20f2d0312c28d35ad
David Pinney <david@pinney.org>
2022-02-07 17:51:37 -0500

    moving matpower to install-time

--- install.py
+++ install.py
@@ -25,6 +25,8 @@ if platform.system() == "Linux" and platform.linux_distribution()[0] in ["Ubuntu
 	# os.system("wget https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd-4.0.0-1.el6.x86_64.rpm")
 	os.system("sudo alien -i omf/static/gridlabd-4.0.0-1.el6.x86_64.rpm")
 	os.system("sudo apt-get install -f")
+	os.system(f"wget -P {source_dir}/omf/solvers/ 'https://github.com/MATPOWER/matpower/releases/download/7.0/matpower7.0.zip'")
+	os.system(f"unzip '{source_dir}/omf/solvers/matpower7.0.zip' -d {source_dir}/omf/solvers/")
 	os.system(f'octave-cli --no-gui -p "{source_dir}/omf/solvers/matpower7.0" --eval "install_matpower(1,1,1)"')
 	os.system("cd omf")
 	os.system("pip3 install --upgrade pip setuptools")
@@ -43,6 +45,8 @@ elif platform.system() == "Linux" and platform.linux_distribution()[0]=="CentOS 
 	os.system("sudo yum -y install python-pip")
 	#os.system("wget --no-check-certificate https://sourceforge.net/projects/gridlab-d/files/gridlab-d/Candidate%20release/gridlabd-4.0.0-1.el6.x86_64.rpm")
 	os.system("rpm -Uvh omf/static/gridlabd-4.0.0-1.el6.x86_64.rpm")
+	os.system(f"wget -P {source_dir}/omf/solvers/ 'https://github.com/MATPOWER/matpower/releases/download/7.0/matpower7.0.zip'")
+	os.system(f"unzip '{source_dir}/omf/solvers/matpower7.0.zip' -d {source_dir}/omf/solvers/")
 	os.system(f'octave-cli --no-gui -p "{source_dir}/omf/solvers/matpower7.0" --eval "install_matpower(1,1,1)"')
 	os.system("cd omf")
 	pipInstallInOrder("pip3")
@@ -66,6 +70,10 @@ elif platform.system()=='Windows':
 	#os.system(wget http://www.ve3ncq.ca/software/SPLAT-1.3.1.zip)
 	#os.system(unzip SPLAT-1.3.1.zip) #need to rename/copy these files.
 	#os.system(f'octave-cli --no-gui -p "{source_dir}/omf/solvers/matpower7.0" --eval "install_matpower(1,1,1)"')
+	# Install matpower
+	os.system(f"wget -P {source_dir}/omf/solvers/ 'https://github.com/MATPOWER/matpower/releases/download/7.0/matpower7.0.zip'")
+	os.system(f"unzip '{source_dir}/omf/solvers/matpower7.0.zip' -d {source_dir}/omf/solvers/")
+	os.system(f'octave-cli --no-gui -p "{source_dir}/omf/solvers/matpower7.0" --eval "install_matpower(1,1,1)"')
 	# Install pygraphviz from wheel because it's finicky
 	graphVizBinPath = 'C:\\Program Files (x86)\\Graphviz2.38\\bin'
 	os.system(f'setx path "%path%;{graphVizBinPath}"')
@@ -92,7 +100,8 @@ elif platform.system()=="Darwin": # MacOS
 		sed -i '' 's/ans=""/ans="2"/g' configure;
 		sudo bash configure;
 	''') # sed is to hack the build to work without user input.
-	print('matpower dir', f"{source_dir}/omf/solvers/matpower7.0")
+	os.system(f"wget -P {source_dir}/omf/solvers/ 'https://github.com/MATPOWER/matpower/releases/download/7.0/matpower7.0.zip'")
+	os.system(f"unzip '{source_dir}/omf/solvers/matpower7.0.zip' -d {source_dir}/omf/solvers/")
 	os.system(f'octave-cli --no-gui -p "{source_dir}/omf/solvers/matpower7.0" --eval "install_matpower(1,1,1)"')
 	# pip installs
 	os.system("cd omf")
