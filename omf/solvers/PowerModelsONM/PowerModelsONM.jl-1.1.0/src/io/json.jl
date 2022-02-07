befd207d99273da3401025e966e24268abddd98c
David Pinney <david@pinney.org>
2022-02-07 14:08:44 -0500

    removing things that shouldn't be here

--- omf/solvers/PowerModelsONM/PowerModelsONM.jl-1.1.0/src/io/json.jl
+++
@@ -1,8 +0,0 @@
-"""
-    load_schema(file::String)::JSONSchema.Schema
-
-Loads a JSON Schema for validation, fixing the ref paths inside the schemas on load
-"""
-function load_schema(file::String)::JSONSchema.Schema
-    return JSONSchema.Schema(JSON.parsefile(file); parent_dir=joinpath(dirname(pathof(PowerModelsONM)), ".."))
-end
