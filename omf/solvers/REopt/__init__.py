3e612d14c5b73826c040bb11212c6afff2d0fe39
Matt Stevenson <mmstevenson@gmail.com>
2022-04-27 16:04:10 -0700

    updating API keys for REopt

--- omf/solvers/REopt/__init__.py
+++ omf/solvers/REopt/__init__.py
@@ -4,8 +4,12 @@ from omf.solvers.REopt import logger
 from omf.solvers.REopt import results_poller
 
 def run(inJSONPath, outputPath):
-	API_KEY = 'WhEzm6QQQrks1hcsdN0Vrd56ZJmUyXJxTJFg6pn9'  # REPLACE WITH YOUR API KEY
+	API_KEY = 'WhEzm6QQQrks1hcsdN0Vrd56ZJmUyXJxTJFg6pn9'  # OMF KEY. REPLACE WITH YOUR API KEY
 	# API_KEY = 'Y8GMAFsqcPtxhjIa1qfNj5ILxN5DH5cjV3i6BeNE'
+	# API_KEY = 'etg8hytwTYRf4CD0c4Vl9U7ACEQnQg6HV2Jf4E5W'
+	# API_KEY = 'BNFaSCCwz5WkauwJe89Bn8FZldkcyda7bNwDK1ic'		 
+	# API_KEY = 'L2e5lfH2VDvEm2WOh0dJmzQaehORDT8CfCotaOcf'
+	# API_KEY = '08USmh2H2cOeAuQ3sCCLgzd30giHjfkhvsicUPPf'
 	root_url = 'https://developer.nrel.gov/api/reopt'
 	post_url = root_url + '/v1/job/?api_key=' + API_KEY
 	results_url = root_url + '/v1/job/<run_uuid>/results/?api_key=' + API_KEY
@@ -28,8 +32,12 @@ def run(inJSONPath, outputPath):
 		logger.log.info("Saved results to {}".format(outputPath))
 
 def runResilience(runID, outputPath):
-	API_KEY = 'WhEzm6QQQrks1hcsdN0Vrd56ZJmUyXJxTJFg6pn9'  # REPLACE WITH YOUR API KEY
+	API_KEY = 'WhEzm6QQQrks1hcsdN0Vrd56ZJmUyXJxTJFg6pn9'  # OMF KEY. REPLACE WITH YOUR API KEY
 	# API_KEY = 'Y8GMAFsqcPtxhjIa1qfNj5ILxN5DH5cjV3i6BeNE'
+	# API_KEY = 'etg8hytwTYRf4CD0c4Vl9U7ACEQnQg6HV2Jf4E5W'
+	# API_KEY = 'BNFaSCCwz5WkauwJe89Bn8FZldkcyda7bNwDK1ic'
+	# API_KEY = 'L2e5lfH2VDvEm2WOh0dJmzQaehORDT8CfCotaOcf'
+	# API_KEY = '08USmh2H2cOeAuQ3sCCLgzd30giHjfkhvsicUPPf'
 	root_url = 'https://developer.nrel.gov/api/reopt'
 	post_url = root_url + '/v1/outagesimjob/?api_key=' + API_KEY
 	results_url = root_url + '/v1/job/<RUN_ID>/resilience_stats/?api_key=' + API_KEY
@@ -57,25 +65,31 @@ def runResilience(runID, outputPath):
 		logger.log.info("Saved results to {}".format(outputPath))
 
 def _test():
-	run('Scenario_POST35.json', 'results_S35.json')
+	run('Scenario_POST72.json', 'results_S72.json')
 
-	with open('results_S35.json') as jsonFile:
+	with open('results_S72.json') as jsonFile:
 		results = json.load(jsonFile)
 		
 	test_ID = results['outputs']['Scenario']['run_uuid']
 	print("PV size_kw:", results['outputs']['Scenario']['Site']['PV']['size_kw'])
 	print("Storage size_kw:", results['outputs']['Scenario']['Site']['Storage']['size_kw'])
 	print("Storage size_kwh:", results['outputs']['Scenario']['Site']['Storage']['size_kwh'])
-	print("Wind size_kw:", results['outputs']['Scenario']['Site']['Wind']['size_kw'])
+	# print("Wind size_kw:", results['outputs']['Scenario']['Site']['Wind']['size_kw'])
 	print("Generator fuel_used_gal:", results['outputs']['Scenario']['Site']['Generator']['fuel_used_gal'])
+	print("Generator fuel_used_gal_bau:", results['outputs']['Scenario']['Site']['Generator']['fuel_used_gal_bau'])
 	print("Generator size_kw:", results['outputs']['Scenario']['Site']['Generator']['size_kw'])
+	print("Generator average_yearly_energy_produced_kwh:", results['outputs']['Scenario']['Site']['Generator']['average_yearly_energy_produced_kwh'])
+	print("Generator average_yearly_energy_exported_kwh:", results['outputs']['Scenario']['Site']['Generator']['average_yearly_energy_exported_kwh'])
+	print("Generator existing_gen_total_fuel_cost_us_dollars:", results['outputs']['Scenario']['Site']['Generator']['existing_gen_total_fuel_cost_us_dollars'])
+	print("Generator year_one_emissions_lb_C02:", results['outputs']['Scenario']['Site']['Generator']['year_one_emissions_lb_C02'])
+	print("Generator year_one_emissions_bau_lb_C02:", results['outputs']['Scenario']['Site']['Generator']['year_one_emissions_bau_lb_C02'])
 	print("npv_us_dollars:", results['outputs']['Scenario']['Site']['Financial']['npv_us_dollars'])
 	print("initial_capital_costs:", results['outputs']['Scenario']['Site']['Financial']['initial_capital_costs'])
-	print("CHP size_kw:", results['outputs']['Scenario']['Site']['CHP']['size_kw'])
-	print("CHP year_one_fuel_used_mmbtu:", results['outputs']['Scenario']['Site']['CHP']['year_one_fuel_used_mmbtu'])
-	print("CHP year_one_electric_energy_produced_kwh:", results['outputs']['Scenario']['Site']['CHP']['year_one_electric_energy_produced_kwh'])
+	# print("CHP size_kw:", results['outputs']['Scenario']['Site']['CHP']['size_kw'])
+	# print("CHP year_one_fuel_used_mmbtu:", results['outputs']['Scenario']['Site']['CHP']['year_one_fuel_used_mmbtu'])
+	# print("CHP year_one_electric_energy_produced_kwh:", results['outputs']['Scenario']['Site']['CHP']['year_one_electric_energy_produced_kwh'])
 
-	runResilience(test_ID, 'resultsResilience_S35.json')
+	# runResilience(test_ID, 'resultsResilience_S40.json')
 
 
 
