fccf89d0cf871def19ca82bb0d373a3319eef17a
rtm8mg <rtm8mg@virginia.edu>
2022-09-27 12:25:44 -0400

    microgridControl bug fixes (with correct file paths)
    Added code to handle issues revolving spaces in openDss object lists, fixed kWTemplate mismatching array size error

--- omf/models/microgridControl.py
+++ omf/models/microgridControl.py
@@ -274,7 +274,7 @@ def customerCost1(duration, season, averagekWperhr, businessType):
 		kWTemplate[1.0] = np.array([3,4,5,6,7,8,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10])
 		kWTemplate[0.25] = np.array([2,3,4,5,5,6,7,7,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8,8])
 	# NOTE: We set a minimum average kWperhr value so the model doesn't crash for low values.
-	kWTemplate[0] = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
+	kWTemplate[0] = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
 	def kWhApprox(kWDict, averagekWperhr, iterate):
 		'helper function for approximating customer outage cost based on annual kWh by iteratively "averaging" the curves'
@@ -754,7 +754,7 @@ def graphMicrogrid(pathToOmd, pathToJson, pathToCsv, outputFile, settingsFile, u
 	outageCostsByType = {busType: [] for busType in businessTypes}
 	avgkWColumn = []
 	durationColumn = []
-	dssTree = dssConvert.dssToTree(f'{workDir}/circuitOmfCompatible.dss')
+	dssTree = dssConvert.dssToTree(f'{workDir}/circuitOmfCompatible_cleanLists.dss')
 	loadShapeMeanMultiplier = {}
 	loadShapeMeanActual = {}
 	for dssLine in dssTree:
@@ -916,7 +916,8 @@ def buildCustomSettings(settingsCSV='', feeder='', customSettings='customSetting
 			tree = json.load(omdFile)['tree']
 		niceDss = dssConvert.evilGldTreeToDssTree(tree)
 		dssConvert.treeToDss(niceDss, 'circuitOmfCompatible.dss')
-		dssTree = dssConvert.dssToTree('circuitOmfCompatible.dss')
+		dssConvert.dssCleanLists('circuitOmfCompatible.dss')
+		dssTree = dssConvert.dssToTree('circuitOmfCompatible_cleanLists.dss')
 	else: return('Error: Feeder must be an OMD file.')
 	outageAssets = [] # formerly row[0] for row in outageReader
 	customEventList = []
@@ -953,6 +954,7 @@ def work(modelDir, inputDict):
 	niceDss = dssConvert.evilGldTreeToDssTree(tree)
 	dssConvert.treeToDss(niceDss, f'{modelDir}/circuit.dss')
 	dssConvert.treeToDss(niceDss, f'{modelDir}/circuitOmfCompatible.dss') # for querying loadshapes
+	dssConvert.dssCleanLists(f'{modelDir}/circuitOmfCompatible.dss') # for querying loadshapes
 
 	# Remove syntax that ONM doesn't like.
 	with open(f'{modelDir}/circuit.dss','r') as dss_file:
