75ba67312818b10c1d46efb40af6db09f1e113f8
David Pinney <david@pinney.org>
2023-03-22 16:39:28 -0400

    updates to testing infrastructure

--- omf/models/hostingCapacity.py
+++ omf/models/hostingCapacity.py
@@ -1,12 +1,10 @@
 ''' Calculate solar photovoltaic system output using PVWatts. '''
 
-import shutil, datetime
+import shutil
 from os.path import join as pJoin
-import mohca_cl;
 
 # OMF imports
-from omf import weather
-from omf.solvers import nrelsam2013
+import omf
 from omf.models import __neoMetaModel__
 from omf.models.__neoMetaModel__ import *
 
@@ -16,10 +14,10 @@ modelName, template = __neoMetaModel__.metadata(__file__)
 hidden = False
 
 def work(modelDir, inputDict):
-
+	import mohca_cl
 	rowCount = 0
 	# Writing the content from the input into a file for the mohca algorithm to read in.
-	with open(pJoin(modelDir,"meter_data_input.csv"),"w", newline='') as pv_stream:
+	with open(pJoin(modelDir,inputDict['inputDataFileName']),"w", newline='') as pv_stream:
 		pv_stream.write(inputDict['inputDataFileContent'])
 	'''
 	# Checking the number of rows in the file
@@ -44,10 +42,10 @@ def work(modelDir, inputDict):
 	elif (inputDict[ "mohcaAlgorithm" ] == "sandia2"):
 		mohcaOutput = mohca_cl.sandia2( modelDir + "/meter_data_input.csv", modelDir + "/mohcaOutput.csv" )
 	'''
-
-
 	mohcaOutput = {}
-	mohcaOutput = mohca_cl.sandia1( modelDir + "/meter_data_input.csv", modelDir + "/mohcaOutput.csv" )
+	input_path = pJoin(modelDir, inputDict['inputDataFileName'])
+	output_path = pJoin(modelDir, "mohcaOutput.csv")
+	mohcaOutput = mohca_cl.sandia1(input_path, output_path)
     # Should I worry about validating the output that the algorithm returns?
 	# mohcaAlgo = csvValidateAndLoad(mohcaOutput.csv, modelDir, header=None, ignore_nans=False, return_type='list_by_col', save_file=None)
 	
@@ -65,9 +63,13 @@ def runtimeEstimate(modelDir):
 
 def new(modelDir):
 	''' Create a new instance of this model. Returns true on success, false on failure. '''
+	test_file_name = 'sandia_loc1_test_data.csv'
+	test_file_path = pJoin(omf.omfDir,'static','testFiles',test_file_name)
+	test_file_contents = open(test_file_path).read()
 	defaultInputs = {
 		"dataVariableName": "None",
-		"inputDataFile": "None",
+		"inputDataFileName": test_file_name,
+		"inputDataFileContent": test_file_contents,
 		"modelType": modelName,
 		"mohcaAlgorithm": "None"
 	}
