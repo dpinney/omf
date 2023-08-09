f1cd6b84868b5249b81a6eca8219dcce35fed388
SaeedRazavi <saeed.louis.razavi@gmail.com>
2023-08-09 12:13:21 -0400

    Add evaluation and graphing for outage incidence and disruption speed

--- omf/models/restoration.py
+++ omf/models/restoration.py
@@ -1,5 +1,8 @@
-import os, shutil
-from os.path import join as pJoin
+'''Calculate the Impacts of Intervention on Outage Incidence'''
+from asyncio.windows_events import NULL
+import pandas as pd
+from random import random
+import numpy as np
 
 # OMF imports
 from omf.models import __neoMetaModel__
@@ -31,15 +34,38 @@ def castAddInputs(val1,val2):
 		except ValueError:
 			return val1+val2
 
+def cleanIntegerInput(inputVal, defaultVal=0, minVal=float('-inf'), maxVal=float('inf')):
+	''' Tries to return the inputVal cast to an integer. If unsuccessful, returns the defaultVal. 
+	If inputVal is outside of bounds, set to closest bound.'''
+	if minVal >= maxVal: 
+		raise Exception("minVal must be < maxVal")
+	try:
+		return max(minVal,min(maxVal,int(inputVal)))
+	except ValueError:
+		return defaultVal
+
+
 def work(modelDir, inputDict):
 	''' Run the model in its directory. '''
 	# Delete output file every run if it exists
-	outData = {}		
+	outData = {}
 
 	# Model operations goes here.
-	inputOne = inputDict.get("number1", 123)
-	inputTwo = inputDict.get("number2", 867)
-	outData["output"] = castAddInputs(inputOne, inputTwo)
+	outageDataCSV = pJoin(omf.omfDir, "static", "testFiles", "restoration", inputDict.get("exampleFileName"))
+	dfOutage = pd.read_csv(outageDataCSV,index_col=0)
+	dfOutageIncidence = dfOutage.drop(['latitude','longitude']).sum(axis=1,).map(lambda x:100*(1.0-(x/dfOutage.shape[1])))
+	timeStep = int(dfOutageIncidence.index[-1])-int(dfOutageIncidence.index[-2])
+	steps = cleanIntegerInput(inputDict.get("timeStepsInInterval",1), defaultVal=1, maxVal=dfOutageIncidence.size-1)
+	dSpeedInterval = timeStep*steps
+
+	outData["timeStep"] = timeStep
+	outData["dSpeedInterval"] = dSpeedInterval
+	outData["startTime"] = int(dfOutageIncidence.index[0])
+	outData["endTime"] = int(dfOutageIncidence.index[-1])
+	outData["outageIncidence"] = dfOutageIncidence.values.tolist()
+	
+	dfDisruptionSpeed = dfOutageIncidence.diff(steps).div(dSpeedInterval)
+	outData["disruptionSpeed"] = dfDisruptionSpeed.fillna(0).values.tolist()
 	# Model operations typically end here.
 
 	# Stdout/stderr.
@@ -50,7 +76,7 @@ def work(modelDir, inputDict):
 
 def new(modelDir):
 	''' Create a new instance of this model. Returns true on success, false on failure. '''
-	omd_fn = "ieee240.dss"
+	omd_fn = "iowa240c1.clean.dss"
 	ex_fn = "example.csv" # example file input
 
 	with open(pJoin(omf.omfDir, "static", "testFiles", "restoration", ex_fn)) as ex_stream:
@@ -61,6 +87,7 @@ def new(modelDir):
 		"date": "2019-07-01T00:00:00Z",
 		"number1": "123",
 		"number2": "987",
+		"timeStepsInInterval":"1",
 		"feederName1": omd_fn,
 		"exampleFileName": ex_fn,
 		"exampleFileContent": ex_ins,
