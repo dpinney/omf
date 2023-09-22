0cdcaf807a2fe8df029cd28f1deb5f6c22adf02d
David Pinney <david@pinney.org>
2023-09-22 14:26:29 -0400

    testing envo

---
+++ omf/scratch/mohca/hosting_cap_repro.py
@@ -0,0 +1,6 @@
+# Example of hosting capacity for all buses with loads, i.e. metered buses.
+fname = '../solvers/opendss/iowa240.clean.dss'
+# meter_buses = get_meter_buses(fname)
+# iowa_hosting = hosting_capacity_all(fname, 10, 10.0, BUS_LIST=meter_buses)
+# iowa_df = list_of_dicts_to_dataframe(iowa_hosting)
+# print( iowa_df )
