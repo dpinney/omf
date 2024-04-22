2aaea6777dc5d229df3fd75f4562c9d2879fdae5
DavidArmahJr <111519747+DavidArmahJr@users.noreply.github.com>
2024-04-22 12:24:56 -0400

    Update leafletLayer.js
    added code to determine color of social vulnerability polygons

--- omf/static/geoJsonMap/v3/leafletLayer.js
+++ omf/static/geoJsonMap/v3/leafletLayer.js
@@ -285,9 +285,31 @@ class LeafletLayer { // implements ObserverInterface
                 }
             }
         } else {
+             if (this.#observable.hasProperty('SOVI_RATNG', 'meta') && this.#observable.getProperty('SOVI_RATNG', 'meta') === 'Very High') {
+                return {
+                    color: 'blue'
+                }
+            } else if (this.#observable.hasProperty('SOVI_RATNG', 'meta') && this.#observable.getProperty('SOVI_RATNG', 'meta') === 'Relatively High') {
+                return {
+                    color: 'lightblue'
+                }
+            }else if (this.#observable.hasProperty('SOVI_RATNG', 'meta') && this.#observable.getProperty('SOVI_RATNG', 'meta') === 'Relatively Moderate') {
+                    return {
+                        color: 'lightgreen'
+                    }
+                } else if (this.#observable.hasProperty('SOVI_RATNG', 'meta') && this.#observable.getProperty('SOVI_RATNG', 'meta') === 'Relatively Low') {
+                        return {
+                            color: 'yellow'
+                        }
+                    } else if (this.#observable.hasProperty('SOVI_RATNG', 'meta') && this.#observable.getProperty('SOVI_RATNG', 'meta') === 'Very Low') {
+                        return {
+                            color: 'grey'
+                        }
+                    }else {
             return {
                 color: 'blue'
             }
+        }
         }
     }
 }
