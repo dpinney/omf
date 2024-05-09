7508ffe71fa28738cf735af70b13a01654fd5a4c
astronobri <brianna.mills@nreca.coop>
2024-05-09 00:50:52 -0400

    derUtilityCost: Added timeseries plot with temperatures

--- omf/models/derUtilityCost.py
+++ omf/models/derUtilityCost.py
@@ -4,16 +4,17 @@ controlling behind-the-meter distributed energy resources (DERs).'''
 ## TODO: check out gridlabMulti.py, cvrDynamic.py, demandResponse.py, 
 ## vbatDispatch.py, solarEngineering.py for potential display and plot feautures 
 
+# Python imports
 import warnings
 # warnings.filterwarnings("ignore")
-
 import shutil, datetime, csv, json
 from os.path import join as pJoin
 import numpy as np
 import pandas as pd
+import plotly.graph_objs as go
+import plotly.utils
 
 # OMF imports
-from omf.models.voltageDrop import drawPlot
 from omf.models import __neoMetaModel__
 from omf.models.__neoMetaModel__ import *
 from omf.models import vbatDispatch as vb
@@ -92,7 +93,120 @@ def work(modelDir, inputDict):
 			print(f"File '{results_file}' not found.")
 			raise
 	
+	## Test plot
+	showlegend = False #temporarily disable the legend toggle
+
+	PV = reoptResults['outputs.PV.electric_to_load_series_kw'][0]
+	BESS = reoptResults['outputs.ElectricStorage.storage_to_load_series_kw'][0]
+	vbpower_series = pd.Series(vbatResults['VBpower'][0])
+	vbat_discharge = vbpower_series.where(vbpower_series < 0, 0) #negative values
+	vbat_charge = vbpower_series.where(vbpower_series > 0, 0) ## positive values; part of the New Load?
+	vbat_discharge_flipsign = vbat_discharge.mul(-1) ## flip sign of vbat discharge for plotting purposes
+	grid_to_load = reoptResults['outputs.ElectricUtility.electric_to_load_series_kw'][0]
+	grid_charging_BESS = reoptResults['outputs.ElectricUtility.electric_to_storage_series_kw'][0]
+
+
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=timestamps,
+                         y=np.asarray(BESS) + np.asarray(demand) + np.asarray(vbat_discharge_flipsign),
+						 yaxis='y1',
+                         mode='none',
+                         fill='tozeroy',
+                         name='BESS Serving Load (kW)',
+                         fillcolor='rgba(0,137,83,1)',
+						 showlegend=showlegend))
+	fig.update_traces(fillpattern_shape='/', selector=dict(name='BESS Serving Load (kW)'))
+
+	fig.add_trace(go.Scatter(x=timestamps,
+							y=np.asarray(vbat_discharge_flipsign)+np.asarray(demand),
+							yaxis='y1',
+							mode='none',
+							fill='tozeroy',
+							fillcolor='rgba(127,0,255,1)',
+							name='vbat Serving Load (kW)',
+							showlegend=showlegend))
+	fig.update_traces(fillpattern_shape='/', selector=dict(name='vbat Serving Load (kW)'))
+
+	fig.add_trace(go.Scatter(x=timestamps,
+                         y=np.asarray(demand)-np.asarray(BESS)-np.asarray(vbat_discharge_flipsign),
+						 yaxis='y1',
+                         mode='none',
+                         name='Original Load (kW)',
+                         fill='tozeroy',
+                         fillcolor='rgba(100,200,210,1)',
+						 showlegend=showlegend))
+
+	fig.add_trace(go.Scatter(x=timestamps,
+                         y=np.asarray(demand) + np.asarray(reoptResults['outputs.ElectricUtility.electric_to_storage_series_kw'][0]) + np.asarray(vbat_charge),
+						 yaxis='y1',
+                         mode='none',
+                         name='Additional Load (Charging BESS and vbat)',
+                         fill='tonexty',
+                         fillcolor='rgba(175,0,42,0)',
+						 showlegend=showlegend))
+	fig.update_traces(fillpattern_shape='.', selector=dict(name='Additional Load (Charging BESS and vbat)'))
 	
+
+	grid_serving_new_load = np.asarray(grid_to_load) + np.asarray(grid_charging_BESS)+ np.asarray(vbat_charge) - np.asarray(vbat_discharge_flipsign) + np.asarray(PV)
+	fig.add_trace(go.Scatter(x=timestamps,
+                         y=grid_serving_new_load,
+						 yaxis='y1',
+                         mode='none',
+                         fill='tozeroy',
+                         name='Grid Serving Load (kW)',
+                         fillcolor='rgba(192,192,192,1)',
+						 showlegend=showlegend))
+	
+	fig.add_trace(go.Scatter(x=timestamps,
+						  y=temperatures,
+						  yaxis='y2',
+						  mode='lines',
+						  line=dict(color='red',width=1),
+						  name='Average Temperature',
+						  showlegend=showlegend 
+						  ))
+
+	#fig.add_trace(go.Scatter(x=timestamps, 
+	#				 y=demand,
+	#				 yaxis='y1',
+	#				 mode='lines',
+	#				 line=dict(color='black'),
+	#				 name='Demand',
+	#				 showlegend=showlegend))
+	#fig.update_traces(legendgroup='Demand', visible='legendonly', selector=dict(name='Original Load (kW)')) ## Make demand hidden on plot by default
+
+	fig.add_trace(go.Scatter(x=timestamps,
+					 y=PV,
+					 yaxis='y1',
+					 mode='none',
+					 fill='tozeroy',
+					 name='PV Serving Load (kW)',
+					 fillcolor='rgba(255,246,0,1)',
+					 showlegend=showlegend
+					 ))
+	
+	fig.update_layout(
+    	title='Utility Data Test',
+    	xaxis=dict(title='Timestamp'),
+    	yaxis=dict(title="Energy (kW)"),
+    	yaxis2=dict(title='degrees Celsius',
+                overlaying='y',
+                side='right'
+                ),
+    	legend=dict(
+			orientation='h',
+			yanchor="bottom",
+			y=1.02,
+			xanchor="right",
+			x=1
+			)
+	)
+
+	fig.show()
+
+	## Encode plot data as JSON for showing in the HTML 
+	outData['plotlyPlot'] = json.dumps(fig.data, cls=plotly.utils.PlotlyJSONEncoder)
+	outData['plotlyLayout'] = json.dumps(fig.layout, cls=plotly.utils.PlotlyJSONEncoder)
 
 
 	# Model operations typically ends here.
@@ -126,7 +240,7 @@ def new(modelDir):
 		"tempCurve": temp_curve,
 		"outage": True,
 		"outage_start_hour": '2100',
-		"outage_duration": '3',
+		"outageDuration": '3',
 		"solar" : "on",
 		"battery" : "on",
 		"generator" : "off",
@@ -150,6 +264,24 @@ def new(modelDir):
 	return __neoMetaModel__.new(modelDir, defaultInputs)
 
 @neoMetaModel_test_setup
+def _tests():
+	# Location
+	modelLoc = pJoin(__neoMetaModel__._omfDir,"data","Model","admin","Automated Testing of " + modelName)
+	# Blow away old test results if necessary.
+	try:
+		shutil.rmtree(modelLoc)
+	except:
+		# No previous test results.
+		pass
+	# Create New.
+	new(modelLoc)
+	# Pre-run.
+	__neoMetaModel__.renderAndShow(modelLoc) ## Why is there a pre-run?
+	# Run the model.
+	__neoMetaModel__.runForeground(modelLoc)
+	# Show the output.
+	__neoMetaModel__.renderAndShow(modelLoc)
+
 def _debugging():
 	## Location
 	modelLoc = pJoin(__neoMetaModel__._omfDir,"data","Model","admin","Automated Testing of " + modelName)
