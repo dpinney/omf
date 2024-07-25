51577224f8ead70835d15b49caceca070baa26c3
jenny <jenthakkar@gmail.com>
2024-07-25 13:01:06 -0400

    Example EPRI for meeting

--- omf/scratch/hostingcapacity/dsstestcircuit/epri.py
+++ omf/scratch/hostingcapacity/dsstestcircuit/epri.py
@@ -16,29 +16,27 @@ def create_testfile( modelDir: PosixPath ):
 
   monitor_names = []
   load_profiles = os.path.join(  modelDir, 'dsstestcircuit', 'Profiles' )
-  master_dss_file = os.path.join( modelDir, 'dsstestcircuit', 'Master copy.DSS')
+  master_dss_file = 'Master copy.DSS'
 
-  with open( os.path.join( modelDir, 'dsstestcircuit', 'Master copy.DSS' ) ) as file:
+  with open( os.path.join( modelDir, 'dsstestcircuit', master_dss_file ) ) as file:
     for line in file:
       opendss.runDssCommand( line )
-  
-  volt_df = pd.read_csv('volts.csv')
-
   # Need a meter at every load
+
+  # vsource, isource, generator, load, capacitor, regcontrol.
+
   add_to_dss = ''
   for file in os.listdir(load_profiles):
     if file.endswith('.dbl'):
       load_file_name = Path(file).stem
+      load_file_name = load_file_name[:-2].capitalize()
       monitor_name = f'monitor-load-{load_file_name}'
-      obj_loadshape = 'Loadshape'
-      # need to get rid of the _P and _Q
-      # don't know if this works. I deleted it with vim
-      # load_file_name = load_file_name[:-2]
-      add_to_dss += f'new object=monitor.{monitor_name} element={obj_loadshape}.{load_file_name} terminal=1 mode=6\n'
+      obj_load = 'Load'
+      add_to_dss += f'new object=monitor.{monitor_name} element={obj_load}.{load_file_name} terminal=1 mode=0\n'
       monitor_names.append(monitor_name)
 
   add_to_dss += f'set mode=yearly stepsize=15m\n'
-  add_to_dss += f'set number=1\n'
+  add_to_dss += f'set number=35040\n'
   add_to_dss += 'solve\n'
   for name in monitor_names:
     add_to_dss += f'export monitors monitorname={name}\n'
@@ -46,10 +44,22 @@ def create_testfile( modelDir: PosixPath ):
   with open( os.path.join(modelDir, 'dsstestcircuit', 'Master copy.DSS'), 'a') as run_file:
     run_file.write(add_to_dss)
 
+def run_testfile():
+  with open( os.path.join( modelDir, 'dsstestcircuit', 'Master copy.DS' ) ) as file:
+    for line in file:
+      opendss.runDssCommand( line )
+
 if __name__ == '__main__':
   epri_file = Path( omf.omfDir, 'solvers', 'opendss', 'epriSecondaryTestCircuit.clean.dss' )
   modelDir = Path(omf.omfDir, 'scratch', 'hostingcapacity')
+
   # run_trad( epri_file, modelDir )
   # create_testfile( modelDir )
-  master_dss_file = os.path.join( modelDir, 'dsstestcircuit', 'Master copy.DSS')
-  opendss.runDSS(master_dss_file)
+  # run_testfile()
+  # volt_df = pd.read_csv('volts.csv')
+  test_df_load_1_1 = pd.read_csv(  os.path.join( modelDir, 'dsstestcircuit', 'SecondaryTestCircuit_Mon_monitor-load-load1_1_1.csv') )
+  test_df_load_1_1['datetime'] = test_df_load_1_1.apply(lambda row: f"{row[0]}:{row[1]:.0f}Z", axis=1)
+  # convert seconds to minutes
+  # remove seconds
+  # change the 0 before the . to be a fake date and then increment that fake date
+  print( test_df_load_1_1.head)
