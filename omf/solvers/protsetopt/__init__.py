7e2442722e9e372cc8259d41f3998567932d0e37
Lily Olson <lilycatolson@gmail.com>
2024-07-30 10:44:11 -0700

    updates to protsetopt solver and microgridPlan

--- omf/solvers/protsetopt/__init__.py
+++ omf/solvers/protsetopt/__init__.py
@@ -23,18 +23,18 @@ def install_pso_env(clean=False, system : list = platform.system()):
     os.system(f'unzip -o "{thisDir}/pso_source.zip" -d "{thisDir}/pso_env/"')
     os.system(f'source "{thisDir}/pso_env/bin/activate"; python -m pip install -r "{thisDir}/pso_env/Protection-settings-optimizer-main/requirements.txt"')
 
-def install_pso_env_without_download(system : list = platform.system()):
-    os.system(f'{sys.executable} -m venv "{thisDir}/pso_env"')
-    os.system(f'source "{thisDir}/pso_env/bin/activate"; {sys.executable} -m pip install -e git+https://github.com/lilycatolson/Protection-settings-optimizer.git#egg=RSO_pack')
+#def install_pso_env_without_download(system : list = platform.system()):
+#    os.system(f'{sys.executable} -m venv "{thisDir}/pso_env"')
+#    os.system(f'source "{thisDir}/pso_env/bin/activate"; {sys.executable} -m pip install -e git+https://github.com/lilycatolson/Protection-settings-optimizer.git#egg=RSO_pack')
 
 def install_pso(clean=False, system : list = platform.system()):
     ''' installs dependency for protsetopt'''
     instantiated_path = os.path.normpath(os.path.join(thisDir,"instantiated.txt"))
     if not os.path.isfile(instantiated_path):
         try:
-            install_pso_env_without_download()
-            #install_pso_env(clean=clean, system=system) #put back in once other version tested
-            #os.system(f"{sys.executable} -m pip install -e git+https://github.com/lilycatolson/Protection-settings-optimizer.git#egg=RSO_pack")
+            #install_pso_env_without_download()
+            #install_pso_env(clean=clean, system=system)
+            os.system(f"{sys.executable} -m pip install -e git+https://github.com/lilycatolson/Protection-settings-optimizer.git#egg=RSO_pack")
             if system == "Windows":
                 os.system(f'copy nul {instantiated_path}')
             else:
@@ -49,24 +49,28 @@ def run_venv_cmd(cmd=''):
     out = check_output([f'''source "{thisDir}/pso_env/bin/activate"; cd "{thisDir}"; pwd; python -c 'import RSO_pack; {cmd}' '''], shell=True)
     return out.decode('utf-8')
 
-def run_pso(testPath, testFile):
+def run_pso(testPath, testFile, Fres=['0.001','1'], Fts=['3ph','SLG','LL'], Force_NOIBR = 1, enableIT = 0, CTI = 0.25, OTmax = 10,
+        type_select = False, Fault_Res = ['R0_001','R1'], Min_Ip = [0.1,0.1], Substation_bus = 'sourcebus'):
+    ''' run ProtectionSettingsOptimizer with OpenDSS '''
     install_pso()
     try:
         import RSO_pack
     except (ImportError, ModuleNotFoundError):
         from . import RSO_pack
 
-    RSO_pack.run(testPath, testFile)
+    RSO_pack.run(testPath, testFile, Fres=Fres, Fts=Fts, Force_NOIBR = Force_NOIBR, enableIT = enableIT, CTI = CTI, OTmax = OTmax, 
+                 type_select = type_select, Fault_Res = Fault_Res, Min_Ip = Min_Ip, Substation_bus = Substation_bus)
 
 def run_pso_venv(testPath, testFile):
+    ''' run ProtectionSettingsOptimizer with OpenDSS, within a virutal environment (WIP) '''
     install_pso()
     run_venv_cmd(cmd=f'RSO_pack.run("{testPath}", "{testFile}")')
 
 def _test():
     testPath = os.path.normpath(os.path.join(thisDir, 'testFiles'))
     testFile = 'IEEE34Test.dss'
-    #run_pso(testPath, testFile)
-    run_pso_venv(testPath, testFile)
+    run_pso(testPath, testFile)
+    #run_pso_venv(testPath, testFile)
 
 if __name__ == "__main__":
     _test()
