01edde62f58bffd696c552b73217c22b29cc9f23
ASC95 <16790624+ASC95@users.noreply.github.com>
2024-09-27 17:57:27 -0400

    show the spinner gif even in offline mode

--- omf/static/geoJsonMap/v4/ui-components/loading-span/loading-span.js
+++ omf/static/geoJsonMap/v4/ui-components/loading-span/loading-span.js
@@ -11,7 +11,10 @@ class LoadingSpan {
         this.span.classList.add('loading-span');
         this.#gif = document.createElement('img');
         this.#gif.classList.add('gif');
+        // - Sometimes, the gif comes from the web server
         this.#gif.src = '/static/geoJsonMap/v4/ui-components/loading-span/spinner.gif'
+        // - In offline mode, check in the current directory for the gif
+        this.#gif.setAttribute('onerror', "this.onerror=null;this.src='./spinner.gif';");
         this.span.append(this.#gif);
         this.#label = document.createElement('span');
         this.#label.classList.add('label');
