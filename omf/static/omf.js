9e528e5ef1b88466da133d89d05a23aeccaeaeda
dpinney <david@pinney.org>
2024-10-10 14:56:28 -0400

    cleanup omf.js, document, add todos

--- omf/static/omf.js
+++ omf/static/omf.js
@@ -1,22 +1,5 @@
-function post_to_url(path, params, method) {
-	method = method || 'post' // Set method to post by default, if not specified.
-	var form = document.createElement('form')
-	form.setAttribute('method', method)
-	form.setAttribute('action', path)
-	for(var key in params) {
-		if(params.hasOwnProperty(key)) {
-			var hiddenField = document.createElement('input')
-			hiddenField.setAttribute('type', 'hidden')
-			hiddenField.setAttribute('name', key)
-			hiddenField.setAttribute('value', params[key])
-			form.appendChild(hiddenField)
-		}
-	}
-	document.body.appendChild(form)
-	form.submit()
-}
-
 function updateFileNameDisplay(fileInputID, userFileDisplayNameID, dataFileNameID) {
+	// Used with the direct file upload handler.
 	var fileInput = document.getElementById(fileInputID);
 	var displayInput = document.getElementById(userFileDisplayNameID);
 
@@ -37,19 +20,6 @@ function handle_files(files, contentsId, nameId) {
 	}
 }
 
-function ajaxReq(requestType, URL, asynch) {
-	var xmlhttp
-	if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest()
-	}
-	else {// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP")
-	}
-	xmlhttp.open(requestType, URL, asynch)
-	xmlhttp.send()
-	return xmlhttp.responseText
-}
-
 function gebi(id) {
 	// Shorten a much-used method name:
 	return document.getElementById(id)
@@ -57,6 +27,7 @@ function gebi(id) {
 
 function clone(obj) {
 	// Deep copy of a given obj.
+	// Todo: move into gridEdit.html, remove from here.
 	if (null == obj || "object" != typeof obj) return obj;
 	var copy = obj.constructor();
 	for (var attr in obj) {
@@ -66,6 +37,7 @@ function clone(obj) {
 }
 
 function tableClear(table) {
+	// Todo: move into gridEdit.html, remove from here.
 	try {
 		while (table.rows.length>0) table.deleteRow(table.rows.length-1)
 	}
@@ -74,16 +46,9 @@ function tableClear(table) {
 	}
 }
 
-function getRadioSetting (className) {
-	// Get which radio button is set for a group with a given className.
-	value='';
-	radios = document.getElementsByClassName(className);
-	for (x in radios) {if (radios[x].checked) value = radios[x].value}
-	return value
-}
-
 function time(func) {
 	// How long does func take to execute?
+	// Todo: move into microgridPlan.html
 	var start = +new Date()
 	func()
 	var end =  +new Date()
@@ -91,30 +56,35 @@ function time(func) {
 }
 
 function round(number,precision) {
+	// Simple rounding function used a couple different places.
 	return parseFloat(number.toPrecision(precision))
 }
 
 function randomGaussian() {
-  // Get a Gaussian from a uniform(0,1) via the Box-Muller transform.
-  do {
-	x1 = 2 * Math.random() - 1
-	x2 = 2 * Math.random() - 1
-	rad = x1 * x1 + x2 * x2
-  } while (rad >= 1 || rad == 0)
-  c = Math.sqrt(-2 * Math.log(rad) / rad);
-  return x1 * c
+	// Get a Gaussian from a uniform(0,1) via the Box-Muller transform.
+	// Todo: move into gridEdit.html, remove from here.
+	do {
+		x1 = 2 * Math.random() - 1
+		x2 = 2 * Math.random() - 1
+		rad = x1 * x1 + x2 * x2
+	} while (rad >= 1 || rad == 0)
+	c = Math.sqrt(-2 * Math.log(rad) / rad);
+	return x1 * c
 }
 
 function randomChoice(inList) {
+	// Todo: move into gridEdit.html, remove from here.
 	return inList[Math.floor(Math.random() * inList.length)]
 }
 
 function randomInt(min,max) {
+	// Todo: move into gridEdit.html, remove from here.
 	return Math.floor(Math.random()*(max - min + 1) + min)
 }
 
 function zip(arrays) {
 	// Take in two arrays as [A,B], return [[a1,b1],[a2,b2,],...]
+	// Todo: move into gridlabmulti, remove from here.
 	return arrays[0].map(function(_,i){
 		return arrays.map(function(array){return array[i]})
 	})
@@ -122,6 +92,7 @@ function zip(arrays) {
 
 function partition(inL, eqRel) {
 	// Group inL as compared by eqRel. Make sure the eqRel is an equivalence relation, or your brain will hurt.
+	// Todo: move into gridlabmulti, remove from here.
 	if (inL.length == 0) {return inL}
 	if (inL.length == 1) {return [inL]}
 	accum = []
@@ -141,6 +112,7 @@ function partition(inL, eqRel) {
 
 function flatten1(matrix) {
 	// Take [A1,A2,...An] and return an in-order list of the items in each Ai.
+	// Todo: move into gridlabmulti, remove from here.
 	accum = []
 	for (i=0;i<matrix.length;i++) {
 		if (typeof matrix[i] == 'object') {
@@ -156,22 +128,27 @@ function flatten1(matrix) {
 
 function arrMax(arr) {
 	// Get max of an array.
+	// Todo: move into gridlabmulti, remove from here.
 	return Math.max.apply(null,arr)
 }
 
 function arrSum(arr) {
 	// Sum an array.
+	// Todo: move into gridlabmulti, remove from here.
 	return arr.reduce(function(x,y){return x+y})
 }
 
 function indexFind(arr, fun) {
 	// Given an array of objects, return the first one where fun(arr[i]) is true.
+	// Todo: move into gridlabmulti, remove from here.
 	for (i=0;i<arr.length;i++) {
 		if (fun(arr[i])) {return i}
 	}
 	return -1
 }
+
 function dropPill(thisButton, name) {
+	// Drop down a pill menu, used on home, transEdit, etc.
 	thisButton.style.color= 'black'
 	thisButton.style.background= '#F8F8F8'
 	thisButton.style.textAlign = 'left'
@@ -188,6 +165,7 @@ function dropPill(thisButton, name) {
 	document.body.addEventListener('click', clickCloseEvent, true)
 }
 function clickCloseEvent(labelName, buttonName) {
+	// close handler for dropdown menus on home, transEdit, etc.
 	var thisButton = document.getElementById(buttonName);
 	thisButton.nextSibling.nextSibling.style.display = 'none'
 	thisButton.innerHTML = labelName + ' ▾'
@@ -208,7 +186,27 @@ function delimitNumbers(nStr) {
 	return x1 + x2
 }
 
+function post_to_url(path, params, method) {
+	// helper function for cancelModel, deleteModel, shareModel, duplicateModel, createModelName.
+	method = method || 'post' // Set method to post by default, if not specified.
+	var form = document.createElement('form')
+	form.setAttribute('method', method)
+	form.setAttribute('action', path)
+	for(var key in params) {
+		if(params.hasOwnProperty(key)) {
+			var hiddenField = document.createElement('input')
+			hiddenField.setAttribute('type', 'hidden')
+			hiddenField.setAttribute('name', key)
+			hiddenField.setAttribute('value', params[key])
+			form.appendChild(hiddenField)
+		}
+	}
+	document.body.appendChild(form)
+	form.submit()
+}
+
 function cancelModel() {
+	// Cancels model execution via the button on each model page.
 	params = {user:allInputData.user,
 		modelName:allInputData.modelName,
 		modelType:allInputData.modelType}
@@ -216,6 +214,7 @@ function cancelModel() {
 }
 
 function deleteModel() {
+	// Warn before deleting when user hits delete button on model.
 	if (confirm("Deleting this model cannot be undone. Continue?")){
 		post_to_url("/delete/Model/"+allInputData.user+"/"+allInputData.modelName, {}, "POST")
 	} else {
@@ -223,10 +222,8 @@ function deleteModel() {
 	}
 }
 
-/**
- *
- */
 function shareModel() {
+	// Display dialog and handle user inputs for the share model button.
 	const viewers = allInputData.viewers == null ? [] : allInputData.viewers;
 	const modal = document.createElement("div");
 	modal.id = "emailModal";
@@ -381,6 +378,7 @@ function shareModel() {
 }
 
 function duplicateModel() {
+	// handle user input when user hits the duplicate model button.
 	newName = prompt("Create a duplicate with name", allInputData.modelName)
 	while (! /^[\w\s]+$/.test(newName)){
 		newName = prompt("Public a copy with new name, only letters, digits and underscore are allowed in the model name.\nPlease rename your new model", allInputData.modelName)
@@ -421,6 +419,7 @@ function isFormValid() {
 }
 
 function createModelName(modelType, modelName) {
+	// get model name before creation and check to make sure it's not a duplicate.
 	var username = sessionStorage.getItem('user');
 	console.log(username)
 	if (typeof(modelName)==='undefined') modelName = '';
@@ -443,12 +442,9 @@ function createModelName(modelType, modelName) {
 		});
 	}
 }
-	
-function showSaveAlert(url){
-	alert("To return to this model in the future, save this url:\n" + url)
-}
 
 function editFeeder(modelName, feederNum) {
+	// TODO: remove, replace with just having the button open a new window with the edit feeder route. But... can buttons be forced to open a new window?
 	console.log("modelName:",modelName)
 	studyUser = allInputData.user
 	window.open('/displayMap/' + studyUser + '/' + modelName + '/' + feederNum, '_blank');
