0af82e6aac025d601be001592ec3b278a6ca9ba8
jenny <jenthakkar@gmail.com>
2024-12-10 04:54:25 -0500

    HostingCapacity: Fixed Feeder name

--- omf/models/hostingCapacity.py
+++ omf/models/hostingCapacity.py
@@ -165,6 +165,7 @@ def run_traditional_algorithm( modelDir, inputDict, outData ):
 	# traditional hosting capacity if they uploaded an omd circuit file and chose to use it.
 	# Check if the file was uploaded and checks to make sure the name matches
 	feederName = [x for x in os.listdir(modelDir) if x.endswith('.omd')][0]
+	inputDict['feederName1'] = feederName[:-4]
 	path_to_omd = Path(modelDir, feederName)
 	tree = opendss.dssConvert.omdToTree(path_to_omd)
 	opendss.dssConvert.treeToDss(tree, Path(modelDir, 'circuit.dss'))
